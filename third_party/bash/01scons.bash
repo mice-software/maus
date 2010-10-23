ce1efc66595f08f29c2d83dac4409070c4823862
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2010-10-23 15:14:18 +0100

    tweaking

--- third_party/bash/01scons.bash
+++ third_party/bash/01scons.bash
@@ -54,12 +54,11 @@ if [ -n "${MAUS_ROOT_DIR+x}" ]; then
 fi
 
 else
-echo "MAUS_ROOT_DIR is not set.  Please run:"
+echo "MAUS_ROOT_DIR is not set, which is required to know where to install this package.  You have two options:"
+echo
+echo "1. Set the MAUS_ROOT_DIR from the command line by (if XXX is the directory where MAUS is installed):"
+echo "        MAUS_ROOT_DIR=XXX ${0}"
+echo
+echo "2. Run the './configure' script in the MAUS ROOT directory, run 'source env.sh', then rerun this command"
 echo 
-echo "./configure"
-echo "from the top MAUS directory. Then:"
-echo 
-echo "source env.sh"
-echo 
-echo "Cheers, bye!"
 fi
