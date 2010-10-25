cf3009e4ce9341bb88ca5945cd2755f4f76f9ccd
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2010-10-25 15:06:12 +0100

    This commit refs #123

--- third_party/bash/23geant4.bash
+++ third_party/bash/23geant4.bash
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-http://geant4.cern.ch/support/source/geant4.9.2.p04.tar.gz
-
-directory=geant4.9.2.p04
-filename=${directory}.tar.gz 
+directory=geant4.9.2.p01
+filename=${directory}.tar.gz
 url=http://geant4.cern.ch/support/source/${filename}
 
 
@@ -11,48 +9,79 @@ if [ -n "${MAUS_ROOT_DIR+x}" ]; then
 
     if [ -e "${MAUS_ROOT_DIR}/third_party/source/${filename}" ]
     then
-	echo "Found source archive in 'source' directory"
-    else
-	echo "Source archive doesn't exist.  Downloading..."
+	echo "INFO: Found source archive in 'source' directory"
+    else  
+	echo "INFO: Source archive doesn't exist.  Downloading..."
 
 	wget --directory-prefix=${MAUS_ROOT_DIR}/third_party/source ${url}
+	wget --directory-prefix=${MAUS_ROOT_DIR}/third_party/source http://www-pnp.physics.ox.ac.uk/~tunnell/geant_config.tar.gz # TODO fixme
+	wget --directory-prefix=${MAUS_ROOT_DIR}/third_party/source http://www-pnp.physics.ox.ac.uk/~tunnell/geant_data.tar.gz  # TODO fixme
 
     fi
    
     if [ -e "${MAUS_ROOT_DIR}/third_party/source/${filename}" ]
     then
-	echo "Source archive exists."
-        echo "Checking MD5 checksum (otherwise the file didn't download properly):"
-        md5sum -c ${MAUS_ROOT_DIR}/third_party/source/${filename}.md5 || { echo "FATAL: Failed to download ${filename}. MD5 checksum failed."  >&2; rm -f ${MAUS_ROOT_DIR}/third_party/source/${filename}; exit 1; }
-
-	echo "Unpacking:"
+	echo "INFO: Source archive exists."
+	echo
+        echo "INFO: Checking MD5 checksum (otherwise the file didn't"
+	echo "INFO: download properly):"
+	echo
+        cd ${MAUS_ROOT_DIR}/third_party/source
+	md5sum -c ${filename}.md5 || { echo "FATAL: Failed to download:" >&2; echo "FATAL: ${filename}." >&2; echo "FATAL: MD5 checksum failed.">&2; echo "FATAL: Try rerunning this command to redownload, or check" >&2; echo "FATAL: internet connection"  >&2; rm -f ${filename}; exit 1; }
+	md5sum -c geant_data.tar.gz.md5 || { echo "FATAL: Failed to download:" >&2; echo "FATAL: geant_data.tar.gz." >&2; echo "FATAL: MD5 checksum failed.">&2; echo "FATAL: Try rerunning this command to redownload, or check" >&2; echo "FATAL: internet connection"  >&2; rm -f ${filename}; exit 1; }  # TODO fixme
+	md5sum -c geant_config.tar.gz.md5 || { echo "FATAL: Failed to download:" >&2; echo "FATAL: geant_config.tar.gz." >&2; echo "FATAL: MD5 checksum failed.">&2; echo "FATAL: Try rerunning this command to redownload, or check" >&2; echo "FATAL: internet connection"  >&2; rm -f ${filename}; exit 1; } # todo fixme
+	sleep 1
+	echo
+	echo "INFO: Unpacking:"
+	echo
         rm -Rf ${MAUS_ROOT_DIR}/third_party/build/${directory}
         sleep 1
         tar xvfz ${MAUS_ROOT_DIR}/third_party/source/${filename} -C ${MAUS_ROOT_DIR}/third_party/build
-        mv ${MAUS_ROOT_DIR}/third_party/build/root ${MAUS_ROOT_DIR}/third_party/build/${directory}
+	mv ${MAUS_ROOT_DIR}/third_party/build/${version}/CLHEP ${MAUS_ROOT_DIR}/third_party/build/${directory}
         cd ${MAUS_ROOT_DIR}/third_party/build/${directory}
-        echo "Configuring:"
+	echo
+        echo "INFO: Configuring:"
+	echo
         sleep 1
-        ./configure --enable-roofit --disable-fftw3 --disable-xrootd --disable-krb5 --build=debug --with-gsl-incdir=${MAUS_ROOT_DIR}/third_party/install/include --with-gsl-libdir=${MAUS_ROOT_DIR}/third_party/install/lib --prefix=${MAUS_ROOT_DIR}/third_party/install
-        echo "Making:"
+        ./configure --prefix=${MAUS_ROOT_DIR}/third_party/install
+	echo
+        echo "INFO: Making:"
+	echo
         sleep 1
-        make -j5
+        make
 	make install
-        echo "${filename} should be locally build now in your third_party directory, which the rest of MAUS will find."
+	            ################################################## 
+	echo
+        echo "INFO: The package should be locally build now in your"
+	echo "INFO: third_party directory, which the rest of MAUS will"
+	echo "INFO: find."
     else
-	echo "Source archive still doesn't exist.  Please file a bug report with your operating system, distribution, and any other useful information at:"
-	echo
-	echo "http://micewww.pp.rl.ac.uk:8080/projects/maus/issues/"
-	echo
-	echo "Giving up, sorry..."
+	echo "FATAL: Source archive still doesn't exist.  Please file a bug report with your operating system,">&2
+	echo "FATAL: distribution, and any other useful information at:" >&2
+	echo "FATAL: " >&2
+	echo "FATAL: http://micewww.pp.rl.ac.uk:8080/projects/maus/issues/" >&2
+	echo "FATAL:" >&2
+	echo "FATAL: Giving up, sorry..." >&2
     fi
   
-else
-echo "MAUS_ROOT_DIR is not set, which is required to know where to install this package.  You have two options:"
+else         ################################################## 
 echo
-echo "1. Set the MAUS_ROOT_DIR from the command line by (if XXX is the directory where MAUS is installed):"
-echo "        MAUS_ROOT_DIR=XXX ${0}"
-echo
-echo "2. Run the './configure' script in the MAUS ROOT directory, run 'source env.sh', then rerun this command"
+echo "FATAL: MAUS_ROOT_DIR is not set, which is required to" >&2  
+echo "FATAL: know where to install this package.  You have two" >&2 
+echo "FATAL: options:" >&2
+echo "FATAL:" >&2
+echo "FATAL: 1. Set the MAUS_ROOT_DIR from the command line by" >&2 
+echo "FATAL: (if XXX is the directory where MAUS is installed):" >&2
+echo "FATAL:" >&2 
+echo "FATAL:        MAUS_ROOT_DIR=XXX ${0}" >&2
+echo "FATAL:" >&2
+echo "FATAL: 2. Run the './configure' script in the MAUS ROOT" >&2 
+echo "FATAL: directory, run 'source env.sh' then rerun this" >&2  
+echo "FATAL: command ">&2
 echo
 fi
+
+# This is the maximum length of text (ignoring whitespace), 50 chars
+##################################################
+# this is the maximum amount of characters (including whitespace) per line)
+######################################################################
