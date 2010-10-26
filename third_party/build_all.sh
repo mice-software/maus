0d4bf1fb66a7807a012a4edc63156f64efb79b78
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2010-10-26 02:47:56 +0100

    nice buildall script (for stuff in bash)

---
+++ third_party/build_all.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+if [ -n "${MAUS_ROOT_DIR+x}" ]; then
+    find bash | grep -v \# | grep bash |grep -v REDUNDANT |sort| xargs -i bash {}
+else
+echo
+echo "FATAL: MAUS_ROOT_DIR is not set, which is required to" >&2
+echo "FATAL: know where to install this package.  You have two" >&2
+echo "FATAL: options:" >&2
+echo "FATAL:" >&2
+echo "FATAL: 1. Set the MAUS_ROOT_DIR from the command line by" >&2
+echo "FATAL: (if XXX is the directory where MAUS is installed):" >&2
+echo "FATAL:" >&2
+echo "FATAL:        MAUS_ROOT_DIR=XXX ${0}" >&2
+echo "FATAL:" >&2
+echo "FATAL: 2. Run the './configure' script in the MAUS ROOT" >&2
+echo "FATAL: directory, run 'source env.sh' then rerun this" >&2
+echo "FATAL: command ">&2
+echo
+fi
+
