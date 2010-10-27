3dd9cadb3f428d9845a447b75420fe3ea9eb8d1e
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2010-10-27 19:48:21 +0100

    exit line forgot maus_root_dir in bash search

--- third_party/build_all.bash
+++ third_party/build_all.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ -n "${MAUS_ROOT_DIR+x}" ]; then
-    find bash -type f -executable |grep -v REDUNDANT |sort| xargs -i bash {}
+    find ${MAUS_ROOT_DIR}/bash -type f -executable |grep -v REDUNDANT |sort| xargs -i bash {}
 else
 echo
 echo "FATAL: MAUS_ROOT_DIR is not set, which is required to" >&2
@@ -15,6 +15,6 @@ echo "FATAL:" >&2
 echo "FATAL: 2. Run the './configure' script in the MAUS ROOT" >&2
 echo "FATAL: directory, run 'source env.sh' then rerun this" >&2
 echo "FATAL: command ">&2
-echo
+exit 1
 fi
 
