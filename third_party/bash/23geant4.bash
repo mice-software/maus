dd4b34d7c696b17096c7cedb899bbe363e13345f
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2010-10-29 22:35:00 +0100

    resolve conflict

--- third_party/bash/23geant4.bash
+++ third_party/bash/23geant4.bash
@@ -16,11 +16,7 @@ if [ -n "${MAUS_ROOT_DIR+x}" ]; then
 	rm -f ${MAUS_ROOT_DIR}/third_party/source/geant_config.tar.gz 
 	rm -f ${MAUS_ROOT_DIR}/third_party/source/geant_data.tar.gz 
 	wget --directory-prefix=${MAUS_ROOT_DIR}/third_party/source ${url}
-<<<<<<< TREE
-	wget --directory-prefix=${MAUS_ROOT_DIR}/third_party/source http://micewww.pp.rl.ac.uk:8080/attachments/download/82/geant_config.tar.gz # TODO fixme
-=======
 	wget --directory-prefix=${MAUS_ROOT_DIR}/third_party/source http://micewww.pp.rl.ac.uk:8080/attachments/download/84/geant_config.tar.gz # TODO fixme
->>>>>>> MERGE-SOURCE
 	wget --directory-prefix=${MAUS_ROOT_DIR}/third_party/source http://micewww.pp.rl.ac.uk:8080/attachments/download/81/geant_data.tar.gz  # TODO fixme
 
     fi
