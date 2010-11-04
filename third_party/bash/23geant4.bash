7c6ad728db9eb2ef34817131bdcdac107bdb2015
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2010-11-04 20:03:39 +0000

    get around geant4 bug about needing interactive shells (removed sanity check, living dangerously ;)

--- third_party/bash/23geant4.bash
+++ third_party/bash/23geant4.bash
@@ -18,6 +18,7 @@ if [ -n "${MAUS_ROOT_DIR+x}" ]; then
 	wget --directory-prefix=${MAUS_ROOT_DIR}/third_party/source ${url}
 	wget --directory-prefix=${MAUS_ROOT_DIR}/third_party/source http://micewww.pp.rl.ac.uk:8080/attachments/download/92/geant_config.tar.gz # TODO fixme
 	wget --directory-prefix=${MAUS_ROOT_DIR}/third_party/source http://micewww.pp.rl.ac.uk:8080/attachments/download/81/geant_data.tar.gz  # TODO fixme
+	wget --directory-prefix=${MAUS_ROOT_DIR}/third_party/source http://micewww.pp.rl.ac.uk:8080/attachments/download/97/Configure # todo fixme
 
     fi
    
@@ -52,6 +53,7 @@ if [ -n "${MAUS_ROOT_DIR+x}" ]; then
         echo
         tar xvfz ${MAUS_ROOT_DIR}/third_party/source/geant_config.tar.gz > /dev/null
 	maus_geant4_setup.py ${MAUS_ROOT_DIR}/third_party/build/geant4.9.2.p01/.config/bin/Linux-g++ ${MAUS_ROOT_DIR}/third_party/build/geant4.9.2.p01 ${MAUS_ROOT_DIR}/third_party/install
+	cp ${MAUS_ROOT_DIR}/third_party/source/Configure .
 	
 	echo
         echo "INFO: Building:"
