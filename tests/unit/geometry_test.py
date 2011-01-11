09e9497f4dbd79da89e99492d9b6baaf29668b1c
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2011-01-11 22:13:42 +0000

    geometry tests

---
+++ tests/unit/geometry_test.py
@@ -0,0 +1,51 @@
+import json
+import unittest
+import subprocess
+import glob
+import os
+import sys
+
+from MapCppSimulation import MapCppSimulation
+
+class MapCppSimulationTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        if not os.environ.get("MICEFILES"):
+            raise Exception('InitializeFail', 'MICEFILES environmental variable not set')
+
+        if not os.path.isdir(os.environ.get("MICEFILES")):
+            raise Exception('InitializeFail', 'MICEFILES is not a directory')
+
+
+    def test_geometries(self):
+        self.assertTrue(os.environ.get("MICEFILES") != None)
+        self.assertTrue(os.path.isdir(os.environ.get("MICEFILES")))
+
+        self.assertTrue(os.environ.get("MAUS_ROOT_DIR") != None)
+        self.assertTrue(os.path.isdir(os.environ.get("MAUS_ROOT_DIR")))
+        
+        files = glob.glob("%s/Models/Configurations/*.dat" % os.environ.get("MICEFILES"))
+        self.assertTrue(len(files) != 0)
+
+        for file in files:
+            args = ['python', '%s/tests/unit/geometry_test.py' %  os.environ.get("MAUS_ROOT_DIR"), file]
+            p = subprocess.Popen(args)
+            p.wait()
+            self.assertEqual(p.returncode, 0)
+
+    def test_bad_geometry(self):
+        args = ['python', '%s/tests/unit/geometry_test.py' %  os.environ.get("MAUS_ROOT_DIR"), '/dev/null']
+        p = subprocess.Popen(args)
+        p.wait()
+        self.assertNotEqual(p.returncode, 0)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        unittest.main()
+    if len(sys.argv) == 2:
+        file = str(sys.argv[1])
+        
+        map = MapCppSimulation(file)
+        map.Birth()
+        map.Death()        
--- tests/unit/test_always_true.py
+++ tests/unit/test_always_true.py
@@ -1,5 +1,9 @@
 import unittest
 
-class DefaultWidgetSizeTestCase(unittest.TestCase):
+class AlwaysTrueTestCase(unittest.TestCase):
     def runTest(self):
         self.assertEqual(1, 1, '1 != 1?!')
+
+if __name__ == '__main__':
+    unittest.main()
+
