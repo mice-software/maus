59dffad4b05e07dd46e85ac0b42e7475c02cecf9
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2011-01-18 04:06:14 +0000

    update doxygen main page

--- doc/mainpage.hh
+++ doc/mainpage.hh
@@ -2,7 +2,7 @@
 
 Welcome to the MAUS code documentation!  This documentation is meant to document the code within MAUS rather than explaining how to use the code.  If you want tutorials, documents explaining the structure of MAUS, or anything other than how classes and functions operation then you should consult the Wiki link above.
 
-These pages are made hourly by running Doxygen.  To learn how to write comments within MAUS such that Doxygen picks then up you can either see the Doxygen homepage or look at other sources files within MAUS.
+These pages are made hourly by running <a href="http://www.stack.nl/~dimitri/doxygen">Doxygen</a> .  To learn how to write comments within MAUS such that <a href="http://www.stack.nl/~dimitri/doxygen">Doxygen</a>  picks then up you can either see the <a href="http://www.stack.nl/~dimitri/doxygen">Doxygen</a>  homepage or look at other sources files within MAUS.
 
 @authors Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>, G4MICE Developers
 
