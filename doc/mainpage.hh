1dcf67ba4ea3c26769d3e6e3351ecc7f10e70651
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2011-01-18 04:03:49 +0000

    update doxygen mainpage

--- doc/mainpage.hh
+++ doc/mainpage.hh
@@ -1,6 +1,10 @@
 /** @mainpage MAUS: MICE Analysis User Software
 
-Welcome to the MAUS code documentation!  I should put something
-profound here.
+Welcome to the MAUS code documentation!  This documentation is meant to document the code within MAUS rather than explaining how to use the code.  If you want tutorials, documents explaining the structure of MAUS, or anything other than how classes and functions operation then you should consult the Wiki link above.
+
+These pages are made hourly by running Doxygen.  To learn how to write comments within MAUS such that Doxygen picks then up you can either see the Doxygen homepage or look at other sources files within MAUS.
+
+@authors Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>, G4MICE Developers
+
 
 */
