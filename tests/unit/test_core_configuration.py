da1379f70019d4341fbf1c9f0a65a9b9fcf65b0e
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2011-01-24 09:27:56 +0000

    configuration works

---
+++ tests/unit/test_core_configuration.py
@@ -0,0 +1,47 @@
+import json
+import unittest
+import subprocess
+import glob
+import os
+import sys
+import StringIO
+
+import Configuration
+
+class ConfigurationTestCase(unittest.TestCase):
+    def test_defaults(self):
+        ## actual data
+        value = Configuration.getConfigJSON()
+
+        ## test setup
+        MAUSRootDir = os.environ.get('MAUS_ROOT_DIR')
+        self.assertNotEqual(MAUSRootDir,  None)
+
+        configDict = {}
+        defaultFilename = '%s/core/ConfigurationDefaults.py' % MAUSRootDir
+        exec(open(defaultFilename,'r').read(), globals(), configDict)
+
+        configJSONStr = json.JSONEncoder().encode(configDict)
+
+        ## compare
+        self.assertEqual(value, configJSONStr)
+
+    def test_new_value(self):
+        ## actual data
+        stringFile = StringIO.StringIO("test = 4")
+        value = Configuration.getConfigJSON(stringFile)
+
+        jsonValue = json.loads(value)
+        self.assertEqual(jsonValue["test"], 4)
+
+    def test_overwrite_value(self):
+        ## actual data
+        stringFile = StringIO.StringIO("geometry_file = 'Stage4.dat'")
+        value = Configuration.getConfigJSON(stringFile)
+
+        jsonValue = json.loads(value)
+        self.assertEqual(jsonValue["geometry_file"], 'Stage4.dat')
+
+
+if __name__ == '__main__':
+    unittest.main()
