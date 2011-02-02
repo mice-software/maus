06ded03e40ad2b8915f05bd3781227982f7a1004
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2011-02-02 00:26:36 +0000

    load test stuff

--- macros/load_test.py
+++ macros/load_test.py
@@ -7,20 +7,29 @@ from MapPyFakeTestSimulation import *
 from MapPyDoNothing import *
 from ReducePyDoNothing import *
 from OutputPyJSON import *
-
+import io
 from Go import *
 
 big_number = 1000
+use_tracks = False
 if len(sys.argv) == 2:
     big_number = int(sys.argv[1])
+    print big_number
+    if big_number < 0:
+        myConfig = io.StringIO(u"MapPyFakeTestSimulation_withTracks = False")
+        big_number = -1 * big_number
+    else:
+        myConfig = io.StringIO(u"MapPyFakeTestSimulation_withTracks = True")
+
 
 print("Testing with %d events..." % big_number)
 
 inputer = InputPyEmptyDocument(big_number)
 mapper = MapPyFakeTestSimulation()
+
 #mapper = MapPyDoNothing()
 reducer = ReducePyDoNothing()
 outputer = OutputPyJSON(gzip.GzipFile('mausput.gz', 'wb'))
 
 
-Go(inputer, mapper, reducer, outputer)
+Go(inputer, mapper, reducer, outputer, myConfig)
