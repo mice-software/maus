6cf3eb3b5f3ef86c3f445bf93436f616aca3d71f
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2011-02-11 19:36:08 +0000

    added comments

--- bin/simulate_mice.py
+++ bin/simulate_mice.py
@@ -1,54 +1,39 @@
 #!/usr/bin/env python
-import io
-from InputPyJSON import *
-from OutputPyJSON import *
-from MapCppSimulation import *
-from MapPyRemoveTracks import *
-from ReducePyDoNothing import *
-from MapPyGroup import *
-from MapCppPrint import *
-from MapCppTOFDigitization import *
-from MapCppTrackerDigitization import *
-from Go import *
+import io  #  generic python library for I/O
 
-# Create an empty array
-myMap = MapPyGroup()
+from InputPyJSON import *  #  read in raw JSON document to start
+from MapPyGroup import *  #  chain map steps together
+from MapCppSimulation import *  #  used for geant4 simulation
+from MapCppTOFDigitization import *  #  TOF digitize hits
+from MapCppTrackerDigitization import *  #  tracker digitize hits
+from ReducePyDoNothing import *  #  do nothing
+from OutputPyJSON import *  #  write output to JSON
 
-file = 'Stage6.dat' #'%s/Models/Configurations/Stage6.dat' % os.environ.get('MICEFILES')
+# All components are tied together with this Go()
+# class.  This is the 'core' of MAUS.
+from Go import * 
 
-print(file)
 
-# Append the mappers we want
-sim = MapCppSimulation()
-myMap.append(sim)
-
-#rt.KeepOnlyMuons()
-#myMap.append(rt)
-tof = MapCppTOFDigitization()
-myMap.append(tof)
-
-tracker = MapCppTrackerDigitization()
-myMap.append(tracker)
-
-myMap.append(MapCppPrint())
-
-#myMap.append(rt)
-
+# Here we create a pseudo-file with an event in it.  If you were to copy
+# and paste this to a file, then you could also do:
+# 
+#   documentFile = open('myFileName.txt', 'r')
+#
+# where the file format has a JSON document per line.  I just toss the file
+# in here for simplicity.
 number_of_events = 10
+documentFile = io.StringIO(number_of_events*u"""{"mc": [{"position": { "x": 0.0, "y": -0.0, "z": -5000 },"particle_id" : 13,"energy" : 210, "random_seed" : 10, "unit_momentum" : { "x":0, "y":0, "z":1 }}]}\n""")
 
-document = io.StringIO(number_of_events*u"""{"mc": [{"position": { "x": 0.0, "y": -0.0, "z": -5000 },"particle_id" : 13,"energy" : 210, "random_seed" : 10, "unit_momentum" : { "x":0, "y":0, "z":1 }}]}\n""")
+myInput = InputPyJSON(documentFile)
 
+# Create an empty array of mappers, then populate it
+# with the functionality you want to use.
+myMap = MapPyGroup()
+myMap.append(MapCppSimulation())  #  geant4 simulation 
+myMap.append(MapCppTOFDigitization())  #  TOF detector digitization
+myMap.append(MapCppTrackerDigitization())  #  tracker detector digitization
+myMap.append(MapCppPrint())  #  print to screen
 
-Go(InputPyJSON(document), myMap, ReducePyDoNothing(), OutputPyJSON())
+#  The Go() drives all the components you pass in.
+Go(myInput, myMap, ReducePyDoNothing(), OutputPyJSON())
 
-#new_document = map(myMap.Process, [document])
-#print new_document
-#new_document = reduce(ReducePyDoNothing.Process, [document])
-#print new_document
-
-
-
-#f = open("output", "w")
-#for thing in new_document:
-#    f.write("%s" % thing)
-#f.close()
