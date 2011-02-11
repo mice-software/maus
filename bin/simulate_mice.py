7f7ffa8f9221c9c061e6ddf4275507bb54e26a38
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2011-02-11 18:47:08 +0000

    simulate one particle demo

---
+++ bin/simulate_mice.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+import io
+from InputPyJSON import *
+from OutputPyJSON import *
+from MapCppSimulation import *
+from MapPyRemoveTracks import *
+from ReducePyDoNothing import *
+from MapPyGroup import *
+from MapCppPrint import *
+from MapCppTOFDigitization import *
+from MapCppTrackerDigitization import *
+from Go import *
+
+import os, sys
+
+# Create an empty array
+myMap = MapPyGroup()
+
+file = 'Stage6.dat' #'%s/Models/Configurations/Stage6.dat' % os.environ.get('MICEFILES')
+
+print(file)
+# Append the mappers we want
+sim = MapCppSimulation()
+sim.DisableStoredTracks()
+myMap.append(sim)
+
+#rt.KeepOnlyMuons()
+#myMap.append(rt)
+tof = MapCppTOFDigitization()
+myMap.append(tof)
+
+tracker = MapCppTrackerDigitization()
+myMap.append(tracker)
+
+#myMap.append(rt)
+
+#Stage1.dat  Stage2.dat  Stage3.dat  Stage4.dat  Stage5.dat  Stage6.dat  Stage6_FieldsWithoutMaterial.dat
+#'%s/Models/Configurations/Stage6_FieldsWithoutMaterial.dat' % os.environ.get('MICEFILES')
+print(sys.argv)
+
+number_of_events = 10
+
+document = io.StringIO(number_of_events*u"""{"mc": [{"position": { "x": 0.0, "y": -0.0, "z": -5000 },"particle_id" : 13,"energy" : 210, "random_seed" : 10, "unit_momentum" : { "x":0, "y":0, "z":1 }}]}\n""")
+
+
+Go(InputPyJSON(document), myMap, ReducePyDoNothing(), OutputPyJSON())
+
+#new_document = map(myMap.Process, [document])
+#print new_document
+#new_document = reduce(ReducePyDoNothing.Process, [document])
+#print new_document
+
+
+
+#f = open("output", "w")
+#for thing in new_document:
+#    f.write("%s" % thing)
+#f.close()
