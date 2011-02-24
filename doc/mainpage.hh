7a62adbe794d8dcefdc5369ff6261e090c2134cf
Timothy Carlisle (MICE 2008) <carlisle@pplxint6.physics.ox.ac.uk>
2011-02-24 17:14:14 +0000

    added name to developer list

--- doc/mainpage.hh
+++ doc/mainpage.hh
@@ -4,7 +4,7 @@ Welcome to the MAUS code documentation!  This documentation is meant to document
 
 These pages are made hourly by running <a href="http://www.stack.nl/~dimitri/doxygen">Doxygen</a> .  To learn how to write comments within MAUS such that <a href="http://www.stack.nl/~dimitri/doxygen">Doxygen</a>  picks then up you can either see the <a href="http://www.stack.nl/~dimitri/doxygen">Doxygen</a>  homepage or look at other sources files within MAUS.
 
-@authors Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>, G4MICE Developers
+@authors Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>, Timothy Carlisle <t.carlisle1@physics.ox.ac.uk>, G4MICE Developers
 
 
 */
