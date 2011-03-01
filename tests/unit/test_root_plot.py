cda926c1cd5c48836ce1c8d8bb45d1c1fddb9b02
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2011-03-01 12:01:24 +0000

    add exception check

--- tests/unit/test_root_plot.py
+++ tests/unit/test_root_plot.py
@@ -4,7 +4,10 @@ import unittest
 class RootPlotTestCase(unittest.TestCase):
     def test_geometries(self):
         h = ROOT.TH1F("h", "", 100, 0, 100)
-        h.Draw() # if crashes, test fails.  tests ROOT environment
+        try:
+            h.Draw() # if crashes, test fails.  tests ROOT environment
+        except:
+            self.fail("ROOT raises exception")
 
 if __name__ == '__main__':
     unittest.main()
