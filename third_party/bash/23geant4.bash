b076d143a25e803ad2e3786dabb93500c68b3dda
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2011-03-01 21:06:52 +0000

    geant fix for header files

--- third_party/bash/23geant4.bash
+++ third_party/bash/23geant4.bash
@@ -65,7 +65,8 @@ if [ -n "${MAUS_ROOT_DIR+x}" ]; then
 	echo
         sleep 1
 
-	./Configure -f .config/bin/Linux-g++/config.sh -d -e -build && ./Configure -install
+	./Configure -f .config/bin/Linux-g++/config.sh -d -e -build
+	./Configure -install
 
 	#echo
         #echo "INFO: Installing:"
