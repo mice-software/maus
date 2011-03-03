36b5eb88a83d7a418fcde315d63de034a9826fdb
Chris Rogers <chris.rogers@stfc.ac.uk>
2011-03-03 09:35:46 +0000

    Testing bzr

--- doc/mainpage.hh
+++ doc/mainpage.hh
@@ -1,6 +1,6 @@
 /** @mainpage MAUS: MICE Analysis User Software
 
-Welcome to the MAUS code documentation!  This documentation is meant to document the code within MAUS rather than explaining how to use the code.  If you want tutorials, documents explaining the structure of MAUS, or anything other than how classes and functions operation then you should consult the Wiki link above.
+Welcome to the MAUS code documentation!  This documentation is meant to document the code within MAUS rather than explaining how to use the code. If you want tutorials, documents explaining the structure of MAUS, or anything other than how classes and functions operation then you should consult the Wiki link above.
 
 These pages are made hourly by running <a href="http://www.stack.nl/~dimitri/doxygen">Doxygen</a> .  To learn how to write comments within MAUS such that <a href="http://www.stack.nl/~dimitri/doxygen">Doxygen</a>  picks then up you can either see the <a href="http://www.stack.nl/~dimitri/doxygen">Doxygen</a>  homepage or look at other sources files within MAUS.
 
