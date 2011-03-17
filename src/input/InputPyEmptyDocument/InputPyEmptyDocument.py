8f1f71aa245ce9586507cd20b2ab0a2d0772139d
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2011-03-17 17:16:33 +0000

    style fixes

--- src/input/InputPyEmptyDocument/InputPyEmptyDocument.py
+++ src/input/InputPyEmptyDocument/InputPyEmptyDocument.py
@@ -1,57 +1,67 @@
 #  This file is part of MAUS: http://micewww.pp.rl.ac.uk:8080/projects/maus
-# 
+#
 #  MAUS is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
-# 
+#
 #  MAUS is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-# 
+#
 #  You should have received a copy of the GNU General Public License
 #  along with MAUS.  If not, see <http://www.gnu.org/licenses/>.
 
-
-## @class InputPyEmptyDocument.InputPyEmptyDocument
-#  InputPyEmptyDocument lets MAUS start with empty spills
-#
-#  This class is useful for either debugging or for simulating
-#  spills since it loads the mostly empty JSON document into
-#  MAUS which is only useful if something like a Simulation
-#  mapper is used to create all the data.  The documents this 
-#  yields contain only the spill number.  
-#
-#  @author Christopher Tunnell <c.tunnell1@physics.ox.ac.uk> 
-
-import json
+"""
+Package that contains the class InputPyEmptyDocument
+"""
 
 class InputPyEmptyDocument:
+    """
+    InputPyEmptyDocument lets MAUS start with empty spills
+    
+    This class is useful for either debugging or for simulating
+    spills since it loads the mostly empty JSON document into
+    MAUS which is only useful if something like a Simulation
+    mapper is used to create all the data.  The documents this
+    yields contain only the spill number.
 
-    ## Setup InputPyEmptyDocument
-    #
-    # Runs for argNumberOfEvents, then halts. If
-    # argNumberOfEvents is -1, then runs forever.
-    def __init__(self, argNumberOfEvents=10000):
-        assert isinstance(argNumberOfEvents, int)
-        self._numberOfEvents = argNumberOfEvents
+    @author Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
+    """
 
-    ## Birth sets spill counter to zero
-    def Birth(self):
-        self._currentSpill= 0
+    def __init__(self, arg_number_of_events=10000):
+        """
+        Setup InputPyEmptyDocument
 
-    ## Emits JSON documents with only event number
-    def Emitter(self):
-        if self._numberOfEvents < 0: # the infinite case 
+        Runs for arg_number_of_events, then halts. If
+        arg_number_of_events is -1, then runs forever.
+        """
+        assert isinstance(arg_number_of_events, int)
+        self._number_of_events = arg_number_of_events
+        self._current_spill = 0
+
+    def birth(self): 
+        """
+        birth() does nothing
+        """
+        self._current_spill = 0
+
+    def emitter(self):
+        """
+        Emits JSON documents with only event number
+        """
+        if self._number_of_events < 0: # the infinite case
             while 1:
-                yield """{ "spill_number" : %d }""" % self._currentSpill
-                self._currentSpill= self._currentSpill+ 1
-        else: # the finite case 
-            for i in range(self._numberOfEvents):
-                yield """{ "spill_number" : %d }""" % self._currentSpill
-                self._currentSpill= self._currentSpill+ 1
+                yield """{ "spill_number" : %d }""" % self._current_spill
+                self._current_spill = self._current_spill + 1
+        else: # the finite case
+            for i in range(self._number_of_events):
+                yield """{ "spill_number" : %d }""" % self._current_spill
+                self._current_spill = i
 
-    ## Death() Does nothing        
-    def Death(self):
+    def death(self):
+        """
+        death() does nothing
+        """
         pass
