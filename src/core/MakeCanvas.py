76922ba42a0208d457e132db789f61797a5f2d6f
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2011-03-19 08:50:31 +0000

    canvas pylint

--- src/core/MakeCanvas.py
+++ src/core/MakeCanvas.py
@@ -5,10 +5,10 @@
 
 import ROOT
 
-canvas_number = 0
+CANVAS_NUMBER = 0
 
-def MakeCanvas():
-    global canvas_number
+def canvas():
+    global CANVAS_NUMBER
 
     # Setup all the ROOT pretty-ing options
     ROOT.gROOT.SetStyle("Plain")
@@ -17,10 +17,10 @@ def MakeCanvas():
     ROOT.gStyle.SetPadColor(0)
     ROOT.gStyle.SetStatColor(0)
     ROOT.gStyle.SetOptTitle(0)
-    ROOT.gStyle.SetLabelFont(42,"XYZ")
+    ROOT.gStyle.SetLabelFont(42, "XYZ")
     ROOT.gStyle.SetTextFont(42)
     ROOT.gStyle.SetOptStat(111110)
-    ROOT.gStyle.SetTitleFont(42,"XYZ")
+    ROOT.gStyle.SetTitleFont(42, "XYZ")
     ROOT.gStyle.SetPadColor(0)
     ROOT.gStyle.SetCanvasColor(0)
     ROOT.gStyle.SetOptFit(0)
@@ -30,22 +30,18 @@ def MakeCanvas():
     ROOT.gStyle.SetStatX(0.75)
     ROOT.gStyle.SetStatY(0.8)
 
-    #hists
-#    ROOT.gStyle.SetHistFillColor(1)
-#    ROOT.gStyle.SetHistFillStyle(3001)
-
-    tsize=0.05
+    tsize = 0.05
     ROOT.gStyle.SetTextSize(tsize)
-    ROOT.gStyle.SetLabelSize(tsize,"xyz")
-    ROOT.gStyle.SetTitleSize(tsize,"xyz")
+    ROOT.gStyle.SetLabelSize(tsize, "xyz")
+    ROOT.gStyle.SetTitleSize(tsize, "xyz")
 
     ROOT.gStyle.SetNdivisions(509, "x")
     ROOT.gStyle.SetNdivisions(305, "y")
 
-    #ROOT.gStyle.SetTitleOffset(1.03, "x")
+    ROOT.gStyle.SetTitleOffset(1.03, "x")
     ROOT.gStyle.SetTitleOffset(1.46, "y")
 
-    ROOT.gStyle.SetPaperSize(20,26)
+    ROOT.gStyle.SetPaperSize(20, 26)
     ROOT.gStyle.SetPadTopMargin(0.05)
     ROOT.gStyle.SetPadRightMargin(0.05)
     ROOT.gStyle.SetPadBottomMargin(0.16)
@@ -55,16 +51,17 @@ def MakeCanvas():
     ROOT.gStyle.SetMarkerStyle(20)
     ROOT.gStyle.SetMarkerSize(1.2)
     ROOT.gStyle.SetHistLineWidth(2)
-    ROOT.gStyle.SetLineStyleString(2,"[12 12]")
+    ROOT.gStyle.SetLineStyleString(2, "[12 12]")
     ROOT.gStyle.SetPadTickX(1)
     ROOT.gStyle.SetPadTickY(1)
 
-    c1 = ROOT.TCanvas("c1_n%d" % canvas_number, "c1",0,44,800,602)
-    c1.Range(66.32653,-678.7319,91.31868,3835.625)
+    my_canvas = ROOT.TCanvas("my_canvas_n%d" % CANVAS_NUMBER, \
+                                 "my_canvas", 0, 44, 800, 602)
+    my_canvas.Range(66.32653, -678.7319, 91.31868, 3835.625)
             
-    c1.SetFrameBorderMode(0)
-    c1.SetBorderSize(0)
+    my_canvas.SetFrameBorderMode(0)
+    my_canvas.SetBorderSize(0)
 
-    canvas_number += 1
+    CANVAS_NUMBER += 1
                                                                 
-    return c1
+    return my_canvas
