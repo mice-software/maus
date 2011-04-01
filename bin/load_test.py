604a15f06c999eb2c6bf22f603ca4b9f7487f08f
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2011-04-01 14:29:37 +0100

    style guide bin

--- bin/load_test.py
+++ bin/load_test.py
@@ -1,36 +1,50 @@
 #!/usr/bin/env python
+
+#################################################################
+###!!! YOU ARE NOT ALLOWED TO MODIFY THIS FILE DIRECTLY    !!!###
+###!!! PLEASE MAKE A COPY OF THIS FILE WITH THE CP COMMAND !!!###
+#################################################################
+
 import sys
 
-from InputPyEmptyDocument import *
-from MapPyGroup import *
-from MapPyFakeTestSimulation import *
-from MapPyDoNothing import *
-from ReducePyDoNothing import *
-from OutputPyDoNothing import *
+from InputPyEmptyDocument import InputPyEmptyDocument
+from MapPyGroup import MapPyGroup 
+from MapPyFakeTestSimulation import MapPyFakeTestSimulation
+from MapPyDoNothing import MapPyDoNothing
+from ReducePyDoNothing import ReducePyDoNothing
+from OutputPyDoNothing import OutputPyDoNothing
 
-from Go import *
+from Go import Go
 
-mapGroup = MapPyGroup()
+def run():
+    """Run a load test
+    """
+    map_group = MapPyGroup()
 
-big_number = 1000
+    big_number = 1000
 
-if len(sys.argv) == 3:
-    print 'do nothing'
-    mapGroup.append(MapPyDoNothing())
-else:
-    mapGroup.append(MapPyFakeTestSimulation())
-
-if len(sys.argv) >= 2:
-    big_number = int(sys.argv[1])
-
-
-print("Testing with %d events..." % big_number)
-
-
-inputer = InputPyEmptyDocument(big_number)
-# mapMapGroup
-reducer = ReducePyDoNothing()
-outputer = OutputPyDoNothing()
+    if len(sys.argv) == 3:
+        print 'Using only empty events'
+        map_group.append(MapPyDoNothing())
+    else:
+        print 'Using fake simulation data'
+        map_group.append(MapPyFakeTestSimulation())
+        
+    if len(sys.argv) >= 2:
+        big_number = int(sys.argv[1])
 
 
-Go(inputer, mapGroup, reducer, outputer)
+    print("Testing with %d events..." % big_number)
+
+
+    inputer = InputPyEmptyDocument(big_number)
+    # mapMapGroup
+    reducer = ReducePyDoNothing()
+    outputer = OutputPyDoNothing()
+
+
+    Go(inputer, map_group, reducer, outputer)
+
+
+if __name__ == '__main__':
+    run()
--- bin/simulate_mice.py
+++ bin/simulate_mice.py
@@ -1,38 +1,60 @@
 #!/usr/bin/env python
+
+#################################################################
+###!!! YOU ARE NOT ALLOWED TO MODIFY THIS FILE DIRECTLY    !!!###
+###!!! PLEASE MAKE A COPY OF THIS FILE WITH THE CP COMMAND !!!###
+#################################################################
+
+
 import io  #  generic python library for I/O
 
-from InputPyJSON import *  #  read in raw JSON document to start
-from MapPyGroup import *  #  chain map steps together
-from MapCppSimulation import *  #  used for geant4 simulation
-from MapCppTOFDigitization import *  #  TOF digitize hits
-from MapCppTrackerDigitization import *  #  tracker digitize hits
-from ReducePyDoNothing import *  #  do nothing
-from OutputPyJSON import *  #  write output to JSON
+from InputPyJSON import InputPyJSON  #  read in raw JSON document to start
+from MapPyGroup import MapPyGroup  #  chain map steps together
+from MapCppSimulation import MapCppSimulation  #  used for geant4 simulation
+from MapCppTOFDigitization import MapCppTOFDigitization 
+from MapCppTrackerDigitization import MapCppTrackerDigitization  
+from ReducePyDoNothing import ReducePyDoNothing  #  do nothing
+from OutputPyJSON import OutputPyJSON  #  write output to JSON
 
 # All components are tied together with this Go()
 # class.  This is the 'core' of MAUS.
-from Go import * 
+from Go import Go
 
 
-# Here we create a pseudo-file with an event in it.  If you were to copy
-# and paste this to a file, then you could also do:
-# 
-#   documentFile = open('myFileName.txt', 'r')
-#
-# where the file format has a JSON document per line.  I just toss the file
-# in here for simplicity.
-number_of_events = 10
-documentFile = io.StringIO(number_of_events*u"""{"mc": [{"position": { "x": 0.0, "y": -0.0, "z": -5000 },"particle_id" : 13,"energy" : 210, "random_seed" : 10, "unit_momentum" : { "x":0, "y":0, "z":1 }}]}\n""")
+def run():
+    """Simulate the MICE experiment
+    """
 
-myInput = InputPyJSON(documentFile)
+    # Here we create a pseudo-file with an event in it.  If you were to copy
+    # and paste this to a file, then you could also do:
+    #
+    #   documentFile = open('myFileName.txt', 'r')
+    #
+    # where the file format has a JSON document per line.  I just toss the file
+    # in here for simplicity.
+    number_of_events = 10
+    document_file = io.StringIO(number_of_events*u"""{"mc": [{"position": { "x": 0.0, "y": -0.0, "z": -5000 },"particle_id" : 13,"energy" : 210, "random_seed" : 10, "unit_momentum" : { "x":0, "y":0, "z":1 }}]}\n""")
+    
+    my_input = InputPyJSON(document_file)
+    
+    # Create an empty array of mappers, then populate it
+    # with the functionality you want to use.
+    my_map = MapPyGroup()
+    my_map.append(MapCppSimulation())  #  geant4 simulation
+    my_map.append(MapCppTOFDigitization())  #  TOF detector digitization
+    my_map.append(MapCppTrackerDigitization())  #  tracker detector digitization
+    
+    datacards = io.StringIO(u"keep_tracks = False")
+    #  You may specify datacards if you wish.  To do so you create a file object
+    #  which can either be a StringIO object or a native python file.  If you
+    #  want to store your datacards in a file 'datacards.dat' then uncomment:
+    # datacards = open('datacards.dat', 'r')
+    
+    #  The Go() drives all the components you pass in, then check the file
+    #  'mausput' for the output
+    
+    Go(my_input, my_map, ReducePyDoNothing(), OutputPyJSON(), datacards)
 
-# Create an empty array of mappers, then populate it
-# with the functionality you want to use.
-myMap = MapPyGroup()
-myMap.append(MapCppSimulation())  #  geant4 simulation 
-myMap.append(MapCppTOFDigitization())  #  TOF detector digitization
-myMap.append(MapCppTrackerDigitization())  #  tracker detector digitization
 
-#  The Go() drives all the components you pass in, then check the file
-#  'mausput' for the output
-Go(myInput, myMap, ReducePyDoNothing(), OutputPyJSON(), io.StringIO(u"keep_tracks = False"))
+if __name__ == '__main__':
+    run()
