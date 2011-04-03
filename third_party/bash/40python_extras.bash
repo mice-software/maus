5188a307b564ba66d4523e03192dd18a0aef09ee
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2011-04-03 20:57:16 +0100

    cleanup configure

--- third_party/bash/40python_extras.bash
+++ third_party/bash/40python_extras.bash
@@ -2,7 +2,7 @@
 
 if [ -n "${MAUS_ROOT_DIR+x}" ]; then
 
-    easy_install pylint numpy suds pychecker validictory matplotlib scons
+    easy_install pylint numpy suds pychecker validictory matplotlib scons readline ipython
     
     echo "INFO: The package should be locally build now in your"
     echo "INFO: third_party directory, which the rest of MAUS will"
