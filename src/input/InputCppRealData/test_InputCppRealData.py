2db9e42669c9c19cb9b6ba57cb22417906f53006
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2011-04-11 16:26:28 +0100

    adding unpacker

--- src/input/InputCppRealData/test_InputCppRealData.py
+++ src/input/InputCppRealData/test_InputCppRealData.py
@@ -19,38 +19,38 @@ class InputCppRealDataTestCase(unittest.TestCase):
 
   def test_init(self):
     self.mapper = InputCppRealData()
-    self.assertTrue(self.mapper.Birth(self._datapath, \
+    self.assertTrue(self.mapper.birth(self._datapath, \
                                       self._datafile))
     # Check re-init without closing fails
-    self.assertFalse(self.mapper.Birth(self._datapath, \
+    self.assertFalse(self.mapper.birth(self._datapath, \
                                        self._datafile))
-    self.assertTrue(self.mapper.Death())
+    self.assertTrue(self.mapper.death())
     return
 
   # Test a single event
   def test_single(self):
     self.mapper = InputCppRealData()
-    self.assertTrue(self.mapper.Birth(self._datapath, \
+    self.assertTrue(self.mapper.birth(self._datapath, \
                                       self._datafile))
     # Get a single event and check it's the right size
     self.assertTrue(self.mapper.readNextEvent())
     data = self.mapper.getCurEvent()
     # Data shold be 15 (an empty spill, first event is start of burst)
     self.assertTrue(len(data) == 15)
-    self.assertTrue(self.mapper.Death())
+    self.assertTrue(self.mapper.death())
     return
 
   # Test reading the whole file
   def test_multi(self):
     self.mapper = InputCppRealData()
-    self.assertTrue(self.mapper.Birth(self._datapath, \
+    self.assertTrue(self.mapper.birth(self._datapath, \
                                       self._datafile))
     evntCount = 0
 
     # We can try md5'ing the whole dataset
     digester = md5.new()
 
-    for i in self.mapper.Emitter():
+    for i in self.mapper.emitter():
       digester.update(i)
       evntCount = evntCount + 1
 
@@ -60,7 +60,7 @@ class InputCppRealDataTestCase(unittest.TestCase):
     # Check the md5 sum matches the expected value
     self.assertTrue(digester.hexdigest() == '3c6f14d25d0254056efe082541a56664')
 
-    self.assertTrue(self.mapper.Death())
+    self.assertTrue(self.mapper.death())
 
   @classmethod
   def tearDownClass(self):
