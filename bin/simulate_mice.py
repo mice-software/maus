c0a48ba0ef806fc85d0f3e071e7fc809bdaeedc7
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2011-05-10 16:50:23 +0100

    use "import MAUS"

--- bin/simulate_mice.py
+++ bin/simulate_mice.py
@@ -8,18 +8,7 @@
 
 import io  #  generic python library for I/O
 
-from InputPyJSON import InputPyJSON  #  read in raw JSON document to start
-from MapPyGroup import MapPyGroup  #  chain map steps together
-from MapCppSimulation import MapCppSimulation  #  used for geant4 simulation
-from MapCppTOFDigitization import MapCppTOFDigitization 
-from MapCppTrackerDigitization import MapCppTrackerDigitization  
-from ReducePyDoNothing import ReducePyDoNothing  #  do nothing
-from OutputPyJSON import OutputPyJSON  #  write output to JSON
-
-# All components are tied together with this Go()
-# class.  This is the 'core' of MAUS.
-from Go import Go
-
+import MAUS
 
 def run(number_of_events):
     """Simulate the MICE experiment
@@ -37,14 +26,14 @@ def run(number_of_events):
     # in here for simplicity.
     document_file = io.StringIO(number_of_events*u"""{"mc": [{"position": { "x": 0.0, "y": -0.0, "z": -5000 },"particle_id" : 13,"energy" : 210, "random_seed" : 10, "unit_momentum" : { "x":0, "y":0, "z":1 }}]}\n""")
     
-    my_input = InputPyJSON(document_file)
+    my_input = MAUS.InputPyJSON(document_file)
     
     # Create an empty array of mappers, then populate it
     # with the functionality you want to use.
-    my_map = MapPyGroup()
-    my_map.append(MapCppSimulation())  #  geant4 simulation
-    my_map.append(MapCppTOFDigitization())  #  TOF detector digitization
-    my_map.append(MapCppTrackerDigitization())  #  tracker detector digitization
+    my_map = MAUS.MapPyGroup()
+    my_map.append(MAUS.MapCppSimulation())  #  geant4 simulation
+    my_map.append(MAUS.MapCppTOFDigitization())  #  TOF detector digitization
+    my_map.append(MAUS.MapCppTrackerDigitization())  #  tracker detector digitization
     
     datacards = io.StringIO(u"keep_tracks = False")
     #  You may specify datacards if you wish.  To do so you create a file object
@@ -55,7 +44,7 @@ def run(number_of_events):
     #  The Go() drives all the components you pass in, then check the file
     #  'mausput' for the output
     
-    Go(my_input, my_map, ReducePyDoNothing(), OutputPyJSON(), datacards)
+    MAUS.Go(my_input, my_map, MAUS.ReducePyDoNothing(), MAUS.OutputPyJSON(), datacards)
 
 
 if __name__ == '__main__':
