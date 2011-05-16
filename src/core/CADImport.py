8cc2e38a3161e2aff1bf5c4a9dd5ca936edf8d20
Matthew Littlefield <matt@ZMVA>
2011-05-16 16:00:52 +0100

    Tidied up the class and added some unit tests

---
+++ src/core/CADImport.py
@@ -0,0 +1,87 @@
+import os.path
+import libxml2
+import libxslt
+
+class CADImport:
+    def __init__(self, xmlin1, xsl , xmlin2=None, output=None, mergein=None, mergeout=None):
+        """
+        @Method Class initialiser
+
+        This method initialises the class and takes 7 parameters. The later 4 parameters are optional
+        but if one of the latter needs to be initialised a blank "" needs to be placed to set preceeding 
+        params to None. All arguments should be file names or paths.
+
+        @param xmlin1,   first xml argument
+        @param xsl,      xslt argument
+        @param xmlin2,   second xml argument, Default None
+        @param output,   output argument, Default None
+        @param mergein,  template XSLT for append, Default Merge.xsl.in
+        @param mergeout, destination for edited XSLT for append, Default Merge.xsl
+        """
+        #if xmlin1 == None: raise StandardError("xmlin1 must be defined", "CADImport::__init__")
+        #else:
+        self.XMLIn1 = xmlin1
+        if xsl == None: raise StandardError("xsl must be defined", "CADImport::__init__")
+        else: self.XSL = xsl
+        if xmlin2 == "": self.XMLIn2 = None
+        else: self.XMLIn2 = xmlin2
+        if output == "": self.Output = None
+        else: self.Output = output
+        if mergein == None: self.MergeIn = 'Merge.xsl.in'
+        else: self.MergeIn = mergein
+        if mergeout == None: self.MergeOut = 'Merge.xsl'
+        else: self.MergeOut = mergeout
+        
+    def XSLTParse(self):
+        """
+        @Method XSLTParse, to parse an XML(GDML) to text or another XML(GDML)
+
+        This method will execute an XSLT stylesheet and produce either a text
+        or another XML(GDML) according to the request.
+
+        """
+        if os.path.exists(self.Output) == None: raise StandardError("Output file name not defined", "CADImport::XSLTParse")
+        #outputExists = False
+        styledoc = libxml2.parseFile(self.XSL)
+        style = libxslt.parseStylesheetDoc(styledoc)
+        doc = libxml2.parseFile(self.XMLIn1)
+        result = style.applyStylesheet(doc, None)
+        style.saveResultToFilename(self.Output, result, 0)
+        #outputExists = os.path.exists(output)
+        #if outputExists == False: raise StandardError("GDML not parsed!", "CADImport::XSLTParse")
+        style.freeStylesheet()
+        doc.freeDoc()
+        result.freeDoc()
+
+    def AppendMerge(self):
+        """
+        @Method which appends the MICE info to GDML
+
+        This method reads in Merge.xsl.in, used to append the MICE info onto the end of
+        a config file, and replaces the text EDIT with the name of the file from which the MICE
+        info comes from. Outputs a new file, Merge.xsl, ready to be parse.
+
+        @param Parameter 1 intialised in class intialiser (2nd xml input)
+        """
+        if self.XMLIn2 == None: raise TypeError("Merge input 2 not defined", "CADImport::AppendMerge")
+        found = False
+        fin = open(self.MergeIn, 'r')
+        fout = open(self.MergeOut, 'w')
+        #check Merge.xsl.in for the word EDIT
+        for line in fin.readlines():
+                if line.find('EDIT')>=0:
+                    found = True
+                    new_line = line.replace('EDIT', self.XMLIn2)
+                    print >>fout,new_line
+                else:
+                    print >>fout,line
+        #throw error if Merge.xsl.in has been altered
+        if found == False: raise StandardError("EDIT not found in Merge.xsl.in, this file has been altered", "CADImport::AppendMerge")
+
+
+geometry1 = CADImport("fastradModel.xml", "GDML2G4MICE.xsl", "", "OUTPUTFILE.txt")
+geometry2 = CADImport("fastradModel.xml", "GDML2G4MICE.xsl")
+geometry3 = CADImport("fastradModel.xml", "Merge.xsl", "FieldInfoTest.xml")
+
+geometry1.XSLTParse()
+geometry3.AppendMerge()
---
+++ tests/unit/test_CADImport.py
@@ -0,0 +1,42 @@
+import test_CADImport
+import unittest
+import CADImport
+
+class  test_CADImport(unittest.TestCase):
+    def setUp(self):
+        self.CADImport_one_xml = CADImport.__init__("fastradModel.xml", "GDML2G4MICE.xsl")
+        #self.CADImport_two_xml = CADImport.__init__("fastradModel.xml", "GDML2G4MICE.xsl", "FieldInfoTest.xml")
+
+    def test_initialiser(self):
+        try:
+            CADImport.__init__()
+            assertTrue(False, "Should have raised a StandardError")
+        except StandardError:
+            pass
+
+    def test_XSLTParse(self):
+        try:
+            self.CADImport_one_xml.XSLTParse()
+            self.assertTrue(False, "Should have raised a StandardError")
+        except StandardError:
+            pass
+        except:
+            self.assertTrue(False, "Should have raised a StandardError")
+
+    def test_AppendMerge(self):
+        try:
+            self.CADImport_one_xml.AppendMerge()
+            self.assertTrue(False, "Should have raised a StandardError")
+        except StandardError:
+            pass
+        except:
+            self.assertTrue(False, "Should have raised a StandardError")
+
+
+
+    #def tearDown(self):
+    #    self.foo.dispose()
+    #    self.foo = None
+
+if __name__ == '__main__':
+    unittest.main()
