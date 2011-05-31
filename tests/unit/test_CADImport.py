03f7368493a9cb6b10574c0820dc2252ca0bbcda
Matthew Littlefield <matt@ZMVA>
2011-05-31 10:02:48 +0100

    added some bash script to build libraries for CADImport.py and modified test_CADImport.py and build_all.bash

--- tests/unit/test_CADImport.py
+++ tests/unit/test_CADImport.py
@@ -1,32 +1,36 @@
-import test_CADImport
+import os.path
 import unittest
 from CADImport import CADImport
 
 class  test_CADImport(unittest.TestCase):
     def setUp(self):
         self.CADImport_one_xml = CADImport.__init__("fastradModel.xml", "GDML2G4MICE.xsl")
-        #self.CADImport_two_xml = CADImport.__init__("fastradModel.xml", "GDML2G4MICE.xsl", "FieldInfoTest.xml")
-
-    def test_initialiser(self):
-        try:
-            CADImport.__init__()
-            assertTrue(False, "Should have raised a StandardError")
-        except StandardError:
-            pass
+        self.CAD_Import_two_xml = CADImport("fastradModel.xml", "GDML2G4MICE.xsl", "FieldInfoTest.xml", "~/maus_littlefield/tmp/OUTPUTFILE.txt")
 
     def test_XSLTParse(self):
         try:
-            self.CADImport_one_xml.XSLTParse()
-            self.assertTrue(False, "Should have raised a StandardError")
+            self.CAD_Import_two_xml.XSLTParse()
+            OutputFile = test1.Output
+            if os.path.getsize(OutputFile)== 0: raise StandardError("No output file produced")
         except StandardError:
             pass
         except:
             self.assertTrue(False, "Should have raised a StandardError")
 
     def test_AppendMerge(self):
+        self.CAD_Import_two_xml.AppendMerge()
         try:
-            self.CADImport_one_xml.AppendMerge()
-            self.assertTrue(False, "Should have raised a StandardError")
+            count1 = 0
+            fout = open(self.CAD_Import_two_xml.MergeOut, 'r')
+            for line in fout.readlines():
+                if line.find('EDIT')>=0: count1 = count1 + 1
+            if count1 != 0: raise StandardError("EDIT was not replaced")
+
+            count2 = 0
+            fin = open(self.CAD_Import_two_xml.MergeIn, 'r')
+            for line in fin.readlines():
+                if line.find('EDIT')>=0: count2 = count2 + 1
+            if count2 != 1: raise StandardError("Wihin Merge.xsl, there is no EDIT or there are too many")
         except StandardError:
             pass
         except:
