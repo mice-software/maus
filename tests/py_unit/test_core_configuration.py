cf47c213d5bf0863a8ac4b70027dfe96df12f113
Chris Rogers <chris.rogers@stfc.ac.uk>
2011-06-02 11:55:53 +0100

    Move configuration path from core to common_py

--- tests/py_unit/test_core_configuration.py
+++ tests/py_unit/test_core_configuration.py
@@ -19,7 +19,7 @@ class ConfigurationTestCase(unittest.TestCase):
         self.assertNotEqual(MAUSRootDir,  None)
 
         configDict = {}
-        defaultFilename = '%s/src/core/ConfigurationDefaults.py' % MAUSRootDir
+        defaultFilename = '%s/src/common_py/ConfigurationDefaults.py' % MAUSRootDir
         exec(open(defaultFilename,'r').read(), globals(), configDict)
 
         configJSONStr = json.JSONEncoder().encode(configDict)
