5df5d5b0e0ca44cd85dc890f140d5f2fd2dda639
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2011-06-10 14:54:26 +0100

    Require certain version of validictory: easy_install "validictory==0.7.0"

--- third_party/bash/40python_extras.bash
+++ third_party/bash/40python_extras.bash
@@ -4,6 +4,7 @@ if [ -n "${MAUS_ROOT_DIR+x}" ]; then
 
     easy_install pylint suds validictory nose coverage scons readline ipython doxypy # numpy pychecker matplotlib
     easy_install "numpy==1.5"
+    easy_install "validictory==0.7.0"
     easy_install bitarray
     
     echo "INFO: The package should be locally build now in your"
