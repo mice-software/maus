35b13faef8dde4a01096fe9def127f2be0c212da
Yodan <dancho@mamba>
2011-07-03 23:15:52 +0100

    Simple application for reading the scalers data.

---
+++ src/map/MapPyScalersAnalysis/MapPyScalersAnalysis.py
@@ -0,0 +1,38 @@
+
+import json
+import types
+import os
+import io
+
+
+class MapPyTOFCabling:
+    def birth(self, configJSON):
+        return True
+
+    def process(self, str):
+        spill = json.loads(str)
+
+        if 'daq_data' not in spill:
+            return str
+
+        if spill['daq_data'] is None:
+            return str
+
+        for subdata in spill['daq_data']:
+            if subdata is None:
+                continue
+
+            if 'scaler' in subdata.keys():
+                 print subdata['scaler'].keys()
+                 scalers = spill['scaler']
+
+        return str
+
+    def death(self):
+        return True
+
+
+
+
+
+
