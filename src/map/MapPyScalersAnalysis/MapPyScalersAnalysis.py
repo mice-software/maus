ee3e5e2489e11c322772f129a23a26d836e7ccad
Yodan <dancho@mamba>
2011-07-04 13:33:35 +0100

    first working version of the MapPyScalersAnalysis

--- src/map/MapPyScalersAnalysis/MapPyScalersAnalysis.py
+++ src/map/MapPyScalersAnalysis/MapPyScalersAnalysis.py
@@ -1,4 +1,10 @@
 
+#  MapPyScalersAnalysis class dumps some scaler information.
+#  This a first prototype and for the moment it dumps only Triggers,
+#  Trigger Requests, GVA, TOF0 and TOF1. It has to be extended and to
+#  include also the other channels of the scaler.
+
+
 import json
 import types
 import os
@@ -7,6 +13,12 @@ import io
 
 class MapPyScalersAnalysis:
     def birth(self, configJSON):
+        self._hits = {}
+        self._triggers         = []
+        self._trigger_requests = []
+        self._GVA              = []
+        self._TOF0             = []
+        self._TOF1             = []
         return True
 
     def process(self, str):
@@ -18,13 +30,21 @@ class MapPyScalersAnalysis:
         if spill['daq_data'] is None:
             return str
 
-        for subdata in spill['daq_data']:
-            if subdata is None:
-                continue
+        daq_event = spill['daq_data']
+        if 'V830' not in daq_event:
+            return str
 
-            if 'scaler' in subdata.keys():
-                 print subdata['scaler'].keys()
-                 scalers = spill['scaler']
+        scalers = daq_event['V830']
+        #print scalers
+        if 'channels' not in scalers:
+            return str
+
+        self._hits = scalers['channels']
+        event = scalers['phys_event_number']
+        time = scalers['time_stamp']
+        print 'Event Num  : ', event, '  time : ', time, '\n'
+        if self.add(self._hits) :
+            self.dump()
 
         return str
 
@@ -32,7 +52,54 @@ class MapPyScalersAnalysis:
         return True
 
 
+    def dump(self):
+        if len(self._triggers):
+            print 'triggers ....... : ', self._triggers[-1],
+            print ' (', sum(self._triggers)/len(self._triggers), ')'
 
+        if len(self._trigger_requests):
+            print 'trigger requests : ', self._trigger_requests[-1],
+            print ' (', sum(self._trigger_requests)/len(self._trigger_requests), ')'
+
+        if len(self._GVA):
+            print 'GVA ............ : ', self._GVA[-1],
+            print ' (', sum(self._GVA)/len(self._GVA), ')'
+
+        if len(self._TOF0):
+            print 'TOF0 ........... : ', self._TOF0[-1],
+            print ' (', sum(self._TOF0)/len(self._TOF0), ')'
+
+        if len(self._TOF1):
+            print 'TOF1 ........... : ', self._TOF1[-1],
+            print ' (', sum(self._TOF1)/len(self._TOF1), ')' , '\n\n'
+
+        return True
+
+    def add(self, hits=None):
+        if  'ch0' not in hits:
+            return False
+
+        self._triggers.append( hits['ch0'] );
+        if len(self._triggers) == 11 :
+            self._triggers.pop(0)
+
+        self._trigger_requests.append( hits['ch1'] );
+        if len(self._trigger_requests) == 11 :
+            self._trigger_requests.pop(0)
+
+        self._GVA.append( hits['ch2'] );
+        if len(self._GVA) == 11 :
+            self._GVA.pop(0)
+
+        self._TOF0.append( hits['ch3'] );
+        if len(self._TOF0) == 11 :
+            self._TOF0.pop(0)
+
+        self._TOF1.append( hits['ch4'] );
+        if len(self._TOF1) == 11 :
+            self._TOF1.pop(0)
+
+        return True
 
 
 
