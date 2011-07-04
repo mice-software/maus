048f71faed3bf7e988109502c930b9250a942e56
Yodan <dancho@mamba>
2011-07-04 13:34:28 +0100

    test for  MapPyScalersAnalysis

---
+++ src/map/MapPyScalersAnalysis/test_MapPyScalersAnalysis.py
@@ -0,0 +1,45 @@
+import os
+import json
+import types
+import unittest
+
+from Configuration import Configuration
+
+import MAUS
+
+class MapPyScalersAnalysisTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        if not os.environ.get("MAUS_ROOT_DIR"):
+            raise Exception('InitializeFail', 'MAUS_ROOT_DIR unset!')
+        # Set our data path & filename
+        # It would be nicer to test with a smaller data file!
+        self._datapath = '%s/src/input/InputCppRealData' % \
+                            os.environ.get("MAUS_ROOT_DIR")
+        self._datafile = '02873'
+        self._c = Configuration()
+
+    def test_something(self):
+        inputter = MAUS.InputCppRealData(self._datapath, self._datafile)
+        inputter.birth( self._c.getConfigJSON() )
+
+        mapper = MAUS.MapPyScalersAnalysis()
+        success = mapper.birth("{}")
+        self.assertTrue(success)
+
+        success = mapper.add("")
+        self.assertFalse(success)
+
+        success = mapper.add("{}")
+        self.assertFalse(success)
+
+        success = mapper.dump()
+        self.assertTrue(success)
+
+        result = mapper.process("{}")
+        self.assertEqual(len(result), 2)
+
+        mapper.death()
+
+if __name__ == '__main__':
+    unittest.main()
