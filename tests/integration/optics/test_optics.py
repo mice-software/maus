c02c436d44b0b48197d9b47119220d43ea29fcf9
Chris Rogers <chris.rogers@stfc.ac.uk>
2011-08-15 16:00:46 +0100

    automatically check optics ouput

--- tests/integration/optics/test_optics.py
+++ tests/integration/optics/test_optics.py
@@ -1,15 +1,71 @@
 """
-Simple script to excercise the optics application. Don't test the output
-automatically - but we should return errors from RF, 6D, polyfit; mapping should
-return a beta trans that is nearly periodic; optimiser should return a beta
-trans that is almost exactly periodic.
+Simple script to excercise the optics application and automatically test output
+transverse beta functions.
 """
 
 import os
+import unittest
+import xboa.Common as Common
 
-os.chdir(os.getenv('MAUS_ROOT_DIR')+'/tests/integration/optics')
-for test in ['mapping', 'optimiser', 'rf', '6d', 'polyfit']:
+TEST_NAMES = ['mapping', 'optimiser'] #, 'rf', '6d', 'polyfit']
+TARGET_BETA = {'mapping':0.655994338, 'optimiser':0.652749967}
+
+def run_optics(test):
+    """
+    Runs optics application and produces some output
+    """
+    os.chdir(os.getenv('MAUS_ROOT_DIR')+'/tests/integration/optics')
     run_command = './optics files/cards.'+test+' >& log.'+test
     print run_command
     os.system(run_command)
+
+def get_data(test):
+    """
+    Extracts data from optics output
+    """
+    z_out = []
+    beta = []
+    fin = open('envelope-'+test+'.txt')
+    line = ''
+    for index in range(8): # pylint: disable=W0612
+        line = fin.readline()
+    while line != '':
+        words = line.split()
+        z_out.append(float(words[6]))
+        beta.append(float(words[11])/1000.)
+        for i in range(7): # pylint: disable=W0612
+            line = fin.readline()
+    return (z_out, beta)
+
+def plot_data(test, z_out, beta):
+    """
+    Plots optics data
+    """
+    canvas = Common.make_root_canvas(test)
+    test_heading = test
+    (hist, graph) = Common.make_root_graph(test_heading, z_out, 'z [m]', \
+                                           beta, 'beta [m]')
+    hist.Draw()
+    graph.Draw()
+    canvas.Update()
+    canvas.Print(test+'_optics.png')
+
+class TestOptics(unittest.TestCase): # pylint: disable=R0904
+    """
+    Run optics for a few options
+    """
+
+    def test_optics(self):
+        """
+        Check that we can calculate beta function correctly; plot output beta
+        function.
+        """
+        for test in TEST_NAMES:
+            run_optics(test)
+            (z_out, beta) = get_data(test)
+            plot_data(test, z_out, beta)
+            self.assertAlmostEqual(TARGET_BETA[test], beta[-1], 3)
+
+if __name__ == '__main__':
+    unittest.main()
 
