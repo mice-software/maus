65e56b2e9e41c252a36f411251ae0ae7d16148bf
Matt <matt@Matts-laptop>
2011-08-23 10:16:32 +0100

    written unit tests for GDMLPacker and it now passes pylint ready to publish

--- src/common_py/geometry/GDMLPacker.py
+++ src/common_py/geometry/GDMLPacker.py
@@ -6,10 +6,12 @@ import os.path
 import zipfile
 from datetime import datetime
 
-# need to re write comments and tests as it has been changed
+#pylint: disable = R0903
+
 class Packer:
-    #pylint: disable = R0903
     """
+    @Class Packer This class packs the GDMLs into a zip file.
+    
     This class collects the files needed to be uploaded and zips them.
     """
     def __init__(self, filelist):
@@ -20,7 +22,7 @@ class Packer:
         The parameter asks for the file name/path of a text file. This text file must contain
         the file names and paths of the GDML files which need to be zipped.
 
-        @Param filelist, file name/path of the file containing the list of GDML files to be zipped.
+        @Param filelist file name/path of the file containing the list of GDML files to be zipped.
         """
         alist = []
         self.list = alist
@@ -38,54 +40,73 @@ class Packer:
 
     def zipfile(self, path):
         """
-        @Method zipfile, takes the collated file, containing all the GDMLs, and zips it.
+        @Method zipfile takes the collated file, containing all the GDMLs, and zips it.
 
         This method zips the GDML files and a text file with a list of files present. An argument of the
         output file is specified. Default is $maus/src/common_py/geometry/zippedGeoms. This method
         also names the zip file as the time and date when method is called.
 
-        @Param Output Path, specify the output path of the zipfile
+        @Param Output Path specify the output path of the zipfile
         """
-        timenow = str(datetime.today())
-        timenow = timenow.replace('', 'Year', 1)
-        timenow = timenow.replace('-', 'Mon', 1)
-        timenow = timenow.replace('-', 'Day', 1)
-        timenow = timenow.replace(' ', 'Hour', 1)
-        timenow = timenow.replace(':', 'Min', 1)
-        end = timenow.find(':')
-        timenow = timenow[0:end]
-        zippath = path + "/" + timenow +".zip"
-        zfile = zipfile.ZipFile(zippath, 'w')
-        fin = open(self.filelist, 'r')
-        numoffiles = len(fin.readlines())
-        for num in range(0, numoffiles):
-            geomlist = self.list[num]
-            base = os.path.basename(self.list[num])
-            zfile.write(geomlist, base, zipfile.ZIP_DEFLATED)
-        base = os.path.basename(self.filelist)
-        zfile.write(self.filelist, base, zipfile.ZIP_DEFLATED)
-        zfile.close()
-        fin.close()
-        return zippath
-    
+        if os.path.exists(path) == False:
+            raise IOError('Path does not exist!', 'Packer::zipfile')
+        else:
+            timenow = str(datetime.today())
+            timenow = timenow.replace('', 'Year', 1)
+            timenow = timenow.replace('-', 'Mon', 1)
+            timenow = timenow.replace('-', 'Day', 1)
+            timenow = timenow.replace(' ', 'Hour', 1)
+            timenow = timenow.replace(':', 'Min', 1)
+            end = timenow.find(':')
+            timenow = timenow[0:end]
+            zippath = path + "/" + timenow +".zip"
+            zfile = zipfile.ZipFile(zippath, 'w')
+            fin = open(self.filelist, 'r')
+            numoffiles = len(fin.readlines())
+            for num in range(0, numoffiles):
+                geomlist = self.list[num]
+                base = os.path.basename(self.list[num])
+                zfile.write(geomlist, base, zipfile.ZIP_DEFLATED)
+            base = os.path.basename(self.filelist)
+            zfile.write(self.filelist, base, zipfile.ZIP_DEFLATED)
+            zfile.close()
+            fin.close()
+            return zippath
+        
 class Unpacker:
-    def __init__(self, file, extract_path):
-        #may need a little test
-        self.file = file
-        self.extract_path = extract_path
-    
+    """
+    @class Unpacker This class unzips zip files.
+    """
+    def __init__(self, input_file, extract_path):
+        """
+        @method Class Constructor This is the class constructor
+        
+        The class constructor takes two arguments. The first is the name
+        of the file which is to be unzipped. The second is the location where this
+        file will be unzipped too.
+        
+        @param zfile: Name of the file to be unzipped
+        @param extraction path: Name of the folder where the information will be unzipped.
+        """
+        if input_file[-4:] != '.zip':
+            raise IOError('File is not a zip file!', 'Unpacker::__init__')
+        elif os.path.exists(extract_path) == False:
+            raise IOError('Path does not exist!', 'Unpacker::__init__')
+        else:    
+            self.input_file = input_file
+            self.extract_path = extract_path
+        
     def unzip_file(self):
-        zfile = zipfile.ZipFile(self.file)
+        """
+        @method unzip file This method unzips the zip file.
+        """
+        zfile = zipfile.ZipFile(self.input_file)
         zfile.extractall(self.extract_path)
 
 def main():
     """
     Main Function
     """
-    test = "/home/matt/workspace/Maus/testCases/GDML_fastradModel/FileList.txt"
-    filetozip = Packer(test)
-    path = '/home/matt/maus-littlefield/src/common_py/geometry/zippedGeoms'
-    filetozip.zipfile(path)
 
 if __name__ == "__main__":
     main()
--- tests/py_unit/test_GDMLPacker.py
+++ tests/py_unit/test_GDMLPacker.py
@@ -2,9 +2,10 @@ import os
 import os.path
 import unittest
 from geometry.GDMLPacker import Packer
+from geometry.GDMLPacker import Unpacker
 
 
-class  Test_GDMLPackerTestCase(unittest.TestCase):
+class  Test_Packer(unittest.TestCase):
     def setUp(self):
         self.constructor_test = None
         testcase = os.environ['MAUS_ROOT_DIR'] + '/src/common_py/geometry/testCases/testPacker/FileList.txt'
@@ -15,14 +16,14 @@ class  Test_GDMLPackerTestCase(unittest.TestCase):
         this test will check the constructor raises appropriate errors.
         """
         try:
-            self.constructor_test = packer("geometry.not_txt")
+            self.constructor_test = Packer("geometry.not_txt")
             self.asserttrue(False, "should have raised an exception, test_GDMLPacker::test_constructor")
         except:
             pass
 
         try:
-            blank_txt = os.environ['MAUS_ROOT_DIR'] + '/src/common_py/geometry/testCases/testPacker'
-            self.constructor_test = packer(blank_txt)
+            blank_txt = os.environ['MAUS_ROOT_DIR'] + '/src/common_py/geometry/testCases/testPacker/test.txt'
+            self.constructor_test = Packer(blank_txt)
             self.assertTrue(False, "should have raised an exception, test_GDMLPacker::test_constructor")
         except:
             pass
@@ -38,10 +39,51 @@ class  Test_GDMLPackerTestCase(unittest.TestCase):
             if fname[-4:] == '.zip':
                 zfile = zippath + '/' + fname
         self.assertTrue(os.path.getsize(zfile) != 0, "zipped file size is zero, test_GDMLPacker::test_zipfile")
-        os.remove(zfile)
 
     def tearDown(self):
         self.GDML_test_case = None
+        
+class Test_Unpacker(unittest.TestCase):
+    
+    def setUp(self):
+        self.constructor_test = None
+        zippath = os.environ['MAUS_ROOT_DIR'] + '/src/common_py/geometry/testCases/testPacker'
+        output = os.listdir(zippath)
+        for fname in output:
+            if fname[-4:] == '.zip':
+                zfile = zippath + '/' + fname
+        testpath = os.environ['MAUS_ROOT_DIR'] + '/src/common_py/geometry/testCases'
+        self.extraction_test = Unpacker(zfile, testpath)
+        
+    def test_constructor(self):
+        try:
+            self.constructor_test = Unpacker('Geometry.not_zip', testpath)
+            self.assertTrue(False, 'Should have raised an error, Test_Unpacker::test_constructor')
+        except:
+            pass
+        
+        try: 
+            self.constructor_test = Unpacker(testcase, 'Not_a_path')
+            self.assertTrue(False, 'Should have raised an error, Test_Unpacker::test_constructor')
+        except:
+            pass
+        
+    def test_unzip_file(self):
+        self.extraction_test.unzip_file()
+        output = os.listdir(self.extraction_test.extract_path)
+        for fname in output:
+            if fname != 'FileList.txt' and fname[-5:] == '.gdml':
+                fname = self.extraction_test.extract_path + '/' + fname
+                os.remove(fname)
+            if fname != 'FileList.txt' and fname[-4:] == '.xml':
+                fname = self.extraction_test.extract_path + '/' + fname
+                os.remove(fname)
+            if fname == 'FileList.txt':
+                fname = self.extraction_test.extract_path + '/' + fname
+                text_file = fname
+        self.assertEqual(text_file, os.environ['MAUS_ROOT_DIR'] + '/src/common_py/geometry/testCases/FileList.txt', 'File not unzipped, Test_Unpakcer::test_unzip_file')
+        os.remove(self.extraction_test.input_file)
+        os.remove(text_file)         
 
 if __name__ == '__main__':
     unittest.main()
