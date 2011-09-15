685e027792c9712298d51c2ac313cc8404511bc2
Matt <matt@Matts-laptop>
2011-09-15 15:16:47 +0100

    hopefully fixing build errors

--- third_party/build_all.bash
+++ third_party/build_all.bash
@@ -36,12 +36,9 @@ if [ -n "${MAUS_ROOT_DIR+x}" ]; then
     ${MAUS_ROOT_DIR}/third_party/bash/51xboas.bash
     ${MAUS_ROOT_DIR}/third_party/bash/52jsoncpp.bash
     ${MAUS_ROOT_DIR}/third_party/bash/53unpacking.bash
-<<<<<<< TREE
     ${MAUS_ROOT_DIR}/third_party/bash/xxlibxml2.bash
     ${MAUS_ROOT_DIR}/third_party/bash/xxlibxslt.bash
-=======
     ${MAUS_ROOT_DIR}/third_party/bash/60recpack.bash
->>>>>>> MERGE-SOURCE
 
 
 else
