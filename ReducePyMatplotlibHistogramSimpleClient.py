601cd57e9da6c0ea0a628e39322195cb3d105f77
Mike Jackson <michaelj@epcc.ed.ac.uk>
2011-09-22 13:05:26 +0100

    Replaced use of filter with list comprehension. Addressed pylint errors.

--- ReducePyMatplotlibHistogramSimpleClient.py
+++ ReducePyMatplotlibHistogramSimpleClient.py
@@ -1,26 +1,33 @@
 #!/usr/bin/env python
+"""Simple example showing use of ReducePyMatplotlibHistogram. """
 
 from Configuration import Configuration
-import io
 import json
 import MAUS
 
-# Build JSON docs.
-jsonStrs = []
-for i in range(1, 3):
-    jsonDoc = {}
-    jsonDoc["digits"] = []
-    for j in range(1, 10):
-#        digit = {"ac_counts":i * j,"tc_counts":i * j * 2}
-        digit = {"adc_counts":i * j,"channel_id":{"fiber_number":106,"plane_number":2,"station_number":1,"tracker_number":0,"type":"Tracker"},"tdc_counts":i * j * 2}
-        jsonDoc["digits"].append(digit)
-    jsonStrs.append(json.dumps(jsonDoc))
+def run():
+    """Create a JSON document and create a histogram."""
+    # Build JSON docs.
+    json_strs = []
+    for i in range(1, 3):
+        json_doc = {}
+        json_doc["digits"] = []
+        for j in range(1, 10):
+#            digit = {"ac_counts":i * j,"tc_counts":i * j * 2}
+            digit = {"adc_counts":i * j,
+                     "channel_id":{"fiber_number":106,
+                     "plane_number":2, "station_number":1,
+                     "tracker_number":0, "type":"Tracker"},
+                     "tdc_counts":i * j * 2}
+            json_doc["digits"].append(digit)
+        json_strs.append(json.dumps(json_doc))
 
-reducer = MAUS.ReducePyMatplotlibHistogram()
-configJson = Configuration()
-reducer.birth(configJson.getConfigJSON())
+    reducer = MAUS.ReducePyMatplotlibHistogram()
+    config_json = Configuration()
+    reducer.birth(config_json.getConfigJSON())
+    for json_str in json_strs:
+        print reducer.process(json_str, json_str)
+    reducer.death()
 
-for jsonStr in jsonStrs:
-    print reducer.process(jsonStr, jsonStr)
-
-reducer.death()
+if __name__ == '__main__':
+    run()
--- src/reduce/ReducePyMatplotlibHistogram/ReducePyMatplotlibHistogram.py
+++ src/reduce/ReducePyMatplotlibHistogram/ReducePyMatplotlibHistogram.py
@@ -1,3 +1,6 @@
+"""
+PyMatplotlibHistogram creates histograms of data within spills.
+"""
 #  This file is part of MAUS: http://micewww.pp.rl.ac.uk:8080/projects/maus
 # 
 #  MAUS is free software: you can redistribute it and/or modify
@@ -13,106 +16,148 @@
 #  You should have received a copy of the GNU General Public License
 #  along with MAUS.  If not, see <http://www.gnu.org/licenses/>.
 
-## @class ReducePyMatplotlibHistogram.PyMatplotlibHistogram
-# PyMatplotlibHistogram creates histograms of data within spills.
-#
-# PyMatplotlibHistogram extracts the tdc_counts and adc_counts for
-# each digit within a spill and plots these. The histogram is saved
-# in the current directory with either a user-specified or UUID
-# prefix. The user can specify the image file type - conditional
-# on those supported by matplot lib (currently 
-# 'svg', 'ps', 'emf', 'rgba', 'raw', 'svgz', 'pdf', 'eps', 'png').
-# 
-# The output JSON document is their concatenation with a line break
-# between them.
-
 import datetime
 import json
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
-import matplotlib.mlab as mlab
 import os
 import uuid
 
 class ReducePyMatplotlibHistogram:
+    """
+    @class ReducePyMatplotlibHistogram.PyMatplotlibHistogram
+    PyMatplotlibHistogram creates histograms of data within spills.
 
-    def birth(self, configJson):
-        configDoc = json.loads(configJson)
-	# Create a FigureCanvas so we can validate the image type.
+    It extracts the tdc_counts and adc_counts for each digit within a
+    spill and plots these, then saves the histogram.
+
+    The caller can configure the reducer and specify.
+    -Image type ("histogram_image_type"). Must be one of those
+     supported by matplot lib (currently 'svg', 'ps', 'emf', 'rgba',
+     'raw', 'svgz', 'pdf', 'eps', 'png'). Default: 'eps'.
+    -File prefix ("histogram_file_prefix"). Default: auto-generated UUID.
+    -Directory for files ("histogram_directory"). Default: current
+     working directory. If the given directory does not exist it
+     will be created.
+    The output is a string of JSON documents read to date and
+    separated by line breaks.
+    """
+
+    def __init__(self):
+        self._image_type = "eps"
+        self._file_prefix = ""
+        self._directory = os.getcwd()
+        self._file_count = 0
+
+    def birth(self, config_json):
+        """
+        Configure reducer from data cards.
+        @param self Object reference.
+        @param config_json JSON document string.
+        @returns True
+        """
+        config_doc = json.loads(config_json)
+        # Create a FigureCanvas so we can validate the image type.
         figure = Figure()
         canvas = FigureCanvas(figure)
 
         key = "histogram_image_type"
-        if key in configDoc:
-            self._imageType = configDoc[key]
-	else:
-            self._imageType = "eps"
-        print "Histogram image type: %s" % self._imageType
+        if key in config_doc:
+            self._image_type = config_doc[key]
+        else:
+            self._image_type = "eps"
+        print "Histogram image type: %s" % self._image_type
 
-        if self._imageType not in canvas.get_supported_filetypes().keys():
+        if self._image_type not in canvas.get_supported_filetypes().keys():
             error = "Unsupported histogram image type: %s Expect one of %s" % (
-                    self._imageType, 
+                    self._image_type, 
                     canvas.get_supported_filetypes().keys())
             print error
             raise ValueError(error)
 
         key = "histogram_file_prefix"
-        if key in configDoc:
-            self._filePrefix = configDoc[key]
-	else:
-            self._filePrefix = uuid.uuid4()
-        print "Histogram file prefix: %s" % self._filePrefix
+        if key in config_doc:
+            self._file_prefix = config_doc[key]
+        else:
+            self._file_prefix = uuid.uuid4()
+        print "Histogram file prefix: %s" % self._file_prefix
 
         key = "histogram_directory"
-        if key in configDoc:
-            self._directory = configDoc[key]
+        if key in config_doc:
+            self._directory = config_doc[key]
             if not os.path.exists(self._directory): 
                 os.makedirs(self._directory) 
-	else:
+        else:
             self._directory = os.getcwd()
         print "Histogram directory: %s" % self._directory
         # Number of files saved so far.
-        self._fileCount = 0
+        self._file_count = 0
         return True
 
-    def process(self, ignore, jsonString):
+    def process(self, ignore, json_string):
+        """
+        Create histogram from data.
+        @param self Object reference.
+        @param ignore String with JSON documents.
+        @param json_string String with current JSON document.
+        @returns String of JSON documents with a line break and the 
+        current one appended.
+        """
         try:
-            jsonDoc = json.loads(jsonString.rstrip())
+            json_doc = json.loads(json_string.rstrip())
         except ValueError:
             print "Bad JSON document"
-            jsonDoc = {"errors": {"bad_json_document":
+            json_doc = {"errors": {"bad_json_document":
                                 "unable to do json.loads on input"} }
-            return self.returnJSON(ignore, json.dumps(jsonDoc))        
-        if "digits" not in jsonDoc:
+            return self.return_json(ignore, json.dumps(json_doc))        
+        if "digits" not in json_doc:
             print "No digits in JSON document"
-            if 'errors' not in jsonDoc:
-                jsonDoc['errors'] = {}
-            jsonDoc['errors']['no digits'] = 'no digits'
-            return self.returnJSON(ignore, json.dumps(jsonDoc))
-        digits = jsonDoc['digits']
+            if 'errors' not in json_doc:
+                json_doc['errors'] = {}
+            json_doc['errors']['no digits'] = 'no digits'
+            return self.return_json(ignore, json.dumps(json_doc))
+        digits = json_doc['digits']
         print "Number of digits: %d" % len(digits)
         # TODO alternatively filter and TDC/ADC count extraction in one
         # list traversal? Otherwise, digit index may get out of synch.
-        trackerdigits = filter(self.filtertrackers, digits)
+        trackerdigits = \
+            [digit for digit in digits if self.filter_trackers(digit)]
         print "Number of tracker digits: %d" % len(trackerdigits)
-	tdc_counts = [self.get_counts(digit, 'tdc_counts') 
+        tdc_counts = [self.get_counts(digit, 'tdc_counts') 
                       for digit in trackerdigits]
         print "tdc_counts: %s" % tdc_counts
-	adc_counts = [self.get_counts(digit, 'adc_counts') 
+        adc_counts = [self.get_counts(digit, 'adc_counts') 
                       for digit in trackerdigits]
         print "adc_counts: %s" % adc_counts
         canvas = self.histogram(tdc_counts, adc_counts)
-        self.saveHistogram(canvas)
-        return self.returnJSON(ignore, jsonString)
+        self.save_histogram(canvas)
+        return self.return_json(ignore, json_string)
 
     def death(self):
+        """
+        A no-op
+        @returns True
+        """
         return True
 
-    def returnJSON(self, jsonStrings, jsonString):
-	# Replicate ReducePyDoNothing for now.
-        return "%s\n%s" % (jsonStrings.rstrip(), jsonString.rstrip())
+    def return_json(self, json_strings, json_string):
+        """
+        Add JSON document to list of documents
+        @param json_strings String with JSON documents.
+        @param json_string String with current JSON document.
+        @returns String of JSON documents with a line break and the 
+        current one appended.
+        """
+        # Replicate ReducePyDoNothing for now.
+        return "%s\n%s" % (json_strings.rstrip(), json_string.rstrip())
 
-    def filtertrackers(self, digit):
+    def filter_trackers(self, digit):
+        """
+        Is the digit a tracker?
+        @param self Object reference.
+        @param digit "digits" list member (a dictionary).
+        @returns True if digit is a Tracker, False otherwise.
+        """
         if "channel_id" not in digit:
             return False
         if "type" not in digit["channel_id"]:
@@ -120,13 +165,27 @@ class ReducePyMatplotlibHistogram:
         else:
             return digit['channel_id']['type'] == 'Tracker'
 
-    def get_counts(self, digit, type):
-        if type in digit:
-            return digit[type]
+    def get_counts(self, digit, digit_key):
+        """
+        Return number of counts of the given type in digit.
+        @param self Object reference.
+        @param digit "digits" list member (a dictionary).
+        @param digit_type Either "adc_counts" or "tdc_counts".
+        @returns Count
+        """
+        if digit_key in digit:
+            return digit[digit_key]
         else:
             return 0
 
     def histogram(self, tdc_counts, adc_counts): 
+        """
+        Create a histogram with the given TDC and ADC counts.
+        @param self Object reference.
+        @param tdc_counts List of TDC counts.
+        @param adc_counts List of ADC counts.
+        @returns matplotlib FigureCanvas with histogram.
+        """
         print "About to histogram..."
         figure = Figure(figsize=(6, 6))
         canvas = FigureCanvas(figure)
@@ -134,29 +193,36 @@ class ReducePyMatplotlibHistogram:
         axes.set_title('TDC and ADC counts', fontsize=14)
         axes.set_xlabel('Digit', fontsize=12)
         axes.set_ylabel('Count', fontsize=12)
-        axes.grid(True,linestyle='-', color='0.75')
+        axes.grid(True, linestyle='-', color='0.75')
 #        if (len(tdc_counts) > 0):
-#            axes.scatter(range(0, len(tdc_counts)), tdc_counts, 10, 'r', label='TDC counts')
+#            axes.scatter(range(0, len(tdc_counts)), tdc_counts, 10, \
+#                'r', label='TDC counts')
 #        if (len(adc_counts) > 0):
-#            axes.scatter(range(0, len(adc_counts)), adc_counts, 10, 'b', label='ADC counts')        
+#            axes.scatter(range(0, len(adc_counts)), adc_counts, 10, \
+#                'b', label='ADC counts')        
         axes.plot(tdc_counts, 'r', label='TDC counts') 
         axes.plot(adc_counts, 'b', label='ADC counts') 
         axes.legend()
         print "Histogrammed"
         return canvas
 
-    def saveHistogram(self, canvas):
+    def save_histogram(self, canvas):
+        """
+        Save histogram.
+        @param self Object reference.
+        @param canvas matplotlib FigureCanvas with histogram.
+        """
         # Create a unique file name based on the prefix, current date
         # and the count. Use the date since strftime doesn't (yet)
         # support milli-seconds.
-        fileName = "%s%s%d.%s" % (
-            self._filePrefix, 
+        file_name = "%s%s%d.%s" % (
+            self._file_prefix, 
             datetime.datetime.now().strftime("%Y%m%d%H%M%S"), 
-            self._fileCount,
-            self._imageType)
-        file = os.path.join(self._directory, fileName)
-        print "Saving in " + file
+            self._file_count,
+            self._image_type)
+        file_path = os.path.join(self._directory, file_name)
+        print "Saving in " + file_path
         # File extension determines format. Default if omitted is .png.
-        canvas.print_figure(file, dpi=500)
-        self._fileCount += 1
+        canvas.print_figure(file_path, dpi=500)
+        self._file_count += 1
         print "Saved!"
