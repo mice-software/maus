6d729992d5b4067ec4a4abb7c1a3be936ebe120b
Chris Rogers <chris.rogers@stfc.ac.uk>
2011-10-05 10:15:05 +0100

    Reduce number of style fails

--- tests/style/test_python_style.py
+++ tests/style/test_python_style.py
@@ -63,7 +63,7 @@ class TestPythonStyle(unittest.TestCase): # pylint: disable=R0904
         counting the number of lines in the pylint summary file. If this
         increases, throws an error.
         """
-        current_n_python_errors = 984 # Rogers, 29Sept2011
+        current_n_python_errors = 902 # Rogers, 29Sept2011
         file_out = os.path.join(self.maus_root_dir, 'tmp', 'pylint.out')
         fout = open(file_out, 'w')
         error_files = []
