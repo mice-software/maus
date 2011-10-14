d8c7e32bda5f0e64455ff8d1259777fe7c752067
Chris Rogers <chris.rogers@stfc.ac.uk>
2011-10-14 10:22:20 +0100

    install script modified again

--- tests/integration/install_libxml2_then_test_it.bash
+++ tests/integration/install_libxml2_then_test_it.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 ./configure
 source env.sh
-#./third_party/bash/01python.bash
-#./third_party/bash/42libxml2.bash
+./third_party/bash/01python.bash
+./third_party/bash/42libxml2.bash
 source env.sh
 python -m libxml2
 if [ $? == 0 ]; then
