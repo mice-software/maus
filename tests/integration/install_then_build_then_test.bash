9c50f47e15f0c235cdd06c5e6d88f359908822a3
Chris Rogers <chris.rogers@stfc.ac.uk>
2011-10-14 10:46:31 +0100

    add libxslt; add libxml2 checks to install_then_build_then_test.bash

--- tests/integration/install_then_build_then_test.bash
+++ tests/integration/install_then_build_then_test.bash
@@ -6,4 +6,36 @@ source env.sh
 scons -c
 scons build || exit 1
 ./run_tests.bash || exit 1
+# see if libxml2 imports okay
+python -m libxml2
+if [ $? == 0 ]; then
+    echo "LIBXML2 IMPORT SUCCESSFUL"
+else
+    echo "LIBXML2 IMPORT FAILED"
+fi
+
+# just another way of doing it - just to check
+echo import libxml2 | python
+if [ $? == 0 ]; then
+    echo "LIBXML2 IMPORT SUCCESSFUL"
+else
+    echo "LIBXML2 IMPORT FAILED"
+fi
+
+# see if libxslt imports okay
+python -m libxslt
+if [ $? == 0 ]; then
+    echo "LIBXML2 IMPORT SUCCESSFUL"
+else
+    echo "LIBXML2 IMPORT FAILED"
+fi
+
+
+# just another way of doing it - just to check
+echo import libxslt | python
+if [ $? == 0 ]; then
+    echo "LIBXML2 IMPORT SUCCESSFUL"
+else
+    echo "LIBXML2 IMPORT FAILED"
+fi
 
