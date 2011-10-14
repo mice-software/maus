78db75f10ac3cde0be1178be821527bd95661a41
Chris Rogers <chris.rogers@stfc.ac.uk>
2011-10-14 10:22:10 +0100

    install script modified

--- tests/integration/install_libxml2_then_test_it.bash
+++ tests/integration/install_libxml2_then_test_it.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 ./configure
 source env.sh
-./third_party/bash/01python.bash
-./third_party/bash/42libxml2.bash
+#./third_party/bash/01python.bash
+#./third_party/bash/42libxml2.bash
 source env.sh
-python -m import libxml2
-if [ $? == 0 ] then
+python -m libxml2
+if [ $? == 0 ]; then
     echo "LIBXML2 IMPORT SUCCESSFUL"
 else
     echo "LIBXML2 IMPORT FAILED"
