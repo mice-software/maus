b7fc7c3423a8ef6f2a0129b30fc003e8d399afec
Chris Rogers <chris.rogers@stfc.ac.uk>
2011-10-21 15:10:57 +0100

    Remove MapPyTOFPlot for now; pylint fix for test_InputCppDAQData

--- src/input/InputCppDAQData/test_InputCppDAQData.py
+++ src/input/InputCppDAQData/test_InputCppDAQData.py
@@ -1,70 +1,89 @@
+# This file is part of MAUS: http://micewww.pp.rl.ac.uk:8080/projects/maus
+#
+# MAUS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MAUS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MAUS.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Tests for InputCppDAQData"""
+
+# pylint: disable = C0103
+
 import os
 import md5
 import unittest
-import json
 from Configuration import Configuration
 from InputCppDAQData import InputCppDAQData
 
-class InputCppDAQDataTestCase(unittest.TestCase):
+class InputCppDAQDataTestCase(unittest.TestCase): # pylint: disable = R0904
+    """Tests for InputCppDAQData"""
 
     @classmethod
-    def setUpClass(self):
+    def setUpClass(cls): # pylint: disable = C0103
+        """Set up location for data file"""
         if not os.environ.get("MAUS_ROOT_DIR"):
             raise Exception('InitializeFail', 'MAUS_ROOT_DIR unset!')
         # Set our data path & filename
         # It would be nicer to test with a smaller data file!
-        self._datapath = '%s/src/input/InputCppDAQData' % \
+        cls._datapath = '%s/src/input/InputCppDAQData' % \
                             os.environ.get("MAUS_ROOT_DIR")
-        self._datafile = '02873'
-        self._c = Configuration()
+        cls._datafile = '02873'
+        cls._c = Configuration()
+        cls.input = None
 
     def test_init(self):
-        self.mapper = InputCppDAQData(self._datapath, \
+        """
+        Set up location for data file; check we can initialise, birth and death
+        properly
+        """
+        self.input = InputCppDAQData(self._datapath, \
                                        self._datafile)
-        self.assertTrue(self.mapper.birth( self._c.getConfigJSON() ))
+        self.assertTrue(self.input.birth( self._c.getConfigJSON() ))
         # Check re-init without closing fails
-        self.assertFalse(self.mapper.birth( self._c.getConfigJSON() ))
-        self.assertTrue(self.mapper.death())
+        self.assertFalse(self.input.birth( self._c.getConfigJSON() ))
+        self.assertTrue(self.input.death())
         return
 
     # Test a single event
     def test_single(self):
-        self.mapper = InputCppDAQData(self._datapath, \
+        """Test for single input datum"""
+        self.input = InputCppDAQData(self._datapath, \
                                        self._datafile)
-        self.assertTrue(self.mapper.birth(self. _c.getConfigJSON() ))
+        self.assertTrue(self.input.birth(self. _c.getConfigJSON() ))
         # Get a single event and check it's the right size
-        self.assertTrue(self.mapper.readNextEvent())
-        data = self.mapper.getCurEvent()
+        self.assertTrue(self.input.readNextEvent())
+        data = self.input.getCurEvent()
         # Data shold be 66 (an empty spill, first event is start of burst)
         self.assertEqual(len(data), 65)
-        self.assertTrue(self.mapper.death())
+        self.assertTrue(self.input.death())
         return
 
     # Test reading the whole file
     def test_multi(self):
-        self.mapper = InputCppDAQData(self._datapath, \
+        """Test for multiple input data"""
+        self.input = InputCppDAQData(self._datapath, \
                                        self._datafile)
-        self.assertTrue(self.mapper.birth( self._c.getConfigJSON() ))
+        self.assertTrue(self.input.birth( self._c.getConfigJSON() ))
         event_count = 0
-
         # We can try md5'ing the whole dataset
         digester = md5.new()
-
-        for i in self.mapper.emitter():
+        for i in self.input.emitter():
             digester.update(i)
             event_count = event_count + 1
-
         # We should now have processed 26 events
         self.assertEqual(event_count, 26)
-
         # Check the md5 sum matches the expected value
-        self.assertEqual(digester.hexdigest(), '6d925662f1844ff0198d7e810bbe9f0c')
-
-        self.assertTrue(self.mapper.death())
-
-    @classmethod
-    def tearDownClass(self):
-        pass
+        self.assertEqual(digester.hexdigest(),
+                         '6d925662f1844ff0198d7e810bbe9f0c')
+        self.assertTrue(self.input.death())
 
 if __name__ == '__main__':
     unittest.main()
