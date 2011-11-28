23807e381fa784d52c6350d67afe06abb3cd1786
Chris Rogers <chris.rogers@stfc.ac.uk>
2011-11-28 06:05:00 +0000

    Pylint errors

--- tests/integration/test_utilities/test_geometry/sim_config.py
+++ tests/integration/test_utilities/test_geometry/sim_config.py
@@ -1,26 +1,26 @@
 import os
-simulation_geometry_filename = os.environ['MAUS_ROOT_DIR']+'/tmp/geometry_download_test/ParentGeometryFile.dat'
+simulation_geometry_filename = os.environ['MAUS_ROOT_DIR']+\
+                            '/tmp/geometry_download_test/ParentGeometryFile.dat'
 
 beam = {
-    "particle_generator":"counter", # routine for generating empty primaries
+    "particle_generator":"counter",
     "random_seed":1,
     "definitions":[
-    ##### MUONS #######
     {
        "reference":{
           "position":{"x":0.0, "y":-0.0, "z":-5500.0},
           "momentum":{"x":0.0, "y":0.0, "z":1.0},
           "particle_id":-13, "energy":226.0, "time":0.0, "random_seed":10
-        }, # reference particle
-       "random_seed_algorithm":"incrementing_random", # algorithm for seeding MC
-       "n_particles_per_spill":1, # probability of generating a particle
+        },
+       "random_seed_algorithm":"incrementing_random",
+       "n_particles_per_spill":1,
        "transverse":{
-          "transverse_mode":"pencil", # transverse distribution matched to constant solenoid field
+          "transverse_mode":"pencil",
        },
-       "longitudinal":{"longitudinal_mode":"pencil", # longitudinal distribution sawtooth in time
-                   "momentum_variable":"p", # Gaussian in total momentum (options energy, pz)
-        }, # end time of sawtooth
-       "coupling":{"coupling_mode":"none"} # no dispersion
+       "longitudinal":{"longitudinal_mode":"pencil",
+                   "momentum_variable":"p",
+        },
+       "coupling":{"coupling_mode":"none"}
     },]
 }
 
