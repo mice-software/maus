17e6bfabc11417a7a86f240d266ce3353cfd7aea
Chris Rogers <chris.rogers@stfc.ac.uk>
2011-12-21 21:10:26 +0000

    Fix build_all also

--- third_party/build_all.bash
+++ third_party/build_all.bash
@@ -36,7 +36,7 @@ if [ -n "${MAUS_ROOT_DIR+x}" ]; then
     ${MAUS_ROOT_DIR}/third_party/bash/43libxslt.bash
     ${MAUS_ROOT_DIR}/third_party/bash/44cdb.bash
     ${MAUS_ROOT_DIR}/third_party/bash/50cloc.bash
-    ${MAUS_ROOT_DIR}/third_party/bash/51xboas.bash
+    ${MAUS_ROOT_DIR}/third_party/bash/51xboa.bash
     ${MAUS_ROOT_DIR}/third_party/bash/52jsoncpp.bash
     ${MAUS_ROOT_DIR}/third_party/bash/53unpacking.bash
     ${MAUS_ROOT_DIR}/third_party/bash/60recpack.bash
