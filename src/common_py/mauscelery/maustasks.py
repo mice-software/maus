87f88ca60cb7d25610a51960014ae1c847a5aeaa
Mike Jackson <michaelj@epcc.ed.ac.uk>
2012-01-19 17:46:38 +0000

    Alternatives for reconfiguring worker

--- src/common_py/mauscelery/maustasks.py
+++ src/common_py/mauscelery/maustasks.py
@@ -17,11 +17,15 @@ MAUS Celery tasks.
 #  along with MAUS.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
+import os 
 import re
-from types import ListType
-from types import StringType
+import signal
 import sys
 import traceback
+
+from multiprocessing import current_process 
+from types import ListType
+from types import StringType
 
 from celery.registry import tasks
 from celery.task import Task
@@ -31,87 +35,154 @@ import ROOT
 
 class MausGenericTransformTask(Task):
     """
-    MAUS task that creates an instance of each type of map worker
-    then uses these to execute pipelines specified by clients.
+    MAUS task that applies a transform. Initially the transform is
+    MapPyDoNothing and the configuration is empty. Both the transform
+    and configuration can be updated. 
     """
     
     def __init__(self):
         """ 
-        Initialise transform to None.
+        Initialise transform to MapPyDoNothing and set PyROOT to
+        batch mode to prevent canvases appearing.
         @param self Object reference.
         """
-        # Set ROOT to batch mode as certain mappers e.g. MapPyTOFPlot
-        # otherwise kick up a PyROOT canvas.
+        # Set ROOT to batch mode in case certain transforms
+        # use ROOT and kick up a canvas, which could give rise
+        # to an exception depending on terminal window display
+        # settings.
         ROOT.gROOT.SetBatch(True) # pylint: disable=E1101
-        self.__transforms = {}
-        for name in dir(MAUS):
-            if re.match("Map", name):
-                map_class = getattr(MAUS, name)
-                self.__transforms[name] = map_class()
-        self.__config = 0
+        # Initial transform and JSON configuration document.
+        self.__transform = MAUS.MapPyDoNothing()
+        self.__configuration = "{}"
 
-    def configure(self, config):
+    def create_transform(self, transform):
         """
-        Set task configuration value.
-        @param self Object reference.
-        @param config Configuration value.
-        """
-        self.__config = config
-
-    def birth(self, json_config_doc):
-        """
-        Setup transform then invoke birth with the JSON
-        configuration document.
-        @param self Object reference.
-        @param json_config_doc JSON configuration document.       
-        @return result of invoking birth.
-        @throws exception if any exceptions arise in birth().
-        """
-        for name in self.__transforms:
-            self.__transforms[name].birth(json_config_doc)
-        self.__config = self.__config + 1
-        return True
-
-    def run(self, workers, spill, client_id = "Unknown", spill_id = 0):
-        """
-        Apply the transform to the spill and return the new spill.
-        @param self Object reference.
-        @param workers Nested list of names of workers to invoke
-        when processing the spill e.g.
+        Create a transform given the name of transform
+        class(es). Either a single name can be given - representing a
+        single transform - or a list of transforms - representing a 
+        MapPyGroup. Sub-lists are treated as nested
+        MapPyGroups. Example list arguments include:  
         @verbatim 
+        []
         ["MapCppTOFDigits", "MapCppTOFSlabHits", "MapCppTOFSpacePoint"]
-        or
         ["MapCppTOFDigits", ["MapCppTOFSlabHits", "MapCppTOFSpacePoint"]]
         @endverbatim
+        Transforms must be in the MAUS module namespace e.g. 
+        MAUS.MapCppTOFSlabHits.        
+        @param self Object reference.
+        @param transform Transform name or list of names.
+        @return transform object or MapPyGroup object (if given a list).
+        @throws ValueError if transform is not a string or a list,
+        or contains an element which is not a list or string, or 
+        specifies an unknown transform name.
+        """
+        if isinstance(transform, ListType):
+            group = MAUS.MapPyGroup()
+            for transform_name in transform:
+                group.append(self.create_transform(transform_name))
+            return group
+        elif isinstance(transform, StringType):
+            if not hasattr(MAUS, transform):
+                raise ValueError("No such transform: %s" % transform)
+            transform_class = getattr(MAUS, transform)
+            return transform_class()
+        else:
+            raise ValueError("Transform name %s is not a string" % transform)
+
+    def set_configuration(self, configuration):
+        """
+        Death the current transforms then birth them with the
+        given configuration document.
+        @param self Object reference.
+        @param configuration JSON configuration document.
+        """
+        print "---set_configuration IN"
+        self.__transform.death()
+        # TODO add errors to return value.
+        self.__configuration = configuration
+        # TODO add errors to return value.
+        self.__transform.birth(self.__configuration)
+        # TODO add errors to return value.
+        print "---set_configuration OUT"
+
+    def set_transform(self, transform):
+        """
+        Death the current transforms, create new transforms then
+        birth them with the current configuration document.
+        @param self Object reference.
+        @param transform Either a single name can be given -
+        representing a single transform - or a list of transforms -
+        representing a MapPyGroup. Sub-lists are treated as nested
+        MapPyGroups.
+        @throws ValueError if transform is not a string or a list,
+        or contains an element which is not a list or string, or 
+        specifies an unknown transform name.
+        """
+        print "---set_transform IN"
+        self.__transform.death()
+        # TODO add errors to return value.
+        self.__transform = self.create_transform(transform)
+        # TODO add errors to return value.
+        self.__transform.birth(self.__configuration)
+        # TODO add errors to return value.
+        print "---set_transform OUT"
+
+    def reset_task(self, configuration = None, transform = None):
+        """
+        Death the current transforms, create new transforms then
+        birth them with the given configuration document.
+        @param self Object reference.
+        @param configuration JSON configuration document.
+        @param transform Either a single name can be given -
+        representing a single transform - or a list of transforms -
+        representing a MapPyGroup. Sub-lists are treated as nested
+        MapPyGroups.
+        @throws ValueError if transform is not a string or a list,
+        or contains an element which is not a list or string, or 
+        specifies an unknown transform name.
+        """
+        print "---reset_task IN"
+        # Validate arguments before invoking death()
+        if (transform != None):
+            new_transform = self.create_transform(transform)
+            # TODO add errors to return value.
+        else:
+            new_transform = self.__transform
+        if (configuration != None):
+            # TODO validate it's JSON
+            self.__configuration = configuration
+            # TODO add errors to return value.
+        self.__transform.death()
+        # TODO add errors to return value.
+        self.__transform = new_transform
+        self.__transform.birth(self.__configuration)
+        # TODO add errors to return value.
+        print "---reset_task OUT"
+
+    def run(self, workers, spill, client_id = "Unknown", spill_id = 0):
+        print "RUN!"
+        print "Sleep..."
+        import time
+        time.sleep(20)
+        print "...awake!"
+        return spill
+
+    def run2(self, spill, client_id = "Unknown", spill_id = 0):
+        """
+        Apply the current transform to the spill and return the new
+        spill. 
+        @param self Object reference.
         @param spill JSON document string holding spill.
         @param client_id ID of client who submitted job.
         @param spill_id Index of spill from this client.
         @return JSON document string holding new spill.
         """
-        print "Configuration: %s" % self.__config
         logger = Task.get_logger()
         if logger.isEnabledFor(logging.INFO):
-            logger.info("Task invoked by %s requesting workers %s on spill %d" \
-                % (client_id, workers, spill_id))    
-        if not isinstance(workers, ListType):
-            raise ValueError("%s is not a list" % workers)
-        # This does a start-to-end comparison of the lists.
-        worker_list = list(workers)
-        while len(worker_list) != 0:
-            worker = worker_list.pop(0)
-            if isinstance(worker, ListType):
-                # Add sub-list members to front of list.
-                worker.extend(worker_list)
-                worker_list = worker
-            else:
-                # Process spill.
-                if logger.isEnabledFor(logging.INFO):
-                    logger.info("Executing worker : %s" % worker)
-                if not isinstance(worker, StringType):
-                    raise ValueError("Worker name %s is not a string" % worker)
-                if not self.__transforms.has_key(worker):
-                    raise ValueError("No such worker: %s" % worker)
-                spill = self.__transforms[worker].process(spill)
+            logger.info("Task invoked by %s on spill %d" \
+                % (client_id, spill_id))    
+            spill = self.__transform.process(spill)
+        # TODO error handling.
         return spill
 
 tasks.register(MausGenericTransformTask) # pylint:disable=W0104, E1101
@@ -119,36 +190,35 @@ tasks.register(MausGenericTransformTask) # pylint:disable=W0104, E1101
 from celery.worker.control import Panel
 
 @Panel.register
-def configure_maus(panel, configuration):
+def get_process_pool(panel):
     """
-    Reconfigure MausGenericTransformTask
+    Get information on the current process pool.
     @param panel Celery panel object.
-    @param configuration Configuration value.
-    @return status document.
+    @return status document of form {master_pid: PID, master_name:
+    PROCESS_NAME, pool_pids: [PID, PID, PID]}.
     """
-    print "Configuring..."
-    from multiprocessing import current_process 
-    print "Process name: %s" % current_process().name 
+    status = {}
+    process_status = current_process()
+    status["master_pid"] = process_status.pid
+    status["master_name"] = process_status.name
     pool = panel.consumer.pool 
-    print "Pool processes: %s " % pool.info["processes"]
-    maustask = tasks["mauscelery.maustasks.MausGenericTransformTask"]
-    maustask.configure(configuration)
-    print "Configured"
-    return {"ok": "configured"}
+    status["pool_pids"] = pool.info["processes"]
+    print "Process pool: %s" % status
+    return status
 
 @Panel.register
-def reset_pool(panel):
+def reset_pool_term(panel):
     """
-    Reset the worker pool by TERM killing the processes.
+    Reset the worker pool by killing the pool processes via
+    a TERM invocation, which forces Celery to spawn new pool
+    processes. 
     @param panel Celery panel object.
     @return status document.
     """
     print "Resetting pool..."
-    from multiprocessing import current_process 
-    print "Process name: %s" % current_process().name 
     pool = panel.consumer.pool 
-    print "Pool.information: %s" % pool.info
     pids = pool.info["processes"]
+    status = {}
     try:
         import os 
         import signal
@@ -156,8 +226,92 @@ def reset_pool(panel):
             print "About to SIGTERM kill %s" % pid
             os.kill(pid, signal.SIGTERM)
             print "Killed %s" % pid
+            status["status"] = "ok"
     except: # pylint: disable=W0702
         traceback.print_exc(file=sys.stdout)
+        status["status"] = "error"
+        # TODO add errors to return value.
+    print status
+    return status
+
+@Panel.register
+def reset_pool_term_job(panel):
+    """
+    Reset the worker pool by killing the pool processes via
+    a pool.terminate_job invocation, which forces Celery to spawn new
+    pool processes. 
+    @param panel Celery panel object.
+    @return status document.
+    """
+    print "Resetting pool..."
+    pool = panel.consumer.pool 
+    pids = pool.info["processes"]
+    status = {}
+    try:
+        for pid in pids:
+            print "About to terminate_job %s" % pid
+            pool.terminate_job(pid)
+            print "Terminated %s" % pid
+            status["status"] = "ok"
+    except: # pylint: disable=W0702
+        traceback.print_exc(file=sys.stdout)
+        status["status"] = "error"
+        # TODO add errors to return value.
+    print status
+    return status
+
+@Panel.register
+def reset_pool_stopstart(panel):
+    """
+    Reset the worker pool by stopping and restarting the
+    pool.
+    @param panel Celery panel object.
+    @return status document.
+    """
+    print "Resetting pool..."
+    pool = panel.consumer.pool 
+    status = {}
+    try:
+        pool.stop()
+        pool.start()
+        status["status"] = "ok"
+    except: # pylint: disable=W0702
+        traceback.print_exc(file = sys.stdout)
         print "Error"
-    print "Reset"
-    return {"ok": "reset"}
+        status["status"] = "error"
+        # TODO add errors to status
+    print status
+    return status
+
+@Panel.register
+def reset_pool_resize(panel):
+    pool = panel.consumer.pool
+    pool_size = pool.num_processes
+    print pool_size
+    print "Shrinking pool..."
+    pool.shrink(pool_size)
+    print "Growing pool..."
+    pool.grow(pool_size)
+    print "Current pool size %s" % pool_size
+
+@Panel.register
+def reset_maus(panel, configuration = None, transform = None):
+    """
+    Death the current transforms, create new transforms then
+    birth them with the given configuration document.
+    @param panel Celery panel object.
+    @param configuration JSON configuration document.
+    @param transform Either a single name can be given -
+    representing a single transform - or a list of transforms -
+    representing a MapPyGroup. Sub-lists are treated as nested
+    MapPyGroups.
+    @throws ValueError if transform is not a list or a string
+    holding a known transform class name, or if transform is a
+    list but has an element which is not a list or a string
+    holding a known transform class name.
+    """
+    print "Configuring..."
+    maustask = tasks["mauscelery.maustasks.MausGenericTransformTask"]
+    maustask.reset_task(configuration, transform)
+    print "...configured"
+    return {"status": "ok"}
