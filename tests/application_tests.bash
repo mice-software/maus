f7d49eb75fd6d983eb531eea8eb360e95d13cf7c
Peter Lane <plane1@iit.edu>
2012-02-15 14:34:29 +0000

    A few more configuration snags fixed.

--- tests/application_tests.bash
+++ tests/application_tests.bash
@@ -3,11 +3,18 @@
 # hard coded to whatever easy_install was set up with (not right if we move code)
 # Issue #819
 
+arch=`uname -s`
+
 if [ -z "${MAUS_ROOT_DIR}" ]; then  # see if the variable exists yet
     echo "ERROR: \$MAUS_ROOT_DIR not set"
     exit 1;
 fi
 
+if [ "$arch" == "Darwin" ]; then
+  NOSETESTS=${MAUS_THIRD_PARTY}/install/Python.framework/Versions/2.7/bin/nosetests
+else
+  NOSETESTS=${MAUS_THIRD_PARTY}/install/bin/nosetests
+fi
 
-MAUS_TEST_PLOT_DIR=${MAUS_ROOT_DIR}/tests/integration/plots/ python ${MAUS_THIRD_PARTY}/third_party/install/bin/nosetests -v ${MAUS_ROOT_DIR}/tests/integration
+MAUS_TEST_PLOT_DIR=${MAUS_ROOT_DIR}/tests/integration/plots/ python ${NOSETESTS} -v ${MAUS_ROOT_DIR}/tests/integration
 
