230f89387fa3f94155df16b66d299dc9825b27c3
mice-lcr <chrisrogers1234@yahoo.co.uk>
2012-05-04 15:33:00 +0100

    Test always fails... need tests here

---
+++ tests/integration/test_analyze_online/test_analyze_online.py
@@ -0,0 +1,13 @@
+
+
+import unittest
+
+class TestAnalyzeOnline(unittest.TestCase):
+
+     def test_analyze_online(self):
+         self.assertTrue(False, msg="""
+         Better make some tests here
+         """)
+
+if __name__ == "__main__":
+    unittest.main()
