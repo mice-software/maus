501c452211b45b47bb112ff307e03f09a1a21454
Chris Rogers <chris.rogers@stfc.ac.uk>
2012-06-19 10:14:56 +0100

    option to get third party libraries en masse off maus server

---
+++ third_party/bash/00get_all_libs.bash
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+version=`grep "version" README | sed s/\ /_/g`
+filename=third_party_libraries.tar.gz
+url=http://micewww.pp.rl.ac.uk/maus/${version}/${filename}
+
+if [ -n "${MAUS_ROOT_DIR+x}" ]; then
+
+    if [ -e "${MAUS_ROOT_DIR}/third_party/source/${filename}" ]
+    then
+        echo "INFO: Found source archive in 'source' directory"
+    else
+        echo "INFO: Source archive doesn't exist.  Downloading..."
+        wget --directory-prefix="${MAUS_ROOT_DIR}/third_party/source" ${url}
+    fi
+
+    if [ -e "${MAUS_ROOT_DIR}/third_party/source/${filename}" ]
+    then
+        echo "INFO: Source archive exists."
+        echo "INFO: Unpacking:"
+        echo
+        tar xvfz "${MAUS_ROOT_DIR}/third_party/source/${filename}" -C "${MAUS_ROOT_DIR}/third_party/source"
+    else
+        echo "ERROR: Failed to get source archive"
+        exit 1
+    fi
+else
+    echo "ERROR: MAUS_ROOT_DIR was not set"
+    echo "ERROR: Need to do (from maus download directory)"
+    echo "ERROR:           ./configure"
+    echo "ERROR:           source env.sh"
+    echo "ERROR: Before attempting to build"
+    echo
+fi
--- third_party/build_all.bash
+++ third_party/build_all.bash
@@ -22,6 +22,11 @@ if [ -n "${MAUS_ROOT_DIR+x}" ]; then
     # ls ${MAUS_ROOT_DIR}/third_party/bash | grep bash | sort| xargs -i echo '${MAUS_ROOT_DIR}/third_party/bash/{}'
     #
 
+    ${MAUS_ROOT_DIR}/third_party/bash/00get_all_libs.bash
+    if [ $? != 0 ]; then
+        echo "WARNING: Failed to download the third party tarball. I will try"
+        echo "WARNING: to download them individually"
+    fi
     ${MAUS_ROOT_DIR}/third_party/bash/01python.bash
     python ${MAUS_ROOT_DIR}/third_party/check_path.py
     if [ $? != 0 ]; then
