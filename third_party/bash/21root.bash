9d759c2a958e455be6a33240cb3fdbdd74667af9
Adam Dobbs <phuccj@gmail.com>
2012-06-19 15:33:16 +0100

    Style fixes and temporary patch while ROOT website is down

--- third_party/bash/21root.bash
+++ third_party/bash/21root.bash
@@ -2,7 +2,8 @@
 directory=root_v5.30.03
 #directory=root_v5.32.01
 filename=${directory}.source.tar.gz 
-url=ftp://root.cern.ch/root/${filename}
+#url=ftp://root.cern.ch/root/${filename}
+url=http://www.hep.ph.ic.ac.uk/~adobbs/Files/${filename}
 
 if [ -n "${MAUS_ROOT_DIR+x}" ]; then
 
