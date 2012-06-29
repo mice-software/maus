d132a203ed8ed27c414a5b732f35ddb77634a0c1
Edward Santos <e.santos10@imperial.ac.uk>
2012-06-29 20:47:45 +0100

    debug.

--- src/common_cpp/Recon/Kalman/KalmanTrackFit.hh
+++ src/common_cpp/Recon/Kalman/KalmanTrackFit.hh
@@ -34,7 +34,7 @@
 #include "src/common_cpp/Recon/Kalman/KalmanTrack.hh"
 #include "src/common_cpp/Recon/Kalman/HelicalTrack.hh"
 #include "src/common_cpp/Recon/Kalman/StraightTrack.hh"
-//#include "src/common_cpp/Recon/Kalman/GlobalTrack.hh"
+#include "src/common_cpp/Recon/Kalman/GlobalTrack.hh"
 #include "src/common_cpp/Recon/Kalman/KalmanSite.hh"
 #include "src/common_cpp/Recon/Kalman/KalmanMonitor.hh"
 #include "src/common_cpp/Recon/SciFi/SeedFinder.hh"
