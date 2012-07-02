d3688f9036dfd1c10fafe2cb233a2f1bb02cd938
Chris Rogers <chris.rogers@stfc.ac.uk>
2012-07-02 09:45:42 +0100

    Remove defunct third party libraries and README, RELEASENOTES for 0.3.0

--- third_party/build_all.bash
+++ third_party/build_all.bash
@@ -48,8 +48,6 @@ if [ -n "${MAUS_ROOT_DIR+x}" ]; then
     ${MAUS_ROOT_DIR}/third_party/bash/51xboa.bash
     ${MAUS_ROOT_DIR}/third_party/bash/52jsoncpp.bash
     ${MAUS_ROOT_DIR}/third_party/bash/53unpacking.bash
-    ${MAUS_ROOT_DIR}/third_party/bash/60recpack.bash
-
 
 else
 echo
