c88765b506a1bcb477edaf15d6757ba08a47d7c6
Adam Dobbs <phuccj@gmail.com>
2012-07-25 14:35:30 +0100

    Adding files missed in previous commit

---
+++ src/common_cpp/DataStructure/SciFiDigit.cc
@@ -0,0 +1,55 @@
+/* This file is part of MAUS: http://micewww.pp.rl.ac.uk/projects/maus
+ *
+ * MAUS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MAUS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MAUS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "src/common_cpp/DataStructure/SciFiDigit.hh"
+
+
+namespace MAUS {
+
+SciFiDigit::SciFiDigit():_used(false) {
+}
+
+SciFiDigit::SciFiDigit(int spill, int event, int tracker, int station,
+                       int plane, int channel, double npe, double time )
+                       :_spill(spill), _event(event),
+                        _tracker(tracker), _station(station), _plane(plane),
+                        _channel(channel), _npe(npe), _time(time), _used(false) {
+}
+
+SciFiDigit::SciFiDigit(const SciFiDigit& _scifidigit) {
+    *this = _scifidigit;
+}
+
+SciFiDigit& SciFiDigit::operator=(const SciFiDigit& _scifidigit) {
+    if (this == &_scifidigit) {
+        return *this;
+    }
+    _spill = _scifidigit._spill;
+    _event = _scifidigit._event;
+    _tracker = _scifidigit._tracker;
+    _station = _scifidigit._station;
+    _plane = _scifidigit._plane;
+    _channel = _scifidigit._channel;
+    _npe = _scifidigit._npe;
+    _time = _scifidigit._time;
+    _used = _scifidigit._used;
+    return *this;
+}
+
+SciFiDigit::~SciFiDigit() {
+}
+
+} // ~namespace MAUS
