c878bd08f9d6c21e540f7a5f0ce607ed7e5a5a83
Chris Rogers <chris.rogers@stfc.ac.uk>
2012-07-31 11:14:26 +0100

    Tweaks to physics model tests

--- tests/integration/test_simulation/test_physics_model/physics_model_config.py
+++ tests/integration/test_simulation/test_physics_model/physics_model_config.py
@@ -31,16 +31,16 @@ spill_generator_number_of_spills = 2
 verbose_level = 0
 keep_tracks = True
 
-simulation_reference_particle = { # used for setting particle phase
+# energy is very low - so that particles probably decay if they are unstable
+simulation_reference_particle = {
     "position":{"x":0.0, "y":-0.0, "z":0.0},
     "momentum":{"x":0.0, "y":0.0, "z":1.0},
-    "particle_id":211, "energy":300.0, "time":0.0, "random_seed":0
+    "particle_id":211, "energy":140.0, "time":0.0, "random_seed":0
 }
 
 beam = {
-    "particle_generator":"counter", # routine for generating empty primaries
-    "random_seed":0, # random seed for beam generation; controls also how the MC
-                     # seeds are generated
+    "particle_generator":"counter",
+    "random_seed":0,
     "definitions":[
     {
        "reference":{
@@ -50,9 +50,9 @@ beam = {
             "energy":300.0,
             "time":0.,
             "random_seed":0
-        }, # reference particle
+        },
        "n_particles_per_spill":1,
-       "random_seed_algorithm":"incrementing_random", # algorithm for seeding MC
+       "random_seed_algorithm":"incrementing_random",
        "transverse":{"transverse_mode":"pencil"},
         "longitudinal":{"longitudinal_mode":"pencil","momentum_variable":"p"},
        "coupling":{"coupling_mode":"none"}
@@ -65,9 +65,9 @@ beam = {
             "energy":226.0,
             "time":0.,
             "random_seed":0
-        }, # reference particle
+        },
        "n_particles_per_spill":1,
-       "random_seed_algorithm":"incrementing_random", # algorithm for seeding MC
+       "random_seed_algorithm":"incrementing_random",
        "transverse":{"transverse_mode":"pencil"},
         "longitudinal":{"longitudinal_mode":"pencil","momentum_variable":"p"},
        "coupling":{"coupling_mode":"none"}
