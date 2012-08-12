1a34a8b199e205b522244922166d93848775224d
Chris Rogers <chris.rogers@stfc.ac.uk>
2012-08-12 14:17:05 +0100

    Script to build third party libraries, then switch to geant4.9.5.p01, then run unit tests only

---
+++ third_party/install_build_test_geant4.9.5.p01.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+./configure
+source env.sh
+./third_party/build_all.bash
+source env.sh
+bash third_party/bash/32clhep2.1.1.0.bash
+bash third_party/bash/33geant4.9.5.bash
+bash third_party/bash/34geant4.9.5-data.bash
+source env_geant4.9.5.p01.sh
+scons -c
+scons -j8
+bash tests/unit_tests.bash
+
+
