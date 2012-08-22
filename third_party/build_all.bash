f6453e3c78e8b6842d23a82168319102592c3034
Alexander Richards <a.richards@imperial.ac.uk>
2012-08-22 11:21:13 +0100

    InputCppDAQOnline module addition

--- third_party/build_all.bash
+++ third_party/build_all.bash
@@ -39,6 +39,7 @@ if [ -n "${MAUS_ROOT_DIR+x}" ]; then
     ${MAUS_ROOT_DIR}/third_party/bash/51xboa.bash
     ${MAUS_ROOT_DIR}/third_party/bash/52jsoncpp.bash
     ${MAUS_ROOT_DIR}/third_party/bash/53unpacking.bash
+    ${MAUS_ROOT_DIR}/third_party/bash/70daq.bash
 
 else
 echo
