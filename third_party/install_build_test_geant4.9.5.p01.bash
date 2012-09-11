6ede7cb0ef47aaafb4adffcd8ec93a9f0cdde5e6
Alexander Richards <a.richards@imperial.ac.uk>
2012-09-11 09:30:07 +0100

    Fix

--- third_party/install_build_test_geant4.9.5.p01.bash
+++ third_party/install_build_test_geant4.9.5.p01.bash
@@ -10,12 +10,12 @@ fi
 source env.sh
 ./third_party/build_all.bash
 source env.sh
-bash third_party/bash/32clhep2.1.1.0.bash
-bash third_party/bash/33geant4.9.5.bash
-bash third_party/bash/34geant4.9.5-data.bash
+#bash third_party/bash/32clhep2.1.1.0.bash
+#bash third_party/bash/33geant4.9.5.bash
+#bash third_party/bash/34geant4.9.5-data.bash
 source env_geant4.9.5.p01.sh
 scons -c
 scons -j8
-bash tests/unit_tests.bash
+#bash tests/unit_tests.bash
 
 
