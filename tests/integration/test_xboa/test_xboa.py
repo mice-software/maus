971e69715d17a6d9f80721516acde52cc1195c5d
Chris Rogers <chris.rogers@stfc.ac.uk>
2013-01-31 09:31:11 +0000

    Add xboa tests to MAUS integrations tests

---
+++ tests/integration/test_xboa/test_xboa.py
@@ -0,0 +1,37 @@
+#  This file is part of MAUS: http://micewww.pp.rl.ac.uk:8080/projects/maus
+#
+#  MAUS is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  MAUS is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with MAUS.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Run xboa tests as part of MAUS integration tests
+"""
+
+import unittest
+import xboa.test.XBOATest
+
+class TestXBOA(unittest.TestCase): # pylint: disable=R0904
+    """
+    Run the tests and check they return 0
+    """
+
+    def test_xboa(self):
+        """
+        Run the xboa test
+        """
+        passes, fails, warns = xboa.test.XBOATest.test_all()
+        self.assertEqual(fails, 0, msg = str((passes, fails, warns)))
+
+if __name__ == "__main__":
+    unittest.main()
+
