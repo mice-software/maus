724f2c2a27527a633363c597cbb7d42d1bf3e312
Ian Taylor <i.taylor@warwick.ac.uk>
2013-05-09 12:09:27 +0100

    I bloody hate style tests!

--- tests/cpp_unit/JsonCppProcessors/PrimitivesProcessorsTest.cc
+++ tests/cpp_unit/JsonCppProcessors/PrimitivesProcessorsTest.cc
@@ -108,28 +108,28 @@ TEST(PrimitivesProcessorsTest, UIntJsonToCpp) {
   value = proc.JsonToCpp(Json::Value(1)); // int
   EXPECT_EQ(*value, static_cast<unsigned int>(1));
   delete value;
-  
+
   value = proc.JsonToCpp(Json::Value(0)); // int
   EXPECT_EQ(*value, static_cast<unsigned int>(0));
   delete value;
 
   EXPECT_THROW(proc.JsonToCpp(Json::Value(-1)), Squeal); // negative int
-  
+
   unsigned int uint_in = 1;
   value = proc.JsonToCpp(Json::Value(uint_in)); // uint
   EXPECT_EQ(*value, static_cast<unsigned int>(1));
   delete value;
-  
+
   uint_in = 0;
   value = proc.JsonToCpp(Json::Value(uint_in)); // uint
   EXPECT_EQ(*value, static_cast<unsigned int>(0));
   delete value;
-  
+
   uint_in = 2147483648;
   value = proc.JsonToCpp(Json::Value(uint_in)); // uint
   EXPECT_EQ(*value, static_cast<unsigned int>(2147483648));
   delete value;
-  
+
   EXPECT_THROW(proc.JsonToCpp(Json::Value("string")), Squeal);
 }
 
