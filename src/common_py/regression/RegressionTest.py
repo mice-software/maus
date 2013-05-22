ce3493beda7b2ecf71f2f2ef9a8f6284f25ff5d2
Alexander Richards <a.richards@imperial.ac.uk>
2013-05-22 10:03:57 +0100

    Fixing remaining style issue

--- src/common_py/regression/RegressionTest.py
+++ src/common_py/regression/RegressionTest.py
@@ -1,3 +1,4 @@
+""" The Regression Test framework code"""
 #  This file is part of MAUS: http://micewww.pp.rl.ac.uk:8080/projects/maus
 #
 #  MAUS is free software: you can redistribute it and/or modify
