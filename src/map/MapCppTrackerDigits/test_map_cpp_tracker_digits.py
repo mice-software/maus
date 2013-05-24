59a3e5b054a996edb8cd8b48a47c69ce96765379
Adam Dobbs <phuccj@gmail.com>
2013-05-24 13:36:08 +0100

    Update to cpp scifi analysis app

--- src/map/MapCppTrackerDigits/test_map_cpp_tracker_digits.py
+++ src/map/MapCppTrackerDigits/test_map_cpp_tracker_digits.py
@@ -25,6 +25,7 @@ import os
 from Configuration import Configuration
 
 from MapCppTrackerDigits import MapCppTrackerDigits
+import maus_cpp.globals
 
 # Disable: Too many public methods
 # pylint: disable-msg=R0904
@@ -46,6 +47,8 @@ class MapCppTrackerDigitsTestCase(unittest.TestCase):
         """
         self.mapper = MapCppTrackerDigits()
         # Test whether the configuration files were loaded correctly at birth
+        if maus_cpp.globals.has_instance():
+            maus_cpp.globals.death()
         success = self.mapper.birth(json.dumps(self.cfg))
         if not success:
             raise Exception('InitializeFail', 'Could not start worker')
