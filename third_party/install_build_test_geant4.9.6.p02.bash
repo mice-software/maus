a8ec1d5d9f4a5814383562aa399c2454135e7b35
Ryan D. Bayes <Ryan.Bayes@glasgow.ac.uk>
2013-06-22 15:50:48 +0100

    Adding installation scripts for geant4.9.6.p02.

---
+++ third_party/install_build_test_geant4.9.6.p02.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cmake --version
+if [ $? != 0 ]; then
+    echo "FATAL: Require cmake to build g4.9.6.p02"
+    exit 1
+fi
+
+./configure
+source env.sh
+./third_party/build_all.bash
+source env.sh
+bash third_party/bash/29expat.bash
+bash third_party/bash/32clhep2.1.1.0.bash
+bash third_party/bash/36geant4.9.6.bash
+source env_geant4.9.6.p02.sh
+scons -c
+scons -j8
+bash tests/run_tests.bash
+
+
