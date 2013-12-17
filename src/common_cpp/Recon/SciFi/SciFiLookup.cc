ed7270b1d5f0cb1a8654240ce98f34d32f05406e
Adam Dobbs <phuccj@gmail.com>
2013-12-17 11:57:13 +0000

    Adding files forgotten last time

---
+++ src/common_cpp/Recon/SciFi/SciFiLookup.cc
@@ -0,0 +1,99 @@
+/* This file is part of MAUS: http://micewww.pp.rl.ac.uk:8080/projects/maus
+ *
+ * MAUS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MAUS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MAUS.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// MAUS headers
+#include "src/common_cpp/Recon/SciFi/SciFiLookup.hh"
+
+namespace MAUS {
+
+SciFiLookup::SciFiLookup() : _hits_map(),
+                             _noise_map() {
+  // Do nothing
+}
+
+SciFiLookup::~SciFiLookup() {
+  // Do nothing
+}
+
+bool SciFiLookup::make_hits_map(MCEvent* evt) {
+  // Check the pointer is valid
+  if (!evt) {
+    std::cerr << "Invalid MC event pointer supplied, aborting" << std::endl;
+    return false;
+  }
+
+  // Make the hits map
+  std::vector<SciFiHit> *hits = evt->GetSciFiHits();
+  for ( size_t i = 0; i < hits->size(); ++i ) {
+    SciFiHit *hit = &(hits->at(i));
+    uint64_t id = (uint64_t)hit->GetChannelId()->GetID();
+    // If id is not in map, make a hits vector for the key, and add the current hit
+    if ( _hits_map.find(id) == _hits_map.end() ) {
+      std::vector<SciFiHit*> v1;
+      _hits_map[id] = v1;
+      _hits_map[id].push_back(hit);
+    // Otherwise add the current hit to the vector already associate with this id
+    } else {
+      _hits_map[id].push_back(hit);
+    }
+  }
+  return true;
+}
+
+bool SciFiLookup::make_noise_map(MCEvent* evt) {
+  // Check the pointer is valid
+  if (!evt) {
+    std::cerr << "Invalid MC event pointer supplied, aborting" << std::endl;
+    return false;
+  }
+
+  // Make the noise hits map
+  std::vector<SciFiNoiseHit> *hits = evt->GetSciFiNoiseHits();
+  for ( size_t i = 0; i < hits->size(); ++i ) {
+    SciFiNoiseHit *hit = &(hits->at(i));
+    uint64_t id = (uint64_t)hit->GetID();
+    // If id is not in map, make a hits vector for the key, and add the current hit
+    if ( _noise_map.find(id) == _noise_map.end() ) {
+      std::vector<SciFiNoiseHit*> v1;
+      _noise_map[id] = v1;
+      _noise_map[id].push_back(hit);
+    // Otherwise add the current hit to the vector already associate with this id
+    } else {
+      _noise_map[id].push_back(hit);
+    }
+  }
+  return true;
+}
+
+std::vector<SciFiHit*> SciFiLookup::get_hits(SciFiDigit* dig) {
+  uint64_t digit_id = (uint64_t)get_digit_id(dig);
+  return _hits_map[digit_id];
+}
+
+std::vector<SciFiNoiseHit*> SciFiLookup::get_noise(SciFiDigit* dig) {
+  uint64_t digit_id = (uint64_t)get_digit_id(dig);
+  return _noise_map[digit_id];
+}
+
+uint64_t SciFiLookup::get_digit_id(SciFiDigit* dig) {
+  uint64_t digit_id = 1 * dig->get_channel() + 1000 * dig->get_plane() +
+                  10000 * dig->get_station() + 100000 * dig->get_tracker() +
+                  1000000 * dig->get_event();
+  return digit_id;
+}
+
+} // ~namespace MAUS
---
+++ src/common_cpp/Recon/SciFi/SciFiLookup.hh
@@ -0,0 +1,74 @@
+/* This file is part of MAUS: http://micewww.pp.rl.ac.uk:8080/projects/maus
+ *
+ * MAUS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MAUS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MAUS.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/** @class SciFiLookup
+ * Class used to link between SciFiDigits and the MC hits used to make them
+ */
+
+#ifndef SCIFILOOKUP_HH
+#define SCIFILOOKUP_HH
+
+// C++ headers
+#include <vector>
+#include <map>
+
+// MAUS headers
+#include "src/common_cpp/DataStructure/MCEvent.hh"
+#include "src/common_cpp/DataStructure/Hit.hh"
+#include "src/common_cpp/DataStructure/SciFiNoiseHit.hh"
+#include "src/common_cpp/DataStructure/SciFiDigit.hh"
+
+namespace MAUS {
+
+class SciFiLookup {
+  public:
+    /** @brief Default constructor */
+    SciFiLookup();
+
+    /** @brief Default destructor, does nothing */
+    ~SciFiLookup();
+
+    /** @brief Create the maps between digits and hits */
+    bool make_hits_map(MCEvent* evt);
+
+    /** @brief Create the maps between digits and noise hits */
+    bool make_noise_map(MCEvent* evt);
+
+    /** @brief Return a vector of SciFiHits used to make a SciFiDigit */
+    std::vector<SciFiHit*> get_hits(SciFiDigit* dig);
+
+    /** @brief Return a vector of SciFiNoiseHits used to make a SciFiDigit */
+    std::vector<SciFiNoiseHit*> get_noise(SciFiDigit* dig);
+
+    /** @brief Return a vector of SciFiNoiseHits used to make a SciFiDigit */
+    uint64_t get_digit_id(SciFiDigit* digit);
+
+    /** @brief Return the digit to hits map */
+    std::map<uint64_t, std::vector<SciFiHit*> > get_hits_map() { return _hits_map; }
+
+    /** @brief Return the digit to noise hits map */
+    std::map<uint64_t, std::vector<SciFiNoiseHit*> > get_noise_map()
+                                                                              { return _noise_map; }
+
+  private:
+    std::map<uint64_t, std::vector<SciFiHit*> > _hits_map;
+    std::map<uint64_t, std::vector<SciFiNoiseHit*> > _noise_map;
+};
+
+} // ~namespace MAUS
+
+#endif
