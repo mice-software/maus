138f6f2499b3073c1332dfb81255230f3e4d3ce2
Ryan Bayes <Ryan.Bayes@glasgow.ac.uk>
2014-04-15 12:22:30 +0100

    Added new tests for the geometry location fit and python based gdml translation. Small changes were made to the location fit algorithm initialization to accommodate the test.

--- src/common_py/geometry/LocationFit.py
+++ src/common_py/geometry/LocationFit.py
@@ -61,10 +61,19 @@ class ElementRotationTranslation: #pylint: disable = R0903, R0902, C0103, E1101
         self.XMLFile = ''
         self.DataFile = ''
         self.RefFile  = ''
+        # Override methods
+        self.DataFile = config_dict['survey_measurement_record']
+        self.RefFile = config_dict['survey_reference_position']
         
         self.UseGDML  = config_dict['use_gdml_source']
+        self.tempList = config_dict['survey_target_detectors']
         if self.UseGDML:
-            # The survey information is in the CAD geometry
+            self.defineGDMLVariables()
+            
+        self.result = []
+
+    def defineGDMLVariables(self):
+                # The survey information is in the CAD geometry
             # file which should have one of these names
             fnames = ['fastradModel.gdml', 'FastradModel.gdml', 'Step_IV.gdml']
             for name in fnames:
@@ -74,18 +83,18 @@ class ElementRotationTranslation: #pylint: disable = R0903, R0902, C0103, E1101
                     # break on the first qualifing instance.
                     break
             # An override method --- if necessary
-            if config_dict['survey_measurement_record'] != "":
-                self.GDMLFile = config_dict['survey_measurement_record']
+            if self.DataFile != "":
+                self.GDMLFile = self.DataFile
             # The default source of reference information for the fit.
             self.XMLFile  = os.path.join(self.dl_dir,'Maus_Information.gdml')
             if not os.path.exists(self.XMLFile):
                 self.XMLFile = ''
             # An override method --- if necessary
-            if config_dict['survey_reference_position'] != "":
-                self.XMLFile  = config_dict['survey_reference_position'] 
+            if self.RefFile != "":
+                self.XMLFile  = self.RefFile
+            # print 
             print self.GDMLFile, self.XMLFile
             self.datafile = libxml2.parseFile(self.XMLFile)
-            print self.GDMLFile
             self.gdmlfile = libxml2.parseFile(self.GDMLFile)
             # Fitting tolerance. Reject fit if chi^2/ndof > value
             self.tolerance = 3 # this should be read from the CDB files
@@ -116,11 +125,7 @@ class ElementRotationTranslation: #pylint: disable = R0903, R0902, C0103, E1101
                         self.sfTarget.append(det)
             if not len(self.sfTarget):
                 # check for an alternate list if no detectors match. 
-                self.sfTarget = config_dict['survey_target_detectors']
-        else:
-            self.DataFile = config_dict['survey_measurement_record']
-            self.RefFile = config_dict['survey_reference_position']
-        self.result = []
+                self.sfTarget = self.tempList
 
     def execute(self): #pylint: disable = R0903, R0902, C0103
         """
@@ -220,6 +225,7 @@ class ElementRotationTranslation: #pylint: disable = R0903, R0902, C0103, E1101
         # if len(det)==0:
         for q in self.gdmlfile.xpathEval(\
             "gdml/structure/volume/physvol/position"):
+            print q.prop("name")
             if q.prop("name") == det[0].prop("gdml_posref"):
                 self.initguess[0] =  float(q.prop('x'))
                 self.initguess[1] =  float(q.prop('y'))
