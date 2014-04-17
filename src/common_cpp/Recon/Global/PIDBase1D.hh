8c5a9dbdbd374aaf9c87cff00133c3bd95168f5e
Celeste Pidcott <c.e.pidcott@warwick.ac.uk>
2014-04-17 18:25:49 +0100

    Adding in files for 2D PID and PIDVarB, plus new Global TrackMatching

---
+++ src/common_cpp/Recon/Global/PIDBase1D.hh
@@ -0,0 +1,100 @@
+/* This file is part of MAUS: http://  micewww.pp.rl.ac.uk:8080/projects/maus
+ * 
+ * MAUS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * MAUS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MAUS.  If not, see <http://  www.gnu.org/licenses/>.
+ */
+
+ /** @class PIDBase
+ *  Base class containing constructors and functions required for PID
+ *  analysis
+ */
+
+#ifndef PIDBASE1D_HH
+#define PIDBASE1D_HH
+
+#include <stdio.h>
+#include <string>
+
+#include "TMath.h"
+#include "TMathBase.h"
+#include "TTimeStamp.h"
+#include "TH1.h"
+#include "TFile.h"
+#include "TUnixSystem.h"
+#include "TSystem.h"
+
+#include "Interface/Squeak.hh"
+
+#include "DataStructure/Global/Track.hh"
+#include "Recon/Global/PIDBase.hh"
+
+
+namespace MAUS {
+namespace recon {
+namespace global {
+
+  class PIDBase1D : public PIDBase {
+
+  public:
+
+    /// Constructor to create a PDF
+    PIDBase1D(std::string variable, std::string hypothesis,
+           std::string unique_identifier, int XminBin, int XmaxBin,
+           int XnumBins);
+    /// Constructor to use a PDF to perform PID analysis
+    PIDBase1D(TFile* file, std::string variable, std::string hypothesis);
+    /// Destructor
+    virtual ~PIDBase1D();
+
+    // Get histogram
+    TH1F* Get_hist();
+
+    /** @brief Calculate log likelihood of incoming track corresponding to 
+     *   a particle hypothesis
+     *
+     *  @param track The track for which the log likelihood is calculated
+     *
+     *  @return Returns the log likelihood
+     */
+    double logL(MAUS::DataStructure::Global::Track* track);
+
+    /** @brief Fill histogram with value of PID variable of global track
+     *
+     *  @param track The track to have its PID variable value added to PDF
+     */
+    void Fill_Hist(MAUS::DataStructure::Global::Track* track);
+
+
+  protected:
+
+    /// If _nonZeroHistEntries is true, fraction of event to add to all bins
+    double _addToAllBins;
+
+  private:
+
+    /// _var_name + _hyp;
+    std::string _varhyp;
+
+    /// Histogram that forms PDF
+    TH1F *_hist;
+
+    /// For 1D hist, Y bins and values set to 0
+    static const int YnumBins = 0;
+    static const int YminBin = 0;
+    static const int YmaxBin = 0;
+  };
+}
+}
+}
+
+#endif
---
+++ src/common_cpp/Recon/Global/PIDVarB.cc
@@ -0,0 +1,131 @@
+/* This file is part of MAUS: http://  micewww.pp.rl.ac.uk:8080/projects/maus
+ * 
+ * MAUS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * MAUS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MAUS.  If not, see <http://  www.gnu.org/licenses/>.
+ */
+
+#include <string>
+#include <vector>
+#include <utility>
+#include "Recon/Global/PIDVarB.hh"
+#include "Utils/Exception.hh"
+
+namespace MAUS {
+namespace recon {
+namespace global {
+
+  const std::string PIDVarB::VARIABLE = "diffTOF0TOF1vsTrackerMom";
+
+  PIDVarB::PIDVarB(std::string hypothesis, std::string unique_identifier)
+    : PIDBase2D(VARIABLE, hypothesis, unique_identifier,
+                 XminBin, XmaxBin, XnumBins, YminBin, YmaxBin, YnumBins) {
+    _nonZeroHistEntries = true;
+  }
+
+  PIDVarB::PIDVarB(TFile* file, std::string hypothesis)
+    : PIDBase2D(file, VARIABLE, hypothesis) {
+  }
+
+  PIDVarB::~PIDVarB() {}
+
+  std::pair<double, double> PIDVarB::Calc_Var(MAUS::DataStructure::Global::Track* track) {
+    double TOF0_t = 0;
+    double TOF1_t = 0;
+    MAUS::DataStructure::Global::DetectorPoint TOF0_DP =
+      MAUS::DataStructure::Global::kTOF0;
+    MAUS::DataStructure::Global::DetectorPoint TOF1_DP =
+      MAUS::DataStructure::Global::kTOF1;
+    // Get trackpoint array from track
+    global_track_points = track->GetTrackPoints();
+    std::vector<const MAUS::DataStructure::Global::TrackPoint*>
+      ::iterator eachTP;
+    for (eachTP = global_track_points.begin();
+	 eachTP != global_track_points.end(); ++eachTP) {
+      if (!(*eachTP)) continue;
+      if ((*eachTP)->get_mapper_name() == "MapCppGlobalTrackMatching") {
+	if ((*eachTP)->get_detector() == TOF0_DP) {
+	  tof0_track_points.push_back(*eachTP);
+	} else if ((*eachTP)->get_detector() == TOF1_DP) {
+	  tof1_track_points.push_back(*eachTP);
+	} else if ((*eachTP)->get_detector() >=
+		   MAUS::DataStructure::Global::kTracker0_1 &&
+		   (*eachTP)->get_detector() <=
+		   MAUS::DataStructure::Global::kTracker0_5) {
+	  tracker0_track_points.push_back(*eachTP);
+	} else {
+	  continue;
+	}
+      }
+    }
+    if (tof0_track_points.size() > 1 || tof1_track_points.size() > 1) {
+      Squeak::mout(Squeak::error) << "Multiple measurements for TOF0/TOF1" <<
+	" times, Recon::Global::PIDVarB::Calc_Var()" << std::endl;
+      tof0_track_points.clear();
+      tof1_track_points.clear();
+      return std::make_pair(-1, 0);
+    } else if ( tof0_track_points.size() == 0 ||
+		tof1_track_points.size() == 0 ) {
+      Squeak::mout(Squeak::error) << "Missing measurements for " <<
+	"TOF0/TOF1 times, Recon::Global::PIDVarB::Calc_Var()" << std::endl;
+      tof0_track_points.clear();
+      tof1_track_points.clear();
+      return std::make_pair(-1, 0);
+    } else if (tracker0_track_points.size() < 1) {
+      Squeak::mout(Squeak::error) << "Global track contained no scifi" <<
+	" trackpoints, Recon::Global::PIDVarB::Calc_Var()" << std::endl;
+      tracker0_track_points.clear();
+      return std::make_pair(-1, 0);
+    } else {
+      TOF0_t = (tof0_track_points[0])->get_position().T();
+      tof0_track_points.clear();
+      TOF1_t = (tof1_track_points[0])->get_position().T();
+          tof1_track_points.clear();
+      if ( YminBin > (TOF1_t - TOF0_t) || (TOF1_t - TOF0_t) > YmaxBin ) {
+	Squeak::mout(Squeak::error) << "Difference between TOF0 and TOF1 " <<
+	  "times outside of range, Recon::Global::PIDVarB::Calc_Var()" <<
+	  std::endl;
+	return std::make_pair(-1, 0);
+      } else {
+	double tracker0_trackpoint_mom = 0;
+	int tracker0_tp_count = 0;
+	double tracker0_momentum = 0;
+	const MAUS::DataStructure::Global::TrackPoint* tracker0_trackpoint;
+	for (size_t i = 0; i < tracker0_track_points.size(); i++) {
+	  tracker0_trackpoint = tracker0_track_points[i];
+	  if (tracker0_trackpoint) {
+	    TLorentzVector trackpoint_4mom = tracker0_trackpoint->get_momentum();
+	    tracker0_trackpoint_mom +=
+	      sqrt(trackpoint_4mom.Px()*trackpoint_4mom.Px() +
+		   trackpoint_4mom.Py()*trackpoint_4mom.Py() +
+		   trackpoint_4mom.Pz()*trackpoint_4mom.Pz());
+	    tracker0_tp_count++;
+	  } else {
+	    continue;
+	  }
+	}
+	tracker0_track_points.clear();
+	tracker0_momentum = tracker0_trackpoint_mom/tracker0_tp_count;
+	if ( XminBin > tracker0_momentum || tracker0_momentum > XmaxBin ) {
+	  Squeak::mout(Squeak::error) << "Momentum for tracker 0 is outside " <<
+	    "of range, Recon::Global::PIDVarB::Calc_Var()" << std::endl;
+	  return std::make_pair(0, -1);
+	} else {
+	  return std::make_pair(tracker0_momentum, (TOF1_t - TOF0_t));
+	}
+      }
+    }
+  }
+}
+}
+}
+
