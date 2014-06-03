dc28e8c3c9f711b99525623d38b25f45c3b49e92
Chris Rogers <chris.rogers@stfc.ac.uk>
2014-06-03 16:06:35 +0100

    Minor fiddling

--- tests/integration/test_load/process_monitor.py
+++ tests/integration/test_load/process_monitor.py
@@ -58,6 +58,26 @@ def make_graphs(pid_list, wait_time, max_time):
     xboa.Common.make_root_legend(canvas, graph_dict.values())
     return canvas, graph_dict
 
+def monitor_step(delta_t, mem_list, graph_dict):
+    """
+    Make a single step in the monitoring; append output to graph
+    """
+    mem_list_step = []
+    for i, item in enumerate(mem_list[-1]):
+        pid = int(item["pid"])
+        mem = print_mem_usage(pid)
+        mem_p = float(mem["%mem"])
+        index = graph_dict[pid].GetN()
+        if mem and (mem_p > 1e-9 or index == 1):
+            mem_list_step.append(mem)
+            graph_dict[pid].Expand(index+1)
+            graph_dict[pid].SetPoint(index+1,
+                                    delta_t.seconds,
+                                    mem_p)
+        graph_dict[pid].SetLineStyle(i+1)
+        graph_dict[pid].Draw()
+    return mem_list_step
+
 def main(pid_list, title, wait_time, max_time):
     """
     Iterate over list of pids; while pids exist, print cpu usage etc
@@ -69,31 +89,20 @@ def main(pid_list, title, wait_time, max_time):
     start = datetime.datetime.now()
     mem_list = [[{"pid":pid} for pid in pid_list]]
     canvas, graph_dict = make_graphs(pid_list, wait_time, max_time)
-    index = 0
     delta = datetime.timedelta.max
     if max_time:
         delta = datetime.timedelta(0, max_time)
-    while len(mem_list[-1]) > 0 and datetime.datetime.now()-start < delta:
-        index += 1
+    delta_t = datetime.datetime.now()-start
+    while len(mem_list[-1]) > 0 and delta_t < delta:
         time.sleep(wait_time)
-        mem_list_step = []
-        for i, item in enumerate(mem_list[-1]):
-            pid = int(item["pid"])
-            mem = print_mem_usage(pid)
-            if mem:
-                mem_list_step.append(mem)
-                graph_dict[pid].Expand(index)
-                graph_dict[pid].SetPoint(index,
-                    (datetime.datetime.now()-start).seconds, float(mem["%mem"]))
-            graph_dict[pid].SetLineStyle(i+1)
-            graph_dict[pid].Draw()
-            print "draw pid", pid
+        mem_list_step = monitor_step(delta_t, mem_list, graph_dict)
         mem_list.append(mem_list_step)
         print mem_list[-1]
         canvas.Update()
         canvas.Print(str(title)+"_resource_usage.root")
         canvas.Print(str(title)+"_resource_usage.png")
         canvas.Print(str(title)+"_resource_usage.eps")
+        delta_t = datetime.datetime.now()-start
     return mem_list
 
 if __name__ == "__main__":
