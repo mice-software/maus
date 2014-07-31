2c5d3466be35396c5bc5e809cf76c31e75040bbd
Durga Rajaram <durga@fnal.gov>
2014-07-30 21:48:11 -0500

    change default test run-number

--- src/common_py/ConfigurationDefaults.py
+++ src/common_py/ConfigurationDefaults.py
@@ -472,7 +472,7 @@ Enable_klgain_correction = True
 EMR_cabling_file = "/files/cabling/EMRChannelMap.txt"
 
 daq_data_path = '%s/src/input/InputCppDAQData' % os.environ.get("MAUS_ROOT_DIR") # path to daq data. Multiple locations can be specified with a space
-daq_data_file = '02873.003' # file name for daq data; if this is just a integer string, MAUS assumes this is a run number. Multiple entries can be specified separated by a space
+daq_data_file = '05466.001' # file name for daq data; if this is just a integer string, MAUS assumes this is a run number. Multiple entries can be specified separated by a space
 
 maus_version = "" # set at runtime - do not edit this (changes are ignored)
 configuration_file = "" # should be set on the command line only (else ignored)
