759c4ef17042b97808270ae211e171c86d762768
Ryan Bayes <Ryan.Bayes@glasgow.ac.uk>
2015-01-20 14:24:58 +0000

    Expand virtual plane analysis to use multiple input data files for use in batch analysis.

--- bin/examples/plot_virtuals.py
+++ bin/examples/plot_virtuals.py
@@ -2,92 +2,123 @@
 
 
 """
-R. Bayes
-
-A demonstration of extracting information from the special virtual planes
+An attempt at extracting information from the special virtual planes
 """
 
+# pylint: disable = E1101, C0103, C0301, W0611
 
-# pylint: disable = C0103, E1101, C0301, W0611, W0622
-
+import sys
 import ROOT
 import math
 
 import libMausCpp
 
-def main(): 
+def main(args):
 
     """ extract information from the special virtual planes
     from the maus_output.root file in the current directory """
+    
+    if len(args) != 3:
+        print """
+        plot_virtuals.py requires three arguments:
 
-    file = ROOT.TFile("maus_eval20141104.root","R")
+        ./plot_virtuals.py <leading file name> <first index> <last index>
 
-    data = ROOT.MAUS.Data()
-    tree = file.Get("Spill")
-    tree.SetBranchAddress("data", data)
+        """
+        
 
     Bfield = []
     zpos   = []
+    h = ROOT.TH1D("hrate", \
+                  ";Position along Z axis (m);Particles at Virtual Plane", \
+                  240,0,24)
     hf = ROOT.TH2D("hfield", \
                    ";Position along Z axis (m); Field Magnitude", \
-                   120,0,24,100,-5,5)
+                   240,0,24,100,-5,5)
     hp = ROOT.TH2D("hp", \
                    ";Position along Z axis (m); Momentum (MeV/c)", \
-                   120,0,24,100,0,400)
+                   240,0,24,100,0,400)
+    hp200 = ROOT.TH2D("hp200", \
+                      ";Position along Z axis (m); Momentum (MeV/c)", \
+                      240,0,24,100,0,400)
     hr = ROOT.TH2D("hr", \
                    ";Position along Z axis (m); Radial Position (m)", \
-                   120,0,24,100,-.5,.5)
+                   240,0,24,100,0.0, 0.5)
+    hbt = ROOT.TH2D("hbt", \
+                    ";Position along Z axis (m); Transverse #beta", \
+                    240,0,24,100,0, 0.5)
     maxsize = -999
-    
-    for i in range(tree.GetEntries()):
-        tree.GetEntry(i)
-        spill = data.GetSpill()
 
-        if spill.GetDaqEventType() == "physics_event":
-            for j in range(spill.GetMCEvents().size()):
-                # if this is not the largest set of virtual hit go to the
-                # next particle
-                if spill.GetMCEvents()[j].GetPrimary().GetParticleId() != -13:
-                    continue
-                nvirthits = spill.GetMCEvents()[j].GetVirtualHits().size()
-                for k in range(nvirthits):
-                    xtmp = spill.GetMCEvents()[j] \
-                           .GetVirtualHits()[k].GetPosition().x()
-                    ytmp = spill.GetMCEvents()[j] \
-                           .GetVirtualHits()[k].GetPosition().y()
-                    r = math.sqrt(math.pow(xtmp, 2) + math.pow(ytmp, 2))
-                    if r < 1000.:
-                        hf.Fill( \
-                            spill.GetMCEvents()[j] \
-                            .GetVirtualHits()[k].GetPosition().z() / 1000, \
-                            spill.GetMCEvents()[j] \
-                            .GetVirtualHits()[k].GetBField().z() * 1000)
-                        hp.Fill( \
-                            spill.GetMCEvents()[j] \
-                            .GetVirtualHits()[k].GetPosition().z() / 1000, \
-                            spill.GetMCEvents()[j] \
-                            .GetVirtualHits()[k].GetMomentum().mag())
-                        hr.Fill( \
-                            spill.GetMCEvents()[j] \
-                            .GetVirtualHits()[k].GetPosition().z() / 1000, \
-                            spill.GetMCEvents()[j] \
-                            .GetVirtualHits()[k].GetPosition().x() / 1000.)
-                           
-                if maxsize < nvirthits:
-                    maxsize = spill.GetMCEvents()[j].GetVirtualHits().size()
-                    print maxsize
-                    # reinitialize the Bfiled and zpos objects
-                    Bfield = []
-                    zpos   = []
-                    for k in range(maxsize):
-                        Bfield.append( \
-                            spill.GetMCEvents()[j] \
-                            .GetVirtualHits()[k].GetBField().z() \
-                            * 1000)
-                        zpos.append( \
-                            spill.GetMCEvents()[j] \
-                            .GetVirtualHits()[k].GetPosition().z() \
-                            / 1000)
+    for fp in range(int(args[1]), int(args[2])):
+        
+        file = ROOT.TFile(str(args[0])+str(fp)+".root","R")
+        if file.IsZombie():
+            continue
+        data = ROOT.MAUS.Data()
+        tree = file.Get("Spill")
+        if not tree:
+            continue
+        tree.SetBranchAddress("data", data) 
+        print "Evaluating maus_output_"+str(fp)+".root"
+        for i in range(tree.GetEntries()):
+            tree.GetEntry(i)
+            spill = data.GetSpill()
+            
+            if spill.GetDaqEventType() == "physics_event":
+                for j in range(spill.GetMCEvents().size()):
+                    # if this is not the largest set of virtual hit go to the
+                    # next particle
+                    if spill.GetMCEvents()[j].GetPrimary().GetParticleId() != -13:
+                        continue
+                    nvirthits = spill.GetMCEvents()[j].GetVirtualHits().size()
+                    for k in range(nvirthits):
+                        h.Fill(spill.GetMCEvents()[j] \
+                               .GetVirtualHits()[k].GetPosition().z() / 1000)
+                        r = math.sqrt((spill.GetMCEvents()[j] \
+                                       .GetVirtualHits()[k].GetPosition().x())**2 +
+                                      (spill.GetMCEvents()[j] \
+                                       .GetVirtualHits()[k].GetPosition().y())**2 )
+                        if r < 200.:
+                            hf.Fill( \
+                                spill.GetMCEvents()[j] \
+                                .GetVirtualHits()[k].GetPosition().z() / 1000,
+                                spill.GetMCEvents()[j] \
+                                .GetVirtualHits()[k].GetBField().z() * 1000)
+                            hp200.Fill( \
+                                spill.GetMCEvents()[j] \
+                                .GetVirtualHits()[k].GetPosition().z() / 1000,
+                                spill.GetMCEvents()[j] \
+                                .GetVirtualHits()[k].GetMomentum().mag())
+                        hp.Fill(spill.GetMCEvents()[j] \
+                                .GetVirtualHits()[k].GetPosition().z() / 1000,
+                                spill.GetMCEvents()[j] \
+                                .GetVirtualHits()[k].GetMomentum().mag())
+                        hr.Fill(spill.GetMCEvents()[j]\
+                                .GetVirtualHits()[k].GetPosition().z() / 1000,
+                                math.sqrt((spill.GetMCEvents()[j] \
+                                           .GetVirtualHits()[k].GetPosition().x())**2 +
+                                          (spill.GetMCEvents()[j] \
+                                           .GetVirtualHits()[k].GetPosition().y())**2 )/ 1000.)
+                        hbt.Fill(spill.GetMCEvents()[j] \
+                                 .GetVirtualHits()[k].GetPosition().z() / 1000,
+                                 math.sqrt((spill.GetMCEvents()[j] \
+                                            .GetVirtualHits()[k].GetMomentum().x())**2 +
+                                           (spill.GetMCEvents()[j] \
+                                            .GetVirtualHits()[k].GetMomentum().y())**2 )/
+                                 math.sqrt((spill.GetMCEvents()[j] \
+                                            .GetVirtualHits()[k].GetMomentum().mag())**2 \
+                                           + 105.65**2) )
+                    if maxsize < nvirthits:
+                        maxsize = spill.GetMCEvents()[j].GetVirtualHits().size()
+                        print maxsize
+                        # reinitialize the Bfiled and zpos objects
+                        Bfield = []
+                        zpos   = []
+                        for k in range(maxsize):
+                            Bfield.append(spill.GetMCEvents()[j] \
+                                          .GetVirtualHits()[k].GetBField().z() * 1000)
+                            zpos.append(spill.GetMCEvents()[j] \
+                                        .GetVirtualHits()[k].GetPosition().z() / 1000)
 
     g = ROOT.TGraph(maxsize)
     for i in range(maxsize):
@@ -104,20 +135,56 @@ def main():
     hf.Draw("colz")
     c1.Print("Bfield_v_z_hist.eps")
 
+
     c3 = ROOT.TCanvas()
     c3.SetLogz()
-    hp.SetStats(0)
     hp.Draw('colz')
     c3.Print("Momentum_v_z_hist.eps")
+
+    c3_1 = ROOT.TCanvas()
+    hp_px = hp.ProjectionX()
+    hp_px.Draw()
+    c3_1.Print("Momentum_v_z_projx.eps")
+
+    c3_2 = ROOT.TCanvas()
+    hp_pfx = hp.ProfileX()
+    hp_pfx.Draw()
+    c3_2.Print("Momentum_v_z_prflx.eps")
+
+    c3_3 = ROOT.TCanvas()
+    c3_3.SetLogz()
+    hp200.Draw('colz')
+    c3_3.Print("Momentum_v_z_hist_lt200mm.eps")
+
+    c3_4 = ROOT.TCanvas()
+    hp200_px = hp200.ProjectionX()
+    hp200_px.Draw()
+    c3_4.Print("Momentum_v_z_projx_lt200mm.eps")
+
+    c3_5 = ROOT.TCanvas()
+    hp200_pfx = hp200.ProfileX()
+    hp200_pfx.Draw()
+    c3_5.Print("Momentum_v_z_prflx_lt200mm.eps")
+
     
     c4 = ROOT.TCanvas()
     c4.SetLogz()
-    hr.SetStats(0)
     hr.Draw('colz')
     c4.Print("BeamRadius_v_z_hist.eps")
+    
+    c5 = ROOT.TCanvas()
+    c5.SetLogz()
+    hbt.Draw('colz')
+    c5.Print("TransverseBeta_v_z_hist.eps")
+
+    
+    c6 = ROOT.TCanvas()
+    c6.SetLogz()
+    h.Draw()
+    c6.Print("MuonRate_v_z_hist.eps")
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
     
     
