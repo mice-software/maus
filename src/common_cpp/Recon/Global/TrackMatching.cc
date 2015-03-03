8a9dbeb80addc8308aebfb242d81538de6459fd8
Celeste Pidcott <c.e.pidcott@warwick.ac.uk>
2015-03-03 11:40:36 +0000

    Update to TrackMatching to include EMR

--- src/common_cpp/Recon/Global/TrackMatching.cc
+++ src/common_cpp/Recon/Global/TrackMatching.cc
@@ -61,6 +61,23 @@ namespace global {
       }
     }
 
+    // Get EMR trackpoints
+    MAUS::DataStructure::Global::Track* EMRTrack =
+      new MAUS::DataStructure::Global::Track();
+    if (global_event->get_track_points()) {
+      std::vector<MAUS::DataStructure::Global::TrackPoint*>*
+	tempTrackPointArray = global_event->get_track_points();
+      for (unsigned int i = 0; i < tempTrackPointArray->size(); i++) {
+	if (tempTrackPointArray->at(i)->get_detector() == MAUS::DataStructure::Global::kCalorimeter) {
+	  EMRTrack->AddTrackPoint(tempTrackPointArray->at(i));
+	}
+      }
+      if (EMRTrack->GetTrackPoints().size() == 0) {
+	delete EMRTrack;
+	EMRTrack = NULL;
+      }
+    }
+
     std::vector<MAUS::DataStructure::Global::SpacePoint*>
       *GlobalSpacePointArray = global_event->get_space_points();
     MAUS::DataStructure::Global::TrackPArray TOFTrackArray;
@@ -99,6 +116,17 @@ namespace global {
 	    GlobalTrack->AddTrackPoint(tempKLTrackPoint);
 	  }
 	}
+	if (EMRTrack != NULL) {
+	  std::vector<const MAUS::DataStructure::Global::TrackPoint*>
+	    tempEMRTrackPointArray = EMRTrack->GetTrackPoints();
+	  for (unsigned int m = 0; m < tempEMRTrackPointArray.size(); m++) {
+	    MAUS::DataStructure::Global::TrackPoint* tempEMRTrackPoint =
+	      const_cast<MAUS::DataStructure::Global::TrackPoint*>
+	      (tempEMRTrackPointArray[m]);
+	    tempEMRTrackPoint->set_mapper_name(mapper_name);
+	    GlobalTrack->AddTrackPoint(tempEMRTrackPoint);
+	  }
+	}
 	global_event->add_track_recursive(GlobalTrack);
       }
     }
@@ -110,16 +138,27 @@ namespace global {
 	MAUS::DataStructure::Global::Track* GlobalTrack = TOFTrackArray[i]->Clone();
 	GlobalTrack->set_mapper_name(mapper_name);
 	if (KLTrack != NULL) {
-	    std::vector<const MAUS::DataStructure::Global::TrackPoint*>
-	      tempKLTrackPointArray = KLTrack->GetTrackPoints();
-	    for (unsigned int l = 0; l < tempKLTrackPointArray.size(); l++) {
-	      MAUS::DataStructure::Global::TrackPoint* tempKLTrackPoint =
-		const_cast<MAUS::DataStructure::Global::TrackPoint*>
-		(tempKLTrackPointArray[l]);
-	      tempKLTrackPoint->set_mapper_name(mapper_name);
-	      GlobalTrack->AddTrackPoint(tempKLTrackPoint);
-	    }
+	  std::vector<const MAUS::DataStructure::Global::TrackPoint*>
+	    tempKLTrackPointArray = KLTrack->GetTrackPoints();
+	  for (unsigned int l = 0; l < tempKLTrackPointArray.size(); l++) {
+	    MAUS::DataStructure::Global::TrackPoint* tempKLTrackPoint =
+	      const_cast<MAUS::DataStructure::Global::TrackPoint*>
+	      (tempKLTrackPointArray[l]);
+	    tempKLTrackPoint->set_mapper_name(mapper_name);
+	    GlobalTrack->AddTrackPoint(tempKLTrackPoint);
 	  }
+	}
+	if (EMRTrack != NULL) {
+	  std::vector<const MAUS::DataStructure::Global::TrackPoint*>
+	    tempEMRTrackPointArray = EMRTrack->GetTrackPoints();
+	  for (unsigned int m = 0; m < tempEMRTrackPointArray.size(); m++) {
+	    MAUS::DataStructure::Global::TrackPoint* tempEMRTrackPoint =
+	      const_cast<MAUS::DataStructure::Global::TrackPoint*>
+	      (tempEMRTrackPointArray[m]);
+	    tempEMRTrackPoint->set_mapper_name(mapper_name);
+	    GlobalTrack->AddTrackPoint(tempEMRTrackPoint);
+	  }
+	}
 	global_event->add_track_recursive(GlobalTrack);
       }
     }
@@ -129,7 +168,7 @@ namespace global {
     if (ImportedSciFiTrack != NULL && TOFTrackArray.empty()) {
       MAUS::DataStructure::Global::Track* GlobalTrack = ImportedSciFiTrack->Clone();
       GlobalTrack->set_mapper_name(mapper_name);
-     if (KLTrack != NULL) {
+      if (KLTrack != NULL) {
 	std::vector<const MAUS::DataStructure::Global::TrackPoint*>
 	  tempKLTrackPointArray = KLTrack->GetTrackPoints();
 	for (unsigned int l = 0; l < tempKLTrackPointArray.size(); l++) {
@@ -140,129 +179,159 @@ namespace global {
 	  GlobalTrack->AddTrackPoint(tempKLTrackPoint);
 	}
       }
+      if (EMRTrack != NULL) {
+	std::vector<const MAUS::DataStructure::Global::TrackPoint*>
+	  tempEMRTrackPointArray = EMRTrack->GetTrackPoints();
+	for (unsigned int m = 0; m < tempEMRTrackPointArray.size(); m++) {
+	  MAUS::DataStructure::Global::TrackPoint* tempEMRTrackPoint =
+	    const_cast<MAUS::DataStructure::Global::TrackPoint*>
+	    (tempEMRTrackPointArray[m]);
+	  tempEMRTrackPoint->set_mapper_name(mapper_name);
+	  GlobalTrack->AddTrackPoint(tempEMRTrackPoint);
+	}
+      }
       global_event->add_track_recursive(GlobalTrack);
     }
 
-    // Adding global tracks for case where global event contains only a KL track
-    if (ImportedSciFiTrack == NULL && TOFTrackArray.empty() && KLTrack != NULL) {
-      MAUS::DataStructure::Global::Track* GlobalTrack = KLTrack->Clone();
-      GlobalTrack->set_mapper_name(mapper_name);
-      global_event->add_track_recursive(GlobalTrack);
+    // Adding global tracks for case where global event contains only a KL or EMR track
+    if (ImportedSciFiTrack == NULL && TOFTrackArray.empty() &&
+	(KLTrack != NULL || EMRTrack != NULL)) {
+      if (KLTrack != NULL && EMRTrack == NULL) {
+	MAUS::DataStructure::Global::Track* GlobalTrack = KLTrack->Clone();
+	GlobalTrack->set_mapper_name(mapper_name);
+	global_event->add_track_recursive(GlobalTrack);
+      }
+      else if (KLTrack == NULL && EMRTrack != NULL) {
+	MAUS::DataStructure::Global::Track* GlobalTrack = EMRTrack->Clone();
+	GlobalTrack->set_mapper_name(mapper_name);
+	global_event->add_track_recursive(GlobalTrack);
+      } else {
+	MAUS::DataStructure::Global::Track* GlobalTrack = KLTrack->Clone();
+	std::vector<const MAUS::DataStructure::Global::TrackPoint*>
+	  tempEMRTrackPointArray = EMRTrack->GetTrackPoints();
+	for (unsigned int m = 0; m < tempEMRTrackPointArray.size(); m++) {
+	  MAUS::DataStructure::Global::TrackPoint* tempEMRTrackPoint =
+	    const_cast<MAUS::DataStructure::Global::TrackPoint*>
+	    (tempEMRTrackPointArray[m]);
+	  tempEMRTrackPoint->set_mapper_name(mapper_name);
+	  GlobalTrack->AddTrackPoint(tempEMRTrackPoint);
+	}
+	GlobalTrack->set_mapper_name(mapper_name);
+	global_event->add_track_recursive(GlobalTrack);
+      }
     }
   }
 
-  void TrackMatching::MakeTOFTracks(
-      MAUS::GlobalEvent* global_event,
-      std::vector<MAUS::DataStructure::Global::SpacePoint*>
-      *GlobalSpacePointArray,
-      MAUS::DataStructure::Global::TrackPArray& TOFTrackArray) {
+    void TrackMatching::MakeTOFTracks(MAUS::GlobalEvent* global_event,
+				      std::vector<MAUS::DataStructure::Global::SpacePoint*>
+				      *GlobalSpacePointArray,
+				      MAUS::DataStructure::Global::TrackPArray& TOFTrackArray) {
 
-    std::string local_mapper_name = "GlobalTOFTrack";
+      std::string local_mapper_name = "GlobalTOFTrack";
 
-    std::vector<MAUS::DataStructure::Global::TrackPoint*> TOF0tp;
-    std::vector<MAUS::DataStructure::Global::TrackPoint*> TOF1tp;
-    std::vector<MAUS::DataStructure::Global::TrackPoint*> TOF2tp;
+      std::vector<MAUS::DataStructure::Global::TrackPoint*> TOF0tp;
+      std::vector<MAUS::DataStructure::Global::TrackPoint*> TOF1tp;
+      std::vector<MAUS::DataStructure::Global::TrackPoint*> TOF2tp;
 
-    std::vector<MAUS::DataStructure::Global::TrackPoint*> tempTOF0tp;
-    std::vector<MAUS::DataStructure::Global::TrackPoint*> tempTOF2tp;
-    double TOF01offset = 30;
-    double TOF12offset = 35;
-    double allowance = 8.0;
-    for (unsigned int i = 0; i < GlobalSpacePointArray->size(); ++i) {
-      MAUS::DataStructure::Global::SpacePoint* sp = GlobalSpacePointArray->at(i);
-      if (!sp) {
-	continue;
+      std::vector<MAUS::DataStructure::Global::TrackPoint*> tempTOF0tp;
+      std::vector<MAUS::DataStructure::Global::TrackPoint*> tempTOF2tp;
+      double TOF01offset = 30;
+      double TOF12offset = 35;
+      double allowance = 8.0;
+      for (unsigned int i = 0; i < GlobalSpacePointArray->size(); ++i) {
+	MAUS::DataStructure::Global::SpacePoint* sp = GlobalSpacePointArray->at(i);
+	if (!sp) {
+	  continue;
+	}
+	MAUS::DataStructure::Global::TrackPoint* tp0;
+	MAUS::DataStructure::Global::TrackPoint* tp1;
+	MAUS::DataStructure::Global::TrackPoint* tp2;
+	if (sp->get_detector() == MAUS::DataStructure::Global::kTOF0) {
+	  tp0 = new MAUS::DataStructure::Global::TrackPoint(sp);
+	  TOF0tp.push_back(tp0);
+	} else if (sp->get_detector() == MAUS::DataStructure::Global::kTOF1) {
+	  tp1 = new MAUS::DataStructure::Global::TrackPoint(sp);
+	  TOF1tp.push_back(tp1);
+	} else if (sp->get_detector() == MAUS::DataStructure::Global::kTOF2) {
+	  tp2 = new MAUS::DataStructure::Global::TrackPoint(sp);
+	  TOF2tp.push_back(tp2);
+	} else {
+	  continue;
+	}
       }
-      MAUS::DataStructure::Global::TrackPoint* tp0;
-      MAUS::DataStructure::Global::TrackPoint* tp1;
-      MAUS::DataStructure::Global::TrackPoint* tp2;
-      if (sp->get_detector() == MAUS::DataStructure::Global::kTOF0) {
-	tp0 = new MAUS::DataStructure::Global::TrackPoint(sp);
-	TOF0tp.push_back(tp0);
-      } else if (sp->get_detector() == MAUS::DataStructure::Global::kTOF1) {
-	tp1 = new MAUS::DataStructure::Global::TrackPoint(sp);
-	TOF1tp.push_back(tp1);
-      } else if (sp->get_detector() == MAUS::DataStructure::Global::kTOF2) {
-	tp2 = new MAUS::DataStructure::Global::TrackPoint(sp);
-	TOF2tp.push_back(tp2);
-      } else {
-	continue;
+
+      for (unsigned int i = 0; i < TOF1tp.size(); ++i) {
+	MAUS::DataStructure::Global::Track* TOFtrack =
+	  new MAUS::DataStructure::Global::Track();
+	TOFtrack->set_mapper_name(local_mapper_name);
+	for (unsigned int j = 0; j < TOF0tp.size(); ++j) {
+	  if ((TOF1tp[i]->get_position().T() - TOF0tp[j]->get_position().T()) <=
+	      (TOF01offset + allowance) &&
+	      (TOF1tp[i]->get_position().T() - TOF0tp[j]->get_position().T()) >=
+	      (TOF01offset - allowance)) {
+	    tempTOF0tp.push_back(TOF0tp[j]);
+	  }
+	}
+	for (unsigned int k = 0; k < TOF2tp.size(); ++k) {
+	  if ((TOF2tp[k]->get_position().T() - TOF1tp[i]->get_position().T()) <=
+	      (TOF12offset + allowance) &&
+	      (TOF2tp[k]->get_position().T() - TOF1tp[i]->get_position().T()) >=
+	      (TOF12offset - allowance)) {
+	    tempTOF2tp.push_back(TOF2tp[k]);
+	  }
+	}
+	if (tempTOF0tp.size() < 2 && tempTOF2tp.size() < 2) {
+	  TOF1tp[i]->set_mapper_name(local_mapper_name);
+	  TOFtrack->AddTrackPoint(TOF1tp[i]);
+	  global_event->add_track_point_recursive(TOF1tp[i]);
+	  if (tempTOF0tp.size() == 1) {
+	    tempTOF0tp[0]->set_mapper_name(local_mapper_name);
+	    TOFtrack->AddTrackPoint(tempTOF0tp[0]);
+	    global_event->add_track_point_recursive(tempTOF0tp[0]);
+	  }
+	  if (tempTOF2tp.size() == 1) {
+	    tempTOF2tp[0]->set_mapper_name(local_mapper_name);
+	    TOFtrack->AddTrackPoint(tempTOF2tp[0]);
+	    global_event->add_track_point_recursive(tempTOF2tp[0]);
+	  }
+	  TOFTrackArray.push_back(TOFtrack);
+	} else {
+	  Squeak::mout(Squeak::debug) << "Global event returned multiple possible"
+				      << " TOF0 and/or TOF2 space points that "
+				      << "could not be separated into tracks."
+				      << std::endl;
+	}
+
+	tempTOF0tp.clear();
+	tempTOF2tp.clear();
       }
     }
 
-    for (unsigned int i = 0; i < TOF1tp.size(); ++i) {
-      MAUS::DataStructure::Global::Track* TOFtrack =
-	new MAUS::DataStructure::Global::Track();
-      TOFtrack->set_mapper_name(local_mapper_name);
-      for (unsigned int j = 0; j < TOF0tp.size(); ++j) {
-	if ((TOF1tp[i]->get_position().T() - TOF0tp[j]->get_position().T()) <=
-	    (TOF01offset + allowance) &&
-	    (TOF1tp[i]->get_position().T() - TOF0tp[j]->get_position().T()) >=
-	    (TOF01offset - allowance)) {
-	  tempTOF0tp.push_back(TOF0tp[j]);
+    void TrackMatching::MakeKLTracks(MAUS::GlobalEvent* global_event,
+				     std::vector<MAUS::DataStructure::Global::SpacePoint*>
+				     *GlobalSpacePointArray,
+				     MAUS::DataStructure::Global::Track* KLTrack) {
+
+
+      std::string local_mapper_name = "GlobalKLTrack";
+
+      std::vector<MAUS::DataStructure::Global::TrackPoint*> KLtp;
+
+      for (unsigned int i = 0; i < GlobalSpacePointArray->size(); ++i) {
+	MAUS::DataStructure::Global::SpacePoint* sp = GlobalSpacePointArray->at(i);
+	if (!sp) {
+	  continue;
+	}
+	MAUS::DataStructure::Global::TrackPoint* tp;
+	if (sp->get_detector() == MAUS::DataStructure::Global::kCalorimeter) {
+	  tp = new MAUS::DataStructure::Global::TrackPoint(sp);
+	  KLtp.push_back(tp);
+	} else {
+	  continue;
 	}
       }
-      for (unsigned int k = 0; k < TOF2tp.size(); ++k) {
-	if ((TOF2tp[k]->get_position().T() - TOF1tp[i]->get_position().T()) <=
-	    (TOF12offset + allowance) &&
-	    (TOF2tp[k]->get_position().T() - TOF1tp[i]->get_position().T()) >=
-	    (TOF12offset - allowance)) {
-	  tempTOF2tp.push_back(TOF2tp[k]);
-	}
-      }
-      if (tempTOF0tp.size() < 2 && tempTOF2tp.size() < 2) {
-	TOF1tp[i]->set_mapper_name(local_mapper_name);
-	TOFtrack->AddTrackPoint(TOF1tp[i]);
-	global_event->add_track_point_recursive(TOF1tp[i]);
-	if (tempTOF0tp.size() == 1) {
-	  tempTOF0tp[0]->set_mapper_name(local_mapper_name);
-	  TOFtrack->AddTrackPoint(tempTOF0tp[0]);
-	  global_event->add_track_point_recursive(tempTOF0tp[0]);
-	}
-	if (tempTOF2tp.size() == 1) {
-	  tempTOF2tp[0]->set_mapper_name(local_mapper_name);
-	  TOFtrack->AddTrackPoint(tempTOF2tp[0]);
-	  global_event->add_track_point_recursive(tempTOF2tp[0]);
-	}
-	TOFTrackArray.push_back(TOFtrack);
-      } else {
-	Squeak::mout(Squeak::debug) << "Global event returned multiple possible"
-				    << " TOF0 and/or TOF2 space points that "
-	                            << "could not be separated into tracks."
-				    << std::endl;
-      }
 
-      tempTOF0tp.clear();
-      tempTOF2tp.clear();
-    }
-  }
-
-  void TrackMatching::MakeKLTracks(
-      MAUS::GlobalEvent* global_event,
-      std::vector<MAUS::DataStructure::Global::SpacePoint*>
-      *GlobalSpacePointArray,
-      MAUS::DataStructure::Global::Track* KLTrack) {
-
-
-    std::string local_mapper_name = "GlobalKLTrack";
-
-    std::vector<MAUS::DataStructure::Global::TrackPoint*> KLtp;
-
-    for (unsigned int i = 0; i < GlobalSpacePointArray->size(); ++i) {
-      MAUS::DataStructure::Global::SpacePoint* sp = GlobalSpacePointArray->at(i);
-      if (!sp) {
-	continue;
-      }
-      MAUS::DataStructure::Global::TrackPoint* tp;
-      if (sp->get_detector() == MAUS::DataStructure::Global::kCalorimeter) {
-	tp = new MAUS::DataStructure::Global::TrackPoint(sp);
-	KLtp.push_back(tp);
-      } else {
-	continue;
-      }
-    }
-
-    for (unsigned int i = 0; i < KLtp.size(); ++i) {
+      for (unsigned int i = 0; i < KLtp.size(); ++i) {
         KLTrack->set_mapper_name(local_mapper_name);
 	KLtp[i]->set_mapper_name(local_mapper_name);
 	KLTrack->AddTrackPoint(KLtp[i]);
