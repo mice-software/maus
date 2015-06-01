bccde7b94f32abc51a5df7a21020edc073f179e3
Adam Dobbs <phuccj@gmail.com>
2015-06-01 11:47:40 +0100

    Bug fix

--- third_party/bash/21root.bash
+++ third_party/bash/21root.bash
@@ -63,8 +63,9 @@ if [ -n "${MAUS_ROOT_DIR+x}" ]; then
     # weird and wonderful library locations to support multiple architectures.
     # Sticks them in ${x11} directory
     python ${MAUS_THIRD_PARTY}/third_party/install/bin/library_finder.py X11 Xext Xft
-    ./configure --prefix=${MAUS_ROOT_DIR}/third_party/install --disable-xrootd \
-              --enable-gsl-shared --enable-minuit2 \
+    ./configure --prefix=${MAUS_ROOT_DIR}/third_party/install
+              --etcdir=${MAUS_ROOT_DIR}/third_party/install/etc \
+              --disable-xrootd \ --enable-gsl-shared --enable-minuit2 \
               --enable-builtin-freetype \
               --with-gsl-incdir=${MAUS_ROOT_DIR}/third_party/install/include \
               --with-gsl-libdir=${MAUS_ROOT_DIR}/third_party/install/lib \
@@ -83,7 +84,7 @@ if [ -n "${MAUS_ROOT_DIR+x}" ]; then
     echo
     sleep 1
     make -j$MAUS_NUM_THREADS LDFLAGS="-Wl,--no-as-needed" || { echo "FAIL: Failed to configure/make"; exit 1; }
-    # make install
+    make install
     # Create a link to the ROOT directory not based on version number
     ln -fs ${MAUS_ROOT_DIR}/third_party/build/${directory}/ ${MAUS_ROOT_DIR}/third_party/build/root
 	            ################################################## 
