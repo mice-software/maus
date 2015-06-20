1e0047aa21af19ee8b43d6b4abd62393286e4b13
Mariyan Bogomilov <mbogomilov@abv.bg>
2015-06-20 15:26:01 +0300

    Add KL position and error.

--- tests/cpp_unit/DataStructure/KLCellHitTest.cc
+++ tests/cpp_unit/DataStructure/KLCellHitTest.cc
@@ -37,9 +37,22 @@ TEST_F(KLCellHitTestDS, test_parameter_constructor) {
   int charge_product = 1000;
   std::string detector = "kl";
   int part_event_number = 10;
+  double global_pos_x = 10.;
+  double global_pos_y = -88.;
+  double global_pos_z = 100.;
+  double local_pos_x = 20.;
+  double local_pos_y = -44.;
+  double local_pos_z = 200.;
+  double error_x = 100.;
+  double error_y = 22.;
+  double error_z = 20.;
+  bool flag = 1;
 
   KLCellHit *cellhit = new KLCellHit(cell, phys_event_number, charge, charge_product,
-				     detector, part_event_number);
+				     detector, part_event_number,
+				     global_pos_x, global_pos_y, global_pos_z,
+				     local_pos_x, local_pos_y, local_pos_z,
+				     error_x, error_y, error_z, flag );
 
   EXPECT_EQ(cellhit->GetCell(), cell);
   EXPECT_EQ(cellhit->GetPhysEventNumber(), phys_event_number);
@@ -47,6 +60,16 @@ TEST_F(KLCellHitTestDS, test_parameter_constructor) {
   EXPECT_EQ(cellhit->GetChargeProduct(), charge_product);
   EXPECT_EQ(cellhit->GetDetector(), detector);
   EXPECT_EQ(cellhit->GetPartEventNumber(), part_event_number);
+  EXPECT_EQ(cellhit->GetGlobalPosX(), global_pos_x);
+  EXPECT_EQ(cellhit->GetGlobalPosY(), global_pos_y);
+  EXPECT_EQ(cellhit->GetGlobalPosZ(), global_pos_z);
+  EXPECT_EQ(cellhit->GetLocalPosX(), local_pos_x);
+  EXPECT_EQ(cellhit->GetLocalPosY(), local_pos_y);
+  EXPECT_EQ(cellhit->GetLocalPosZ(), local_pos_z);
+  EXPECT_EQ(cellhit->GetErrorX(), error_x);
+  EXPECT_EQ(cellhit->GetErrorY(), error_y);
+  EXPECT_EQ(cellhit->GetErrorZ(), error_z);
+  EXPECT_EQ(cellhit->GetFlag(), flag);
 }
 
 TEST_F(KLCellHitTestDS, test_copy_constructor) {
@@ -56,9 +79,22 @@ TEST_F(KLCellHitTestDS, test_copy_constructor) {
   int charge_product = 1000;
   std::string detector = "kl";
   int part_event_number = 10;
+  double global_pos_x = 10;
+  double global_pos_y = -88.;
+  double global_pos_z = 100;
+  double local_pos_x = 20;
+  double local_pos_y = -44;
+  double local_pos_z = 200;
+  double error_x = 100.;
+  double error_y = 22.;
+  double error_z = 20.;
+  bool flag = 1;
 
   KLCellHit *cellhit1 = new KLCellHit(cell, phys_event_number, charge, charge_product,
-				      detector, part_event_number);
+				      detector, part_event_number,
+				      global_pos_x, global_pos_y, global_pos_z,
+				      local_pos_x, local_pos_y, local_pos_z,
+				      error_x, error_y, error_z, flag );
   KLCellHit *cellhit2 = new KLCellHit(*cellhit1);
   delete cellhit1;
 
@@ -68,6 +104,16 @@ TEST_F(KLCellHitTestDS, test_copy_constructor) {
   EXPECT_EQ(cellhit2->GetChargeProduct(), charge_product);
   EXPECT_EQ(cellhit2->GetDetector(), detector);
   EXPECT_EQ(cellhit2->GetPartEventNumber(), part_event_number);
+  EXPECT_EQ(cellhit2->GetGlobalPosX(), global_pos_x);
+  EXPECT_EQ(cellhit2->GetGlobalPosY(), global_pos_y);
+  EXPECT_EQ(cellhit2->GetGlobalPosZ(), global_pos_z);
+  EXPECT_EQ(cellhit2->GetLocalPosX(), local_pos_x);
+  EXPECT_EQ(cellhit2->GetLocalPosY(), local_pos_y);
+  EXPECT_EQ(cellhit2->GetLocalPosZ(), local_pos_z);
+  EXPECT_EQ(cellhit2->GetErrorX(), error_x);
+  EXPECT_EQ(cellhit2->GetErrorY(), error_y);
+  EXPECT_EQ(cellhit2->GetErrorZ(), error_z);
+  EXPECT_EQ(cellhit2->GetFlag(), flag);
 }
 
 TEST_F(KLCellHitTestDS, test_assignment_operator) {
@@ -77,9 +123,22 @@ TEST_F(KLCellHitTestDS, test_assignment_operator) {
   int charge_product = 1000;
   std::string detector = "kl";
   int part_event_number = 10;
+  double global_pos_x = 10;
+  double global_pos_y = -88.;
+  double global_pos_z = 100;
+  double local_pos_x = 20;
+  double local_pos_y = -44;
+  double local_pos_z = 200;
+  double error_x = 100.;
+  double error_y = 22.;
+  double error_z = 20.;
+  bool flag = 1;
 
   KLCellHit *cellhit1 = new KLCellHit(cell, phys_event_number, charge, charge_product,
-				      detector, part_event_number);
+				      detector, part_event_number,
+				      global_pos_x, global_pos_y, global_pos_z,
+				      local_pos_x, local_pos_y, local_pos_z,
+				      error_x, error_y, error_z, flag );
   KLCellHit *cellhit2 = new KLCellHit();
   *cellhit2 = *cellhit1;
   delete cellhit1;
@@ -90,6 +149,16 @@ TEST_F(KLCellHitTestDS, test_assignment_operator) {
   EXPECT_EQ(cellhit2->GetChargeProduct(), charge_product);
   EXPECT_EQ(cellhit2->GetDetector(), detector);
   EXPECT_EQ(cellhit2->GetPartEventNumber(), part_event_number);
+  EXPECT_EQ(cellhit2->GetGlobalPosX(), global_pos_x);
+  EXPECT_EQ(cellhit2->GetGlobalPosY(), global_pos_y);
+  EXPECT_EQ(cellhit2->GetGlobalPosZ(), global_pos_z);
+  EXPECT_EQ(cellhit2->GetLocalPosX(), local_pos_x);
+  EXPECT_EQ(cellhit2->GetLocalPosY(), local_pos_y);
+  EXPECT_EQ(cellhit2->GetLocalPosZ(), local_pos_z);
+  EXPECT_EQ(cellhit2->GetErrorX(), error_x);
+  EXPECT_EQ(cellhit2->GetErrorY(), error_y);
+  EXPECT_EQ(cellhit2->GetErrorZ(), error_z);
+  EXPECT_EQ(cellhit2->GetFlag(), flag);
 }
 
 
@@ -100,6 +169,16 @@ TEST_F(KLCellHitTestDS, test_setters_getters) {
   int charge_product = 1000;
   std::string detector = "kl";
   int part_event_number = 10;
+  double global_pos_x = 10;
+  double global_pos_y = -88.;
+  double global_pos_z = 100;
+  double local_pos_x = 20;
+  double local_pos_y = -44;
+  double local_pos_z = 200;
+  double error_x = 100.;
+  double error_y = 22.;
+  double error_z = 20.;
+  bool flag = 1;
   KLCellHit *cellhit = new KLCellHit();
 
   cellhit->SetCell(cell);
@@ -108,6 +187,16 @@ TEST_F(KLCellHitTestDS, test_setters_getters) {
   cellhit->SetChargeProduct(charge_product);
   cellhit->SetDetector(detector);
   cellhit->SetPartEventNumber(part_event_number);
+  cellhit->SetGlobalPosX(global_pos_x);
+  cellhit->SetGlobalPosY(global_pos_y);
+  cellhit->SetGlobalPosZ(global_pos_z);
+  cellhit->SetLocalPosX(local_pos_x);
+  cellhit->SetLocalPosY(local_pos_y);
+  cellhit->SetLocalPosZ(local_pos_z);
+  cellhit->SetErrorX(local_pos_x);
+  cellhit->SetErrorY(local_pos_y);
+  cellhit->SetErrorZ(local_pos_z);
+  cellhit->SetFlag(flag);
 
   EXPECT_EQ(cellhit->GetCell(), cell);
   EXPECT_EQ(cellhit->GetPhysEventNumber(), phys_event_number);
@@ -115,6 +204,16 @@ TEST_F(KLCellHitTestDS, test_setters_getters) {
   EXPECT_EQ(cellhit->GetChargeProduct(), charge_product);
   EXPECT_EQ(cellhit->GetDetector(), detector);
   EXPECT_EQ(cellhit->GetPartEventNumber(), part_event_number);
+  EXPECT_EQ(cellhit->GetGlobalPosX(), global_pos_x);
+  EXPECT_EQ(cellhit->GetGlobalPosY(), global_pos_y);
+  EXPECT_EQ(cellhit->GetGlobalPosZ(), global_pos_z);
+  EXPECT_EQ(cellhit->GetLocalPosX(), local_pos_x);
+  EXPECT_EQ(cellhit->GetLocalPosY(), local_pos_y);
+  EXPECT_EQ(cellhit->GetLocalPosZ(), local_pos_z);
+  EXPECT_EQ(cellhit->GetErrorX(), error_x);
+  EXPECT_EQ(cellhit->GetErrorY(), error_y);
+  EXPECT_EQ(cellhit->GetErrorZ(), error_z);
+  EXPECT_EQ(cellhit->GetFlag(), flag);
 }
 
 } // ~namespace MAUS
