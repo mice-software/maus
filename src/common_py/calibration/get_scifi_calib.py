75ee6d1f6d1a33a836ff4f054c5430ba88275d1e
cheid001 <>
2015-09-15 18:19:31 +0100

    update to meet style tests

--- src/common_py/calibration/get_scifi_calib.py
+++ src/common_py/calibration/get_scifi_calib.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+#pylint: disable=C0103
+#pylint: disable=R0201
 """ 
 Read Tracker calibrations from the CDB
 Includes ADC calibrations and channel mapping
@@ -12,86 +14,93 @@ from Configuration import Configuration
 
 
 class GetTrackerCalib:
+    """
+    Connects to the CDB and downloads tracker
+    calibration, mapping, and bad channel lists
+    """
 
-  def __init__(self):
-    print "Init running"
-    self.config = json.loads(Configuration().getConfigJSON())
-    calib_cdb_url = self.config['cdb_download_url'] + 'calibration?wsdl'
-    self._calib = cdb.Calibration()
-    self._calib.set_url(calib_cdb_url)
-    print "Connected to calibration"
-    cable_cdb_url = self.config['cdb_download_url'] + 'cabling?wsdl'
-    self._cable = cdb.Cabling()
-    self._cable.set_url(cable_cdb_url)
-    print "Connected to cabling"
-    
-    try:
-      cdb.Calibration().get_status()
-      cdb.Cabling().get_status()
-    except CdbPermanentError:
-      raise CdbPermanentError("CDB error")
-      
-#########################################################################################
-# Needs datafile set to Run/Date/Current; date formate: "1984-09-14 00:10:00.0"         #
-#########################################################################################
-  def Process(self):
-    print "Starting process"
-    method = self.config["SciFiCalibMethod"]
-    input = self.config["SciFiCalibSrc"]
-    if method == "Date":
-      self.Date(input)
-    if method == "Run":
-      self.Date(self.Convert_Run(input))
-    if method == "Current":
-      self.Current()
-      
-#########################################################################################
-  def Current(self):
-    calib = self._calib.get_current_calibration("Trackers","trackers")
-    badchan = self._calib.get_current_calibration("Trackers","bad_chan")
-    cable = self._cable.get_current_cabling("Trackers")
-    
-    self.Output(calib, badchan, cable)
+    def __init__(self):
+        print "Init running"
+        self.config = json.loads(Configuration().getConfigJSON())
+        calib_cdb_url = self.config['cdb_download_url'] + 'calibration?wsdl'
+        self._calib = cdb.Calibration()
+        self._calib.set_url(calib_cdb_url)
+        print "Connected to calibration"
+        cable_cdb_url = self.config['cdb_download_url'] + 'cabling?wsdl'
+        self._cable = cdb.Cabling()
+        self._cable.set_url(cable_cdb_url)
+        print "Connected to cabling"
 
-#########################################################################################
-  def Date(self, date):
-    calib = self._calib.get_calibration_for_date("Trackers", date, "trackers")
-    badchan = self._calib.get_calibration_for_date("Trackers", date, "bad_chan")
-    cable = self._cable.get_cabling_for_date("Trackers", date)
-    
-    self.Output(calib, badchan, cable)
-    
-#########################################################################################
-# If new calibration files are uploaded this function needs updated                     #
-#########################################################################################
-  def Convert_Run(self, input):
-    run = int(input)
-    if run<6664:
-      print "No tracker configuration data for this run"
-    if run<=6664 and run>7273:
-      date="2015-06-02 00:10:00.0"
-    if run<=7273 and run>7294:
-      date="2015-07-22 00:10:00.0"
-    if run<=7294:
-      date="2015-08-19 00:10:00.0"
-    return date
-  
-#########################################################################################
-  def Output(self, cal, bc, cab):
-    path = os.environ["MAUS_ROOT_DIR"]
-    path += "/files/calibration/"
-    print path
-    calib_out = open(path+"scifi_calibraion.txt","w")
-    badchan_out = open(path+"scifi_bad_channels.txt","w")
-    cable_out = open(path+"scifi_cabling.txt","w")
+    def Process(self):
+        """
+        Needs datafile set to Run/Date/Current; date formate: "1984-09-14 00:10:00.0"
+        """
+        print "Starting process"
+        method = self.config["SciFiCalibMethod"]
+        _input = self.config["SciFiCalibSrc"]
+        if method == "Date":
+            self.Date(_input)
+        if method == "Run":
+            self.Date(self.Convert_Run(_input))
+        if method == "Current":
+            self.Current()
 
-    calib_out.write(cal)
-    badchan_out.write(bc)
-    cable_out.write(cab)
+    def Current(self):
+        """
+        Collects current calibration info
+        """
+        calib = self._calib.get_current_calibration("Trackers","trackers")
+        badchan = self._calib.get_current_calibration("Trackers","bad_chan")
+        cable = self._cable.get_current_cabling("Trackers")
 
-    calib_out.close()
-    badchan_out.close()
-    cable_out.close()
-    
+        self.Output(calib, badchan, cable)
+
+    def Date(self, date):
+        """
+        Collects calibration info by date
+        """
+        calib = self._calib.get_calibration_for_date\
+                ("Trackers", date, "trackers")
+        badchan = self._calib.get_calibration_for_date\
+                  ("Trackers", date, "bad_chan")
+        cable = self._cable.get_cabling_for_date\
+                ("Trackers", date)
+
+        self.Output(calib, badchan, cable)
+
+    def Convert_Run(self, _input):
+        """
+        If new calibration files are uploaded this function needs updated
+        """
+        run = int(_input)
+        if run < 6664:
+            print "No tracker configuration data for this run"
+        if run <= 6664 and run > 7273:
+            date = "2015-06-02 00:10:00.0"
+        if run <= 7273 and run > 7294:
+            date = "2015-07-22 00:10:00.0"
+        if run <= 7294:
+            date = "2015-08-19 00:10:00.0"
+        return date
+
+    def Output(self, CAL, BC, CAB):
+        """
+        Outputs calibration files
+        """
+        path = os.environ["MAUS_ROOT_DIR"]
+        path += "/files/calibration/"
+        print path
+        calib_out = open(path+"scifi_calibraion.txt","w")
+        badchan_out = open(path+"scifi_bad_channels.txt","w")
+        cable_out = open(path+"scifi_cabling.txt","w")
+
+        calib_out.write(CAL)
+        badchan_out.write(BC)
+        cable_out.write(CAB)
+
+        calib_out.close()
+        badchan_out.close()
+        cable_out.close()
+
 if __name__ == "__main__":
-  GetTrackerCalib().Process()
+    GetTrackerCalib().Process()
