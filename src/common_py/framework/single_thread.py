397c856125f5a507c33a181cffb3aa46ab24935e
Yordan Karadzhov <yordan.karadzhov@cern.ch>
2015-10-26 07:11:13 +0100

    Fixes issue #1776. 2nd attempt.

--- src/common_py/framework/single_thread.py
+++ src/common_py/framework/single_thread.py
@@ -189,9 +189,9 @@ class PipelineSingleThreadDataflowExecutor: # pylint: disable=R0902
                 self.run_number = current_run_number
             # now transform the event and reduce it
             event = self.transformer.process(event)
-            merged_event = self.merger.process(event)
+            self.merger.process(event)
         # done with tranform-merge, now write it out
-        self.outputer.save(merged_event)
+        self.outputer.save(event)
         # if we converted to a different representation, delete the old one
         try:
             maus_cpp.converter.del_data_repr(event)
