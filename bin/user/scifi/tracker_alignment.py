16afbb28281972a5541774b02bcec39dc39bd608
Adam Dobbs adobbs@physics.org <>
2016-01-21 17:34:21 +0000

    More scifi optimization work

--- bin/user/scifi/tracker_alignment.py
+++ bin/user/scifi/tracker_alignment.py
@@ -5,7 +5,7 @@
 import sys
 import ROOT
 import libMausCpp #pylint: disable = W0611
-import MausReducedData as mrd
+import MausReducedData
 
 # pylint: disable = E1101
 # pylint: disable = R0902
@@ -18,8 +18,94 @@ import MausReducedData as mrd
 # pylint: disable = W0612
 # pylint: disable = C0103
 
-class Alignment:
-    """ Class for checking tracker alignment vs TOFs"""
+class KalmanAlignment:
+    """ Class for checking tracker Kalman alignment vs TOFs"""
+
+    def __init__(self):
+        """ Constructor """
+        self.num_good_events = 0
+        self.spoints_npe_cut = 0
+        self.nbins = 100
+        self.llimit = -400
+        self.ulimit = 400
+        self.stations = []
+        self.histos_x = []
+        self.histos_y = []
+        self.histos_xy = []
+
+        self.tof1 = MausReducedData.TOF()
+        self.tof2 = MausReducedData.TOF()
+
+        for i in range(2):
+            for j in range(1, 6):
+                self.stations.append(MausReducedData.TrackerStation(i, j))
+                for k in range(3):
+                self.stations.append(MausReducedData.TrackerStation(i, j))
+                self.histos_x.append(ROOT.TH1D("hx_" + str(i) + "_" + str(j)
+                    + "_" + str(k),
+                    "x residuals T" + str(i) + " S" + str(j) + " P" + str(k),
+                    self.nbins, self.llimit, self.ulimit))
+                self.histos_y.append(ROOT.TH1D("hy_" + str(i) + "_" + str(j)
+                    + "_" + str(k),
+                    "y residuals T" + str(i) + " S" + str(j) + " P" + str(k),
+                    self.nbins, self.llimit, self.ulimit))
+                self.histos_xy.append(ROOT.TH2D("hxy_" + str(i) + "_" + str(j)
+                    + "_" + str(k),
+                    "x-y residuals T" + str(i) + " S" + str(j) + " P" + str(k),
+                    self.nbins, self.llimit, self.ulimit,
+                    self.nbins, self.llimit, self.ulimit))
+
+        self.canvas_x = \
+            ROOT.TCanvas("canvas_x", "Trackpoint x Residuals", 0, 0, 700, 500)
+        self.canvas_x.Divide(15, 2)
+        self.canvas_y = \
+            ROOT.TCanvas("canvas_y", "Trackpoint y Residuals", 0, 0, 700, 500)
+        self.canvas_y.Divide(15, 2)
+        self.canvas_xy = \
+            ROOT.TCanvas("canvas_xy", "Trackpoint xy Residuals", 0, 0, 700, 500)
+        self.canvas_xy.Divide(15, 2)
+
+    def clear_data(self):
+        """ Clear internal detector data containers"""
+        for stat in self.stations:
+            stat.clear_spacepoint_data()
+        self.tof1.clear()
+        self.tof2.clear()
+
+    def process(self, spill):
+        """ Accumulate a spill of data into internal containers and process """
+        for i in range(spill.GetReconEvents().size()):
+            self.clear_data()
+            tk_evt = spill.GetReconEvents()[i].GetSciFiEvent()
+            tof_evt = spill.GetReconEvents()[i].GetTOFEvent()
+
+            # Only look at events with exactly 1 spacepoint in tof1, tof2
+            # and each tracker station
+            good_event = False
+            self.tof1.accumulate_data(tof_evt, 1)
+            self.tof2.accumulate_data(tof_evt, 2)
+            if (self.tof1.num_spoints == 1 and self.tof2.num_spoints == 1):
+                good_event = True
+            if good_event:
+                for stat in self.stations:
+                    stat.accumulate_data(tk_evt)
+                    if self.measure == "spacepoints":
+                        if len(stat.spoints_global_x) != 1:
+                            good_event = False
+                            break
+                    elif self.measure == "pr_straight_seeds":
+                        if len(stat.straight_seeds_global_x) != 1:
+                            good_event = False
+                            break
+                    else:
+                        print "WARNING: Bad measure set"
+                        return False
+            if good_event:
+                self.num_good_events += 1
+                self.fill_histograms(tk_evt)
+
+class PatternRecognitionAlignment:
+    """ Class for checking tracker Pat Rec alignment vs TOFs"""
 
     def __init__(self):
         """ Constructor """
@@ -39,7 +125,7 @@ class Alignment:
         self.histos_my = []
         for i in range(2):
             for j in range(1, 6):
-                self.stations.append(mrd.TrackerStation(i, j))
+                self.stations.append(MausReducedData.TrackerStation(i, j))
                 self.histos_x.append(ROOT.TH1D("hx_" + str(i) + "_" + str(j),
                     "x residuals T" + str(i) + " S" + str(j),
                     self.nbins, self.llimit, self.ulimit))
@@ -63,9 +149,9 @@ class Alignment:
                 "my residuals T" + str(i),
                 self.nbins, -0.2, 0.2))
 
-        self.tof0 = mrd.TOF()
-        self.tof1 = mrd.TOF()
-        self.tof2 = mrd.TOF()
+        self.tof0 = MausReducedData.TOF()
+        self.tof1 = MausReducedData.TOF()
+        self.tof2 = MausReducedData.TOF()
 
         self.canvas_x = \
             ROOT.TCanvas("canvas_x", "Spacepoint x Residuals", 0, 0, 700, 500)
@@ -145,11 +231,19 @@ class Alignment:
         self.canvas_mxy.Update()
 
     def fill_histograms(self, tk_evt):
-        """ Select events with spoints in all tracker stations and tof1 and tof2
-            Draw a line between tof1 and tof2 and measure
-            tracker residuals from it """
+        """ Fill all the histograms """
 
         # Find tof line in z - x and z - y, t = gradient, c = intercept
+        c_zx, t_zx, c_zy, t_zy = self.draw_tof12_line()
+
+        # Fill the straight pat rec track parameter residuals histos
+        self.fill_spr_parameter_histograms(t_zx, t_zy, tk_evt.straightprtracks())
+
+        # Now find the straight pat rec seed residuals and fill the histos
+        self.fill_spr_seed_histograms(c_zx, t_zx, c_zy, t_zy)
+
+    def draw_tof12_line(self):
+        """ Find tof line in z - x and z - y, t = gradient, c = intercept """
         dx = self.tof2.spoints_global_x[0] - self.tof1.spoints_global_x[0]
         dy = self.tof2.spoints_global_y[0] - self.tof1.spoints_global_y[0]
         dz = self.tof2.spoints_global_z[0] - self.tof1.spoints_global_z[0]
@@ -161,17 +255,20 @@ class Alignment:
         c_zy = self.tof1.spoints_global_y[0] - \
                 (t_zy * self.tof1.spoints_global_z[0])
 
-        # Find the residuals of the gradients of the PatRec fits
-        # to the TOF line
-        trks = tk_evt.straightprtracks()
-        for trk in trks:
+        return [c_zx, t_zx, c_zy, t_zy]
+
+    def fill_spr_parameter_histograms(self, t_zx, t_zy, tracks):
+        """ Find the residuals of the gradients of the PatRec fits
+            to the TOF line """
+        for trk in tracks:
             tracker_id = trk.get_tracker()
             dmx = t_zx - trk.get_global_mx()
             dmy = t_zy - trk.get_global_my()
             self.histos_mx[tracker_id].Fill(dmx)
             self.histos_my[tracker_id].Fill(dmy)
 
-        # Now find the seed spacepoint residuals and fill the histos
+    def fill_spr_seed_histograms(self, c_zx, t_zx, c_zy, t_zy):
+        """ Find the straight pat rec seed residuals and fill the histos """
         for i in range(len(self.stations)):
             stat = self.stations[i]
             hx = self.histos_x[i]
@@ -219,7 +316,7 @@ def main(file_name):
     tree.SetBranchAddress("data", data)
 
     # Set up Aligment object
-    alignment = Alignment()
+    alignment = PatternRecognitionAlignmentAlignment()
     alignment.measure = "pr_straight_seeds"
 
     # Loop over spills
