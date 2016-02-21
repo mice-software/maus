26a5136eda431f6dce927009b34d7802b7e709a6
francois.drielsma@gmail.com <>
2016-02-21 16:31:01 +0000

    Speed up EMRGeometryMap

--- src/common_cpp/Utils/EMRGeometryMap.cc
+++ src/common_cpp/Utils/EMRGeometryMap.cc
@@ -24,15 +24,11 @@ EMRGeometryMap::EMRGeometryMap() {
 }
 
 EMRGeometryMap::~EMRGeometryMap() {
-
-  _Ckey.clear();
-  _Plkey.clear();
-  _Pgkey.clear();
 }
 
 bool EMRGeometryMap::InitializeFromCards(Json::Value configJSON) {
 
-  // Fetch variables
+  // Fetch default variables
   _number_of_planes = configJSON["EMRnumberOfPlanes"].asInt();
   _number_of_bars = configJSON["EMRnumberOfBars"].asInt();
 
@@ -43,9 +39,6 @@ bool EMRGeometryMap::InitializeFromCards(Json::Value configJSON) {
 
   _globalPosEMR = Hep3Vector(0., 0., 0.);
   _globalRotEMR = HepRotation(0., 0., 0.);
-
-  // Fill the vector containing all EMR channel keys.
-  this->MakeEMRChannelKeys();
 
   // Find and load geometry file, return false if it fails
   char* pMAUS_ROOT_DIR = getenv("MAUS_ROOT_DIR");
@@ -65,15 +58,6 @@ bool EMRGeometryMap::Initialize(std::string geomtetryFile) {
   return this->LoadGeometryFile(geomtetryFile);
 }
 
-int EMRGeometryMap::MakeEMRChannelKeys() {
-
-  for (int iPlane = 0; iPlane < _number_of_planes; iPlane++)
-    for (int iBar = 1; iBar < _number_of_bars; iBar++)
-      _Ckey.push_back(EMRChannelKey(iPlane, iPlane%2, iBar, "emr"));
-
-  return _Ckey.size();
-}
-
 bool EMRGeometryMap::LoadGeometryFile(std::string geometryFile) {
 
   MiceModule* geo_module = new MiceModule(geometryFile);
@@ -83,39 +67,23 @@ bool EMRGeometryMap::LoadGeometryFile(std::string geometryFile) {
   if ( emr_modules.size() ) {
 
     // There is only one parent module for the EMR
+    // Set the global position and rotation of the EMR mother volume
     _globalPosEMR = emr_modules[0]->globalPosition();
     _globalRotEMR = (emr_modules[0]->globalRotation()).inverse();
 
-    double bar_width = emr_modules[0]->propertyDouble("BarWidth"); // mm
-    double bar_height = emr_modules[0]->propertyDouble("BarHeight"); // mm
-    double bar_length = emr_modules[0]->propertyDouble("BarLength"); // mm
+    // Set the dimensions of EMR bars and the size of the gaps
+    _bar_width = emr_modules[0]->propertyDouble("BarWidth"); // mm
+    _bar_height = emr_modules[0]->propertyDouble("BarHeight"); // mm
+    _bar_length = emr_modules[0]->propertyDouble("BarLength"); // mm
+    _D = ThreeVector(_bar_length, _bar_width, _bar_height);
+
     _gap = emr_modules[0]->propertyDouble("Gap"); // mm
 
-    _localStartEMR = ThreeVector(-(_number_of_bars/2)*(.5*bar_width+_gap),
-		     		 -(_number_of_bars/2)*(.5*bar_width+_gap),
-		     		 -(_number_of_planes/2)*(bar_height+_gap));
+    // Set the limits of the fiducial volume of the EMR
+    _localStartEMR = ThreeVector(-(_number_of_bars/2)*(.5*_bar_width+_gap),
+		     		 -(_number_of_bars/2)*(.5*_bar_width+_gap),
+		     		 -(_number_of_planes/2)*(_bar_height+_gap));
     _localEndEMR = -_localStartEMR;
-
-    // Set the positions of the bar in local and global coordinates
-    for (int iPlane = 0; iPlane < _number_of_planes; iPlane++) {
-      for (int iBar = 1; iBar < _number_of_bars; iBar++) {
-        double x = ((iPlane+1)%2)*(_localStartEMR.x()+iBar*(.5*bar_width+_gap));
-        double y = (iPlane%2)*(_localStartEMR.y()+iBar*(.5*bar_width+_gap));
-        double z = _localStartEMR.z()+(iPlane+(1.+iBar%2)/3)*(bar_height+_gap);
-
-        ThreeVector localPos(x, y, z);
-        _Plkey.push_back(localPos);
-
-        Hep3Vector gPos(x, y, z);
-        gPos.transform(_globalRotEMR);
-	gPos += _globalPosEMR;
-        ThreeVector globalPos(gPos.x(), gPos.y(), gPos.z());
-        _Pgkey.push_back(globalPos);
-      }
-
-      ThreeVector dimensions(bar_length, bar_width, bar_height);
-      _D = dimensions;
-    }
   } else {
 
     // Warn that there will not be any global positions
@@ -124,60 +92,36 @@ bool EMRGeometryMap::LoadGeometryFile(std::string geometryFile) {
     << "as a sensitive detector !!! Global positions will not be reconstructed."
     << std::endl;
 
-    // Fill using the configuration defaults
+    // Set the dimensions of EMR bars and the size of the gaps
+    _D = ThreeVector(_bar_length, _bar_width, _bar_height);
+
+    // Set the limits of the fiducial volume of the EMR
     _localStartEMR = ThreeVector(-(_number_of_bars/2)*(.5*_bar_width+_gap),
 			   	 -(_number_of_bars/2)*(.5*_bar_width+_gap),
 			   	 -(_number_of_planes/2)*(_bar_height+_gap));
     _localEndEMR = -_localStartEMR;
-
-    for (int iPlane = 0; iPlane < _number_of_planes; iPlane++) {
-      for (int iBar = 1; iBar < _number_of_bars; iBar++) {
-        double x = ((iPlane+1)%2)*(_localStartEMR.x()+iBar*(.5*_bar_width+_gap));
-        double y = (iPlane%2)*(_localStartEMR.y()+iBar*(.5*_bar_width+_gap));
-        double z = _localStartEMR.z()+(iPlane+(1.+iBar%2)/3)*(_bar_height+_gap);
-
-        ThreeVector localPos(x, y, z);
-        ThreeVector globalPos(NOGEO, NOGEO, NOGEO);
-
-        _Plkey.push_back(localPos);
-        _Pgkey.push_back(globalPos);
-      }
-
-      ThreeVector dimensions(_bar_length, _bar_width, _bar_height);
-      _D = dimensions;
-    }
   }
 
   return true;
 }
 
-int EMRGeometryMap::FindEMRChannelKey(EMRChannelKey key) const {
-  for (unsigned int i = 0; i < _Ckey.size(); ++i )
-    if ( _Ckey.at(i) == key )
-      return i;
-
-  return NOGEO;
-}
-
 ThreeVector EMRGeometryMap::LocalPosition(EMRChannelKey key) const {
 
-  int n = FindEMRChannelKey(key);
-  ThreeVector pos;
-  if ( n != NOGEO )
-    return _Plkey[n];
+  int xPlane = key.GetPlane();
+  int xBar = key.GetBar();
 
-  return ThreeVector(NOGEO, NOGEO, NOGEO);
+  double x = ((xPlane+1)%2)*(_localStartEMR.x()+xBar*(.5*_bar_width+_gap));
+  double y = (xPlane%2)*(_localStartEMR.y()+xBar*(.5*_bar_width+_gap));
+  double z = _localStartEMR.z()+(xPlane+(1.+xBar%2)/3)*(_bar_height+_gap);
+
+  return ThreeVector(x, y, z);
 }
 
 ThreeVector EMRGeometryMap::GlobalPosition(EMRChannelKey key) const {
 
-  int n = FindEMRChannelKey(key);
+  ThreeVector lPos = LocalPosition(key);
 
-  ThreeVector pos;
-  if (n != NOGEO)
-    return _Pgkey[n];
-
-  return ThreeVector(NOGEO, NOGEO, NOGEO);
+  return MakeGlobal(lPos);
 }
 
 ThreeVector EMRGeometryMap::MakeGlobal(ThreeVector pos) const {
@@ -187,22 +131,5 @@ ThreeVector EMRGeometryMap::MakeGlobal(ThreeVector pos) const {
   hepPos += _globalPosEMR;
 
   return ThreeVector(hepPos.x(), hepPos.y(), hepPos.z());
-}
-
-void EMRGeometryMap::Print() {
-  std::cerr << "====================== EMRGeometryMap =========================" << std::endl;
-  std::cerr << " Number of channels : " << _Ckey.size() << std::endl;
-
-  for (unsigned int i = 0; i < _Ckey.size(); i++) {
-    std::cerr << "Channel: " << _Ckey[i] << std::endl
-	      << "Local Pos.: " << _Plkey[i].x() << "  " << _Plkey[i].y()
-	      << "  " << _Plkey[i].z() << std::endl
-	      << "Global Pos.: " << _Pgkey[i].x() << "  " << _Pgkey[i].y()
-	      << "  " << _Pgkey[i].z() << std::endl
-	      << "Dimensions: " << _D.x() << "  " << _D.y()
-	      << "  " << _D.z() << std::endl;
-  }
-
-  std::cerr << "===================================================================" << std::endl;
 }
 } // namespace MAUS
