e1854da8285a2d07e4530febcd7ca39e0750d6a0
Adam Dobbs <phuccj@gmail.com>
2016-04-19 16:38:11 +0100

    Test system updates

--- tests/application_tests.bash
+++ tests/application_tests.bash
@@ -2,15 +2,33 @@
 # force nosetests here otherwise use easy_install default python - which is 
 # hard coded to whatever easy_install was set up with (not right if we move code)
 # Issue #819
+set -e
 
 if [ -z "${MAUS_ROOT_DIR}" ]; then  # see if the variable exists yet
     echo "ERROR: \$MAUS_ROOT_DIR not set"
     exit 1;
 fi
 
+echo
+echo "INFO: Running the integration tests"
+echo
 
 MAUS_TEST_PLOT_DIR=${MAUS_ROOT_DIR}/tests/integration/plots/ \
 python ${MAUS_THIRD_PARTY}/third_party/install/bin/nosetests -v \
 --exclude-dir tests/integration/test_simulation/test_physics_model_full \
+--exclude-dir tests/integration/test_utilities/test_execute_against_data \
 ${MAUS_ROOT_DIR}/tests/integration
 
+# We run test_execute_against_data separately so captured logging can be turned off
+# to stop the splurge of suds output when the preprod cdb is unreachable
+
+echo
+echo "INFO: Running test_execute_against_data"
+echo
+
+python ${MAUS_THIRD_PARTY}/third_party/install/bin/nosetests -v --nologcapture \
+${MAUS_ROOT_DIR}/tests/integration/test_utilities/test_execute_against_data
+
+echo
+echo "INFO: Integration tests complete"
+echo
