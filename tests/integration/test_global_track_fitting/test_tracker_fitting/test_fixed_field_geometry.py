1778b86a1633dabc718a0c3bc8e43504febd2747
Chris Rogers <chris.rogers@stfc.ac.uk>
2016-06-08 15:41:21 +0100

    fiddling

--- tests/integration/test_global_track_fitting/test_tracker_fitting/test_fixed_field_geometry.py
+++ tests/integration/test_global_track_fitting/test_tracker_fitting/test_fixed_field_geometry.py
@@ -6,16 +6,22 @@ class FixedFieldGeometryTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.test_dir = os.path.expandvars("${MAUS_ROOT_DIR}/tests/"+\
-                   "integration/test_global_track_fitting/test_tracker_fitting"
+                   "integration/test_global_track_fitting/test_tracker_fitting")
         cls.sim = os.path.expandvars("${MAUS_ROOT_DIR}/bin/simulate_mice.py")
+        cls.out = os.path.expandvars("${MAUS_TMP_DIR}/tmp/test_fixed_field_geometry.root")
 
     def generate_dataset(self):
         config = os.path.join(self.test_dir, "fixed_field_config.py")
         geometry = os.path.join(self.test_dir, "fixed_field_geometry.dat")
-        subprocess.POpen([self.sim, "--configuration_file", config])
+        proc = subprocess.Popen([self.sim, "--configuration_file", config,
+                                    "--simulation_geometry_filename", geometry,
+                                    "--output_root_file_name", self.out])
+        proc.wait()
 
     def test_fixed_field(self):
-        pass
+        print "Generating data"
+        self.generate_dataset()
+        print "Generated data"
 
 if __name__ == "__main__":
     unittest.main()
