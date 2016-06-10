7839cfde1a8c20321a2dd94375906576ab060b0e
Adam Dobbs <phuccj@gmail.com>
2016-06-10 12:57:21 +0100

    Style fix; updated the bin/user simulation scripts; updated the workbook

--- bin/user/simulate_mice.py
+++ bin/user/simulate_mice.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python
 
-#################################################################
-###!!! YOU ARE NOT ALLOWED TO MODIFY THIS FILE DIRECTLY    !!!###
-###!!! PLEASE MAKE A COPY OF THIS FILE WITH THE CP COMMAND !!!###
-#################################################################
+"""
+Simulate the MICE experiment
 
-"""Simulate the MICE experiment
-
-This will simulate 'number_of_spills' MICE events through the entirity
-of MICE using Geant4.  At present, TOF and Tracker hits will be digitized.
+This will simulate MICE spills through the entirety of MICE using Geant4, then
+digitize and reconstruct TOF and tracker hits to space points.
 """
 
 import io   #  generic python library for I/O
-import gzip #  For compressed output # pylint: disable=W0611
 
-import MAUS
+import MAUS # MAUS libraries
 
 def run():
     """ Run the macro
@@ -26,24 +21,48 @@ def run():
     # Create an empty array of mappers, then populate it
     # with the functionality you want to use.
     my_map = MAUS.MapPyGroup()
+
+    # GEANT4
     my_map.append(MAUS.MapPyBeamMaker()) # beam construction
     my_map.append(MAUS.MapCppSimulation())  #  geant4 simulation
-    # my_map.append(MAUS.MapCppTrackerMCDigitization())  # SciFi electronics model
-    # my_map.append(MAUS.MapCppTrackerRecon())  # SciFi recon
+
+    # Pre detector set up
+    # my_map.append(MAUS.MapPyMCReconSetup())  #  geant4 simulation
+    my_map.append(MAUS.MapCppMCReconSetup())  #  geant4 simulation
+
+    # TOF
+    my_map.append(MAUS.MapCppTOFMCDigitizer())  # TOF MC Digitizer
+    my_map.append(MAUS.MapCppTOFSlabHits()) # TOF MC Slab Hits
+    my_map.append(MAUS.MapCppTOFSpacePoints()) # TOF Space Points
+
+
+    # KL
+    my_map.append(MAUS.MapCppKLMCDigitizer())  # KL MC Digitizer
+    my_map.append(MAUS.MapCppKLCellHits())  # KL CellHit Reco
+
+    # SciFi
+    my_map.append(MAUS.MapCppTrackerMCDigitization()) # SciFi electronics model
+    my_map.append(MAUS.MapCppTrackerRecon()) # SciFi Recon
+
+    # EMR
+    my_map.append(MAUS.MapCppEMRMCDigitization())  # EMR MC Digitizer
+    my_map.append(MAUS.MapCppEMRSpacePoints())  # EMR Space Points
+    my_map.append(MAUS.MapCppEMRRecon()) # EMR Recon
+
+    # Ckov
+    my_map.append(MAUS.MapCppCkovMCDigitizer())
+
+    # Global Digits - post detector digitisation
+
+    # Then construct a MAUS output component - filename comes from datacards
+    my_output = MAUS.OutputCppRoot()
+
     # can specify datacards here or by using appropriate command line calls
     datacards = io.StringIO(u"")
 
-    # reducer = MAUS.ReduceCppPatternRecognition()
-    reducer = MAUS.ReducePyDoNothing()
-
-    # Then construct a MAUS output component - filename comes from datacards
-    my_output = MAUS.OutputPyJSON()
-    # my_output = MAUS.OutputCppRoot()
-
     # The Go() drives all the components you pass in, then check the file
     # (default simulation.out) for output
-
-    MAUS.Go(my_input, my_map, reducer, my_output, datacards)
+    MAUS.Go(my_input, my_map, MAUS.ReducePyDoNothing(), my_output, datacards)
 
 if __name__ == '__main__':
     run()
