417df184ce5b018efb504f8fd2ee5b5be7f54ca1
Chris Rogers <chris.rogers@stfc.ac.uk>
2016-11-08 17:55:16 +0000

    typo

--- tests/cpp_unit/Recon/Kalman/Global/ErrorTrackingControlTest.cc
+++ tests/cpp_unit/Recon/Kalman/Global/ErrorTrackingControlTest.cc
@@ -56,5 +56,4 @@ TEST(ErrorTrackingControlTest, SetStepSizeTest) {
     EXPECT_NEAR(step, 1., 1e-9);
     EXPECT_EQ(ret_val, GSL_ODEIV_HADJ_NIL);
     gsl_odeiv_control_free(control);
->>>>>>> MERGE-SOURCE
 }
