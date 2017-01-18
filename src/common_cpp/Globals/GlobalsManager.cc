5e653f29284e075297c34f70123c89879008ef7a
Chris Rogers <chris.rogers@stfc.ac.uk>
2017-01-18 13:53:38 +0000

    Fix segv in Globals (when setting MiceModules to NULL)

--- src/common_cpp/Globals/GlobalsManager.cc
+++ src/common_cpp/Globals/GlobalsManager.cc
@@ -219,8 +219,10 @@ void GlobalsManager::SetMonteCarloMiceModules(MiceModule* mc_mods) {
     delete Globals::_process->_mc_geometry_navigator;
     Globals::_process->_mc_geometry_navigator = new GeometryNavigator();
     Globals::_process->_mc_geometry_navigator->Initialise(world);
-    double z_world = mc_mods->propertyHep3Vector("Dimensions").z();
-    MaterialModelAxialLookup::BuildLookupTable(-z_world, z_world);
+    if (mc_mods != NULL) {
+        double z_world = mc_mods->propertyHep3Vector("Dimensions").z();
+        MaterialModelAxialLookup::BuildLookupTable(-z_world, z_world);
+    }
 }
 
 void GlobalsManager::SetLegacyCards(dataCards* legacy_cards) {
